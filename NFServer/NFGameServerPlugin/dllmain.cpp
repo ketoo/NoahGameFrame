beddc53baf5760dbf82d94e200f5b2e3e622469f
kytooooo <ketoo@users.noreply.github.com>
2014-05-23 12:01:38 +0800

    fixed bug for build

--- NFServer/NFGameServerPlugin/dllmain.cpp
+++ NFServer/NFGameServerPlugin/dllmain.cpp
@@ -12,7 +12,7 @@
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
 #pragma comment( lib, "NFCore_d.a" )
 #pragma comment( lib, "NFMessageDefine_d.a" )
-#pragma comment( lib, "libglog.lib" )
+#pragma comment( lib, "libglog_d.lib" )
 
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
@@ -21,14 +21,13 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "NFCore.lib" )
-//#pragma comment( lib, "NFMessageDefine.lib" )
-#pragma comment( lib, "libglog_static_x64.lib" )
+#pragma comment( lib, "NFMessageDefine.lib" )
+#pragma comment( lib, "libglog.lib" )
 
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-//#pragma comment( lib, "RakNet_LibStatic_Release_Win32.a" )
-//#pragma comment( lib, "NFNet.a" )
 #pragma comment( lib, "NFCore.a" )
-//#pragma comment( lib, "NFMessageDefine.a" )
+#pragma comment( lib, "NFMessageDefine.a" )
+#pragma comment( lib, "libglog.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
