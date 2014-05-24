84be12b2c9511661ae2323b7189e8d17f407069b
kytooooo <ketoo@users.noreply.github.com>
2014-05-24 17:52:24 +0800

    complete component module

--- NFComm/NFCore/NFCComponent.cpp
+++ NFComm/NFCore/NFCComponent.cpp
@@ -5,3 +5,30 @@
 //    @Module           :    NFCComponent
 //
 // -------------------------------------------------------------------------
+
+#include "NFCComponent.h"
+
+NFIDENTID NFCComponent::Self()
+{
+    return mSelf;
+}
+
+bool NFCComponent::SetEnable( const bool bEnable )
+{
+    mbEnable = bEnable;
+}
+
+bool NFCComponent::Enable()
+{
+    return mbEnable;
+}
+
+const std::string& NFCComponent::ComponentName()
+{
+    return mstrName;
+}
+
+const std::string& NFCComponent::ScriptName()
+{
+    return mstrScriptName;
+}
