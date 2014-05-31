8e3087d3bda2cb201eed2590811a23d7cc31cb9f
kytooooo <ketoo@users.noreply.github.com>
2014-05-31 11:04:23 +0800

    add script frame for component

---
+++ NFComm/NFPluginModule/NFILuaScriptModule.h
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------
+//    @FileName         £º    NFILuaScriptModule.h
+//    @Author           £º    LvSheng.Huang
+//    @Date             £º    2013-07-08
+//    @Module           £º    NFILuaScriptModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef _NFI_LUA_SCRIPT_MODULE_H_
+#define _NFI_LUA_SCRIPT_MODULE_H_
+
+#include <iostream>
+#include "NFIScriptModule.h"
+
+class NFILuaScriptModule
+    : public NFIScriptModule
+{
+
+public:
+
+};
+
+#endif
