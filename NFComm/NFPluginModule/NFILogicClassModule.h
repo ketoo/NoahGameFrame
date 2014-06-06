75f1d900ca7521118b6fc11be8ba9a537b9638c8
lvsheng.huang <lvsheng.huang@mokun.net>
2014-06-06 11:44:38 +0800

    modified some interface for kernel(add property)
    Former-commit-id: a99bbf34ee6d0722de79ba6c0390dd74d2d19793 [formerly e1e3bef1bdd6c398213361bd22db19c6fcef4c36]
    Former-commit-id: 46ca13c9a28f7bfa265363c3e617f3fdb8ecad48

--- NFComm/NFPluginModule/NFILogicClassModule.h
+++ NFComm/NFPluginModule/NFILogicClassModule.h
@@ -13,6 +13,7 @@
 #include "NFILogicModule.h"
 #include "NFComm/NFCore/NFIPropertyManager.h"
 #include "NFComm/NFCore/NFIRecordManager.h"
+#include "NFComm/NFCore/NFIComponentManager.h"
 
 class NFILogicClass
     : public NFList<std::string>//include files
@@ -23,6 +24,8 @@ public:
     virtual NFIPropertyManager* GetPropertyManager() = 0;
 
     virtual NFIRecordManager* GetRecordManager() = 0;
+
+    virtual NFIComponentManager* GetComponentManager() = 0;
 
     virtual void SetParent(NFILogicClass* pClass) = 0;
     virtual NFILogicClass* GetParent() = 0;
@@ -47,6 +50,8 @@ public:
     virtual NFIPropertyManager* GetClassPropertyManager(const std::string& strClassName) = 0;
 
     virtual NFIRecordManager* GetClassRecordManager(const std::string& strClassName) = 0;
+
+    virtual NFIComponentManager* GetClassComponentManager(const std::string& strClassName) = 0;
 
 };
 
