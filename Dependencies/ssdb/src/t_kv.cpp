5f5c13ff7762d77b86d7df005792c78c49538ef5
eliteYang <elite_yang@163.com>
2014-06-07 17:52:12 +0800

    add ssdb project , for dbserver
    Former-commit-id: e119e8466d52d7b5704fc976fa5930a1db6caabf [formerly 8666c8fe16d27e36b1415cbccab0ce9159fc0c63]
    Former-commit-id: 1f3a044c863e07d83274161b11a8c55ecb8315a2

---
+++ Dependencies/ssdb/src/t_kv.cpp
@@ -0,0 +1,199 @@
+#include "t_kv.h"
+#include "leveldb/write_batch.h"
+
+int SSDB::multi_set(const std::vector<Bytes> &kvs, int offset, char log_type){
+	Transaction trans(binlogs);
+
+	std::vector<Bytes>::const_iterator it;
+	it = kvs.begin() + offset;
+	for(; it != kvs.end(); it += 2){
+		const Bytes &key = *it;
+		if(key.empty()){
+			log_error("empty key!");
+			return 0;
+			//return -1;
+		}
+		const Bytes &val = *(it + 1);
+		std::string buf = encode_kv_key(key);
+		binlogs->Put(buf, val.Slice());
+		binlogs->add_log(log_type, BinlogCommand::KSET, buf);
+	}
+	leveldb::Status s = binlogs->commit();
+	if(!s.ok()){
+		log_error("multi_set error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return (kvs.size() - offset)/2;
+}
+
+int SSDB::multi_del(const std::vector<Bytes> &keys, int offset, char log_type){
+	Transaction trans(binlogs);
+
+	std::vector<Bytes>::const_iterator it;
+	it = keys.begin() + offset;
+	for(; it != keys.end(); it++){
+		const Bytes &key = *it;
+		std::string buf = encode_kv_key(key);
+		binlogs->Delete(buf);
+		binlogs->add_log(log_type, BinlogCommand::KDEL, buf);
+	}
+	leveldb::Status s = binlogs->commit();
+	if(!s.ok()){
+		log_error("multi_del error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return keys.size() - offset;
+}
+
+int SSDB::set(const Bytes &key, const Bytes &val, char log_type){
+	if(key.empty()){
+		log_error("empty key!");
+		//return -1;
+		return 0;
+	}
+	Transaction trans(binlogs);
+
+	std::string buf = encode_kv_key(key);
+	binlogs->Put(buf, val.Slice());
+	binlogs->add_log(log_type, BinlogCommand::KSET, buf);
+	leveldb::Status s = binlogs->commit();
+	if(!s.ok()){
+		log_error("set error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return 1;
+}
+
+int SSDB::setnx(const Bytes &key, const Bytes &val, char log_type){
+	if(key.empty()){
+		log_error("empty key!");
+		//return -1;
+		return 0;
+	}
+	Transaction trans(binlogs);
+
+	std::string tmp;
+	int found = this->get(key, &tmp);
+	if(found != 0){
+		return 0;
+	}
+	std::string buf = encode_kv_key(key);
+	binlogs->Put(buf, val.Slice());
+	binlogs->add_log(log_type, BinlogCommand::KSET, buf);
+	leveldb::Status s = binlogs->commit();
+	if(!s.ok()){
+		log_error("set error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return 1;
+}
+
+int SSDB::getset(const Bytes &key, std::string *val, const Bytes &newval, char log_type){
+	if(key.empty()){
+		log_error("empty key!");
+		//return -1;
+		return 0;
+	}
+	Transaction trans(binlogs);
+
+	int found = this->get(key, val);
+	std::string buf = encode_kv_key(key);
+	binlogs->Put(buf, newval.Slice());
+	binlogs->add_log(log_type, BinlogCommand::KSET, buf);
+	leveldb::Status s = binlogs->commit();
+	if(!s.ok()){
+		log_error("set error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return found;
+}
+
+
+int SSDB::del(const Bytes &key, char log_type){
+	Transaction trans(binlogs);
+
+	std::string buf = encode_kv_key(key);
+	binlogs->begin();
+	binlogs->Delete(buf);
+	binlogs->add_log(log_type, BinlogCommand::KDEL, buf);
+	leveldb::Status s = binlogs->commit();
+	if(!s.ok()){
+		log_error("del error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return 1;
+}
+
+int SSDB::incr(const Bytes &key, int64_t by, std::string *new_val, char log_type){
+	Transaction trans(binlogs);
+
+	int64_t val;
+	std::string old;
+	int ret = this->get(key, &old);
+	if(ret == -1){
+		return -1;
+	}else if(ret == 0){
+		val = by;
+	}else{
+		val = str_to_int64(old.data(), old.size()) + by;
+	}
+
+	*new_val = int64_to_str(val);
+	std::string buf = encode_kv_key(key);
+	
+	binlogs->Put(buf, *new_val);
+	binlogs->add_log(log_type, BinlogCommand::KSET, buf);
+
+	leveldb::Status s = binlogs->commit();
+	if(!s.ok()){
+		log_error("del error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return 1;
+}
+
+int SSDB::get(const Bytes &key, std::string *val) const{
+	std::string buf = encode_kv_key(key);
+
+	leveldb::Status s = db->Get(leveldb::ReadOptions(), buf, val);
+	if(s.IsNotFound()){
+		return 0;
+	}
+	if(!s.ok()){
+		log_error("get error: %s", s.ToString().c_str());
+		return -1;
+	}
+	return 1;
+}
+
+KIterator* SSDB::scan(const Bytes &start, const Bytes &end, uint64_t limit) const{
+	std::string key_start, key_end;
+	key_start = encode_kv_key(start);
+	if(end.empty()){
+		key_end = "";
+	}else{
+		key_end = encode_kv_key(end);
+	}
+	//dump(key_start.data(), key_start.size(), "scan.start");
+	//dump(key_end.data(), key_end.size(), "scan.end");
+
+	return new KIterator(this->iterator(key_start, key_end, limit));
+}
+
+KIterator* SSDB::rscan(const Bytes &start, const Bytes &end, uint64_t limit) const{
+	std::string key_start, key_end;
+
+	key_start = encode_kv_key(start);
+	if(start.empty()){
+		key_start.append(1, 255);
+	}
+	if(end.empty()){
+		key_end = "";
+	}else{
+		key_end = encode_kv_key(end);
+	}
+	//dump(key_start.data(), key_start.size(), "scan.start");
+	//dump(key_end.data(), key_end.size(), "scan.end");
+
+	return new KIterator(this->rev_iterator(key_start, key_end, limit));
+}
