f2e8f46da0f0720c50edfa2e837dac4224e93bb7
kytooooo <kytooooo>
2014-06-12 21:00:56 +0800

    fixed some bug for net library

--- NFComm/NFNet/NFCPacket.h
+++ NFComm/NFNet/NFCPacket.h
@@ -18,20 +18,42 @@
 class NFCPacket : public NFIPacket
 {
 public:
-	NFCPacket()
+	NFCPacket(NFIMsgHead::NF_Head head)
 	{
 		munFD = 0;
-        mHead.unHead = 0;
+        switch (head)
+        {
+        case NFIMsgHead::NF_HEAD_LENGTH:
+            pHead = new NFCMsgHead();
+            break;
+//         case NFIMsgHead::NF_CS_HEAD_LENGTH:
+//             pHead = new MsgCSHead();
+//             break;
+//         case NFIMsgHead::NF_SS_HEAD_LENGTH:
+//             pHead = new MsgSSHead();
+//             break;
+        default:
+            break;
+        }
 	}
 
-	virtual const MsgHead& GetMsgHead() const
+    ~NFCPacket()
+    {
+        if (pHead)
+        {
+            delete pHead;
+            pHead = NULL;
+        }
+    }
+
+	virtual const NFIMsgHead* GetMsgHead() const
 	{
-		return mHead;
+		return pHead;
 	}
 
     virtual const uint32_t GetMsgID() const
     {
-        return mHead.unMsgID;
+        return pHead->GetMsgID();
     }
 
 	virtual const char* GetPacketData() const
@@ -41,17 +63,17 @@ public:
 
 	virtual const uint32_t GetPacketLen() const
 	{
-		return mHead.unDataLen;
+		return pHead->GetMsgLength();
 	}
 
     virtual const uint32_t GetDataLen() const
     {
-        return mHead.unDataLen - NF_HEAD_SIZE;
+        return pHead->GetMsgLength() - pHead->GetHeadLength();
     }
 
 	virtual const char* GetData() const
 	{
-		return strPackData + NF_HEAD_SIZE;//not include head
+		return strPackData + pHead->GetHeadLength();//not include head
 	}
 
 	virtual void Construction(const NFIPacket& packet)
@@ -59,35 +81,32 @@ public:
 		memcpy(this->strPackData, packet.GetPacketData(), this->GetPacketLen());
 	}
 
-	virtual const uint16_t GetFd() const
+	virtual const uint32_t GetFd() const
 	{
 		return munFD;
 	}
 
-	virtual void SetFd(const uint16_t nFd)
+	virtual void SetFd(const uint32_t nFd)
 	{
 		munFD = nFd;
 	}
 
     virtual uint32_t GetHeadSize()
     {
-        return sizeof(MsgHead);
+        return pHead->GetHeadLength();
     }
 
-//     virtual int EnCodeHead(uint32_t uMsgID, const char* strData, const uint32_t unLen);
-//     virtual int DeCodeHead(const char* strData, const uint32_t unLen);
-
     //const uint32_t unLen: length of data, not include head
-	virtual int EnCode(uint32_t uMsgID, const char* strData, const uint32_t unLen);
+	virtual int EnCode(char* pHeadBuffer, const char* strData, const uint32_t unLen);
 
     //const uint32_t unLen: length of buff
 	virtual int DeCode(const char* strData, const uint32_t unLen);
 
 protected:
 private:
-	MsgHead mHead;
+	NFIMsgHead* pHead;
 	char strPackData[NF_MAX_SERVER_PACKET_SIZE];//include head
-	uint16_t munFD;;
+	uint32_t munFD;;
 };
 
 
