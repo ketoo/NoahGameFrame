af19db89a7924051f291fc5b699bacf749ac11ce
eliteYang <elite_yang@163.com>
2014-06-29 21:10:18 +0800

    modify protocol type; modify proxyserver connection info(comment temporally)

--- NFServer/NFProxyServerNet_ServerPlugin/NFCProxyServerNet_ServerModule.h
+++ NFServer/NFProxyServerNet_ServerPlugin/NFCProxyServerNet_ServerModule.h
@@ -69,6 +69,31 @@ protected:
     int OnWantToConnectObjectEvent(const NFIDENTID& self, const std::string& strClassNames, const CLASS_OBJECT_EVENT eClassEvent, const NFIValueList& var);
 
 protected:
+    struct ConnectData 
+    {
+        ConnectData()
+        {
+            strAccount = "";
+            strConnectKey = "";
+        }
+
+        std::string strAccount;
+        std::string strConnectKey;
+    };
+
+    struct ServerData
+    {
+        ServerData()
+        {
+            nServerID = 0;
+            eState = NFMsg::EST_CRASH;
+        }
+
+        int nServerID;
+        NFMsg::EServerState eState;
+    };
+
+protected:
     //新建立的连接对象，等待他们自己发验证KEY，KEY验证后删掉
     //-1
     int mnConnectContainer;
@@ -80,6 +105,15 @@ protected:
     //-3
     int mnGameContainerID;
 
+    // 世界服务器发过来，谁想登录此服务器的那个对象，包含帐号和KEY信息，KEY验证后删掉
+    typedef NFMap<std::string, ConnectData> ConnectionMap; // map<Account, ConnectData>
+    ConnectionMap mnWantConnectionMap;
+
+    // Game容器
+    typedef NFMap<int, ServerData> ServerDataMap;
+    ServerDataMap mnGameDataMap;
+
+protected:
     NFIProxyServerNet_ClientModule* m_pProxyServerNet_ClientModule;
 	NFIKernelModule* m_pKernelModule;
     NFILogModule* m_pLogModule;
