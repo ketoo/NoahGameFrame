3752d1c15cd21a2749fe0ecd9c6605f0107deb6a
eliteYang <elite_yang@163.com>
2014-06-30 15:01:17 +0800

    Add management for game and connection
    Former-commit-id: 6510066b3db1d087421797add2d45d00d2074b85 [formerly d33bf0543f377ea4db2c0dda5a04cb033c373c74]
    Former-commit-id: 99983f552e636d9bc9b89b398023f1ae576167ed

--- NFComm/NFPluginModule/NFIProxyServerNet_ClientModule.h
+++ NFComm/NFPluginModule/NFIProxyServerNet_ClientModule.h
@@ -16,13 +16,48 @@
 class NFIProxyServerNet_ClientModule
     : public  NFINetModule, public NFMap<int64_t, NFINetModule>
 {
+public:
+    struct ConnectData 
+    {
+        ConnectData()
+        {
+            strAccount = "";
+            strConnectKey = "";
+        }
 
+        std::string strAccount;
+        std::string strConnectKey;
+    };
+
+    struct GameData 
+    {
+        GameData()
+        {
+            nGameID = 0;
+            strIP = "";
+            nPort = 0;
+            strName = "";
+            eState = NFMsg::EServerState::EST_CRASH;
+        }
+
+        int nGameID;
+        std::string strIP;
+        int nPort;
+        std::string strName;
+        NFMsg::EServerState eState;
+    };
+
+    typedef NFMap<std::string, ConnectData> ConnectDataMap;
+    typedef NFMap<int, GameData> GameDataMap;
 public:
     //>0 gs_id, ==0 all gs, < 0 worldserver
     //     virtual int Transpond( int nGameServerID, NFIPacket* pMsgPacket ) = 0;
     //     virtual int Transpond( int nGameServerID, RakNet::BitStream* pBitStream ) = 0;
     virtual int Transpond(int nGameServerID, const NFIPacket& msg) = 0;
 
+    virtual ConnectData* GetConnectData(const std::string& strAccount) = 0;
+    virtual GameData* GetGameData(int nGameID) = 0;
+    virtual GameDataMap& GetGameDataMap() = 0;
 };
 
 #endif
