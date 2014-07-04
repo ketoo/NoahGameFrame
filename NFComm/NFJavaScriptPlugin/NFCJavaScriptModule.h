98283640561e7c46e2344272776e70121b3f63ec
kytooooo <kytooooo>
2014-07-04 20:10:02 +0800

    fixed bug for compile java script plugin

--- NFComm/NFJavaScriptPlugin/NFCJavaScriptModule.h
+++ NFComm/NFJavaScriptPlugin/NFCJavaScriptModule.h
@@ -29,6 +29,16 @@ public:
 
     virtual bool AfterInit();
 
+    virtual int DoScript(const NFIDENTID& self, const std::string& strComponentName, const std::string& strFunction, const NFCScriptVarList& arg);
+    virtual int DoEventScript(const NFIDENTID& self, const int nEventID, const std::string& strComponentName, const std::string& strFunction, const NFCScriptVarList& arg);
+    virtual int DoHeartBeatScript(const NFIDENTID& self, const std::string& strHeartBeat, const float fTime, const int nCount, std::string& strComponentName, const std::string& strFunction, const NFCScriptVarList& arg);
+
+
+    virtual int DoScriptPropertyCallBack(const NFIDENTID& self, const std::string& strPropertyName, const std::string& strComponentName, const std::string& strFunction, const NFCScriptVarList& oldVar, const NFCScriptVarList& neVar, const NFCScriptVarList& arg);
+    virtual int DoScriptRecordCallBack(const NFIDENTID& self, const std::string& strRecordName, const std::string& strComponentName, const std::string& strFunction, const int nOpType, const int nRow, const int nCol, const NFCScriptVarList& oldVar, const NFCScriptVarList& newVar, const NFCScriptVarList& arg);
+
+    virtual int DoHeartBeatCommonCB(const NFIDENTID& self, const std::string& strHeartBeat, const float fTime, const int nCount, const NFIValueList& var);
+    virtual int DoEventCommonCB(const NFIDENTID& self, const int nEventID, const NFIValueList& var);
 
 protected:
 
