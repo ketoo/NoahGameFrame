9c008a016452a364bff08e4ff0b2690fdaa53632
kytooooo <kytooooo>
2014-07-05 19:40:32 +0800

    .
    Former-commit-id: 87a059950da69c0c73e553236d0439bc3ca315f9 [formerly 6ffc880e22aba3da26aaa88db3cf833e47240419]
    Former-commit-id: 0f4c7431de736be91f6f51155ffaff8158ba539d

--- _Out/Server/Debug/Run.bat
+++ _Out/Server/Debug/Run.bat
@@ -2,16 +2,8 @@ cd /d %~dp0
 
 @echo off
 
-cd  ..
-cd  Release\Redis\redisbin
-echo Starting Redis...
-start "" "redis-server.exe"
 
-choice /t 2 /d y /n >nul
-cd  ..
-cd  ..
-cd  ..
-cd  Debug\NFMasterServer
+cd  NFMasterServer
 
 echo Starting NFMasterServer...
 start "" "NFPluginLoader_d.exe"
@@ -35,13 +27,6 @@ choice /t 2 /d y /n >nul
 cd  ..
 cd  NFGameServer1
 echo Starting NFGameServer1...
-start "" "NFPluginLoader_d.exe"
-
-choice /t 2 /d y /n >nul
-
-cd  ..
-cd  NFGameServer2
-echo Starting NFGameServer2...
 start "" "NFPluginLoader_d.exe"
 
 choice /t 4 /d y /n >nul
