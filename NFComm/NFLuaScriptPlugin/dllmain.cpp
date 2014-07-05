44bd2bf26e8df26b327e80f6baad4772189e8222
flyicegood <flyicegood@163.com>
2014-07-05 11:10:42 +0800

    remove lib
    Former-commit-id: 77eee552054c2372e24842ad899d05c470ace167 [formerly 7d29e9a1449eb36319fac9183f1e5253897d2e57]
    Former-commit-id: 1bf46565b806a9af3756b56aac1af589b33e7170

--- NFComm/NFLuaScriptPlugin/dllmain.cpp
+++ NFComm/NFLuaScriptPlugin/dllmain.cpp
@@ -4,16 +4,12 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "NFNet_d.lib" )
 #pragma comment( lib, "NFCore_d.lib" )
-#pragma comment( lib, "NFMessageDefine_d.lib" )
 #pragma comment( lib, "lua-d.lib" )
 #pragma comment( lib, "Luabind-d.lib" )
 
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "NFNet_d.a" )
 #pragma comment( lib, "NFCore_d.a" )
-#pragma comment( lib, "NFMessageDefine_d.a" )
 #pragma comment( lib, "lua-d.a" )
 #pragma comment( lib, "Luabind-d.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
@@ -23,15 +19,11 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "NFNet.lib" )
 #pragma comment( lib, "NFCore.lib" )
-#pragma comment( lib, "NFMessageDefine.lib" )
 #pragma comment( lib, "lua.lib" )
 #pragma comment( lib, "Luabind.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "NFNet.a" )
 #pragma comment( lib, "NFCore.a" )
-#pragma comment( lib, "NFMessageDefine.a" )
 #pragma comment( lib, "lua.a" )
 #pragma comment( lib, "Luabind.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
