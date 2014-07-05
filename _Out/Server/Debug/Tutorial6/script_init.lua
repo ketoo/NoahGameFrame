c6586ee5bfd045d8633609f1e1bd6aa6d9bfaeef
flyicegood <flyicegood@163.com>
2014-07-05 16:40:37 +0800

    add lua comment
    Former-commit-id: 047c0595a4a148a7146f02f16a97bda7e0fd18ff [formerly 8783449d38ba166a3a7a7817d4d26d99092f25eb]
    Former-commit-id: 272d5d90cf5c2fa9c10ee88db0f71ebcb64b797e

--- _Out/Server/Debug/Tutorial6/script_init.lua
+++ _Out/Server/Debug/Tutorial6/script_init.lua
@@ -1,7 +1,89 @@
--- the first program in every language
---
---
---
+-- --NFLuaAPI:
+
+-- SetComponentEnable(kernel, self, strComponentName, bEnable)
+-- QueryComponentEnable(kernel, self, strComponentName)
+
+-- CreateContainer(kernel, nContainerIndex, strSceneConfigID)
+-- ExistContainer(kernel, nContainerIndex)
+
+-- CreateObject(kernel, self, nContainerID, nGroupID, strClassName, strConfigIndex, arg)
+-- DestroyObject(kernel, self)
+
+-- FindProperty(kernel, self, strPropertyName)
+-- AddProperty(kernel, self, strPropertyName, varType, bPublic, bPrivate, bSave, nIndex, strScriptFunction)
+
+-- SetPropertyInt(kernel, self, strPropertyName, nValue)
+-- SetPropertyFloat(kernel, self, strPropertyName, fValue)
+-- SetPropertyString(kernel, self, strPropertyName, strValue)
+-- SetPropertyObject(kernel, self, strPropertyName, n64Value)
+
+-- QueryPropertyInt(kernel, self, strPropertyName)
+-- QueryPropertyFloat(kernel, self, strPropertyName)
+-- QueryPropertyString(kernel, self, strPropertyName)
+-- QueryPropertyObject(kernel, self, strPropertyName)
+
+-- AddRow(kernel, self, strRecordName, var)
+-- SetRecordInt(kernel, self, strRecordName, nRow, nCol, nValue)
+-- SetRecordFloat(kernel, self, strRecordName, nRow, nCol, fValue)
+-- SetRecordString(kernel, self, strRecordName, nRow, nCol, strValue)
+-- SetRecordObject(kernel, self, strRecordName, nRow, nCol, n64Value)
+
+-- QueryRecordInt(kernel, self, strRecordName, nRow, nCol)
+-- QueryRecordFloat(kernel, self, strRecordName, nRow, nCol)
+-- QueryRecordString(kernel, self, strRecordName, nRow, nCol)
+-- QueryRecordObject(kernel, self, strRecordName, nRow, nCol)
+
+-- HasEventCallBack(kernel, self, nEventID)
+-- RemoveEvent(kernel, nEventID)
+-- RemoveEventCallBack(kernel, self, nEventID)
+-- DoEvent(kernel, self, nEventID, arg)
+
+-- FindHeartBeat(kernel, self, strHeartBeatName)
+-- RemoveHeartBeat(kernel, self, strHeartBeatName)
+
+
+-- ExistElement(kernel, strConfig)
+-- QueryPropertyInt(kernel, strConfig, strPropertyName)
+-- QueryPropertyFloat(kernel, strConfig, strPropertyName)
+-- QueryPropertyString(kernel, strConfig, strPropertyName)
+
+-- AddPropertyCallBack(kernel, self, strPropertyName, strComponentName, strFunction)
+-- -- eg:look at "Test:MaxPropertyCallBack(kernel, self, propertyName, oldVar, newVar, arg)" in Test.lua
+
+-- AddRecordCallBack(kernel, self, strRecordName, strComponentName, strFunction)
+-- -- eg:look at "Test:TaskListCallBack(kernel, self, recordName, nOpType, nRow, nCol,oldVar, newVar, arg)" in Test.lua
+	
+-- AddEventCallBack(kernel, self, nEventID, strComponentName, strFunction)
+-- -- eg:look at "Test:EventCallBack(kernel, self, nEventID, arg)" in Test.lua
+	
+-- AddHeartBeat(kernel, self, strHeartBeatName, strComponentName, strFunction, fTime, nCount)
+-- -- eg:look at "Test:HearCallBack(kernel, self, strHeartBeat, fTime, nCount, arg)" in Test.lua
+
+-- -- NFLuaClass
+
+-- local Var = NFCVarList();
+-- Var:IsEmpty()
+-- Var:GetCount()
+-- Var:Type(index)
+-- Var:AddInt(nValue)
+-- Var:AddFloat(fValue)
+-- Var:AddString(strValue)
+-- Var:AddObject(n64Value)
+-- Var:SetInt(index, nValue)
+-- Var:SetFloat(index, fValue)
+-- Var:SetString(index, strValue)
+-- Var:SetObject(index, n64Value)
+-- Var:IntVal(index)
+-- Var:FloatVal(index)
+-- Var:StringVal(index)
+-- Var:ObjectVal(index)
+
+-- local n64Var = NFINT64();
+-- n64Var:GetIdent()
+-- n64Var:GetnSerial()
+-- n64Var:SetIdent(nValue)
+-- n64Var:SetnSerial(nValue)
+
 package.path = '../../NFDataCfg/Ini/Components/?.lua;'
 
 function load_script_file(name)
