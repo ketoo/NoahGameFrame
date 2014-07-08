742b07129314ea03be45cb24376d480de4ae8a6d
eliteYang <elite_yang@163.com>
2014-07-08 15:25:39 +0800

    Add xls to xml tool;add sample logic config excel file(Player.xlsx)
    Former-commit-id: b96028694d0b2d7ca5a5154ef9214a6428c2a0dc [formerly 8aed5f8871246b074fcdc5dacd4747f19c032aba]
    Former-commit-id: 9b726664042f9664740a81a09038d85eab98d337

---
+++ _Out/Server/NFDataCfg/Tool/main.py
@@ -0,0 +1,8 @@
+# encoding:utf-8
+from cx_Freeze import setup, Executable
+
+setup(
+name = "xls2xml",
+version = "0.1",
+descrīption = "COPY (C) NoahGameFramework www.yowoyo.com",
+executables = [Executable("xls2xml.py", icon = "setting.ico")])
