f443209f34b4b88cd0425fe60b9e212f1bd545df
kytooooo <kytooooo>
2014-07-08 20:38:36 +0800

    add element's manager and logic class for client

---
+++ MiniClient/Assets/NFCoreEx/Interface/NFIElement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFCoreEx
+{
+	public abstract class NFIElement
+	{
+        public abstract NFIPropertyManager GetPropertyManager();
+	}
+}
