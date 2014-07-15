3d9a60513c04960c72c65317ae74d04320949a72
kytooooo <kytooooo>
2014-07-15 18:28:38 +0800

    modified project property

---
+++ NFComm/NFPluginModule/NFIGameServerModule.h
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------
+//    @FileName         £º    NFIGameLogicModule.h
+//    @Author           £º    LvSheng.Huang
+//    @Date             £º    2012-12-15
+//    @Module           £º    NFIGameLogicModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef _NFI_GAMESERVER_MODULE_H_
+#define _NFI_GAMELOGIC_MODULE_H_
+
+#include <iostream>
+#include "NFILogicModule.h"
+
+class NFIGameServerModule
+    : public NFILogicModule
+{
+
+public:
+};
+
+#endif
