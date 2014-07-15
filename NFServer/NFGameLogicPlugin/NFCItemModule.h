ab906f841103c2bc35a2656b39c73eedea64dfd6
kytooooo <kytooooo>
2014-07-15 20:10:34 +0800

    add logic plugin
    Former-commit-id: 8d9b84bc2bf697718cd7e19936271613f5bfc432 [formerly 8f8f92d1d1b186ddcdd8047a3be6f814f9e36671]
    Former-commit-id: 43d3e5562bf5536d99bc90eabe5980ce80c8fbba

---
+++ NFServer/NFGameLogicPlugin/NFCItemModule.h
@@ -0,0 +1,56 @@
+// -------------------------------------------------------------------------
+//    @FileName      :    NFCMoveModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2013-06-11
+//    @Module           :    NFCMoveModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef _NFC_ITEM_MODULE_H_
+#define _NFC_ITEM_MODULE_H_
+
+#include "NFComm/NFPluginModule/NFIPluginManager.h"
+#include "NFComm/NFPluginModule/NFIKernelModule.h"
+#include "NFComm/NFPluginModule/NFIGameLogicModule.h"
+#include "NFComm/NFPluginModule/NFIEventProcessModule.h"
+#include "NFComm/NFPluginModule/NFIItemModule.h"
+#include "NFComm/NFPluginModule/NFIItemConsumeManagerModule.h"
+#include "NFCPackModule.h"
+#include "NFComm/NFPluginModule/NFILogicClassModule.h"
+
+class NFCItemModule
+    : public NFIItemModule
+{
+public:
+
+    NFCItemModule( NFIPluginManager* p )
+    {
+        pPluginManager = p;
+    }
+    virtual ~NFCItemModule() {};
+
+    virtual bool Init();
+    virtual bool Shut();
+    virtual bool Execute( const float fLasFrametime, const float fStartedTime );
+    virtual bool AfterInit();
+
+protected:
+    /**/int OnClassObjectEvent( const NFIDENTID& self, const std::string& strClassNames, const CLASS_OBJECT_EVENT eClassEvent, const NFIValueList& var );
+
+    /**/int OnRequireUseItemEvent( const NFIDENTID& self, const int nEventID, const NFIValueList& var );
+    /**/int OnRequireUseItemPosEvent( const NFIDENTID& self, const int nEventID, const NFIValueList& var );
+
+	virtual bool CheckConfig();
+private:
+    char* mstrPackTableName;
+
+    NFIEventProcessModule* m_pEventProcessModule;
+    NFIKernelModule* m_pKernelModule;
+    NFIItemConsumeManagerModule* m_pItemConsumeManagerModule;
+    NFIPackModule* m_pPackModule;
+    NFIElementInfoModule* m_pElementInfoModule;
+	NFILogicClassModule* m_pLogicClassModule;
+};
+
+
+#endif
