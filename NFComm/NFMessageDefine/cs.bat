6abd97fbc484b6ffd1512f865d61286e1ee7b597
kytooooo <kytooooo>
2014-08-04 14:11:24 +0800

    remove unused file

--- NFComm/NFMessageDefine/cs.bat
+++
@@ -1,8 +0,0 @@
-echo on
-protogen -i:NFDefine.proto -o:NFDefine.cs
-protogen -i:NFMsgBase.proto -o:NFMsgBase.cs
-protogen -i:NFMsgBaseEx.proto -o:NFMsgBaseEx.cs
-protogen -i:NFMsgPreGame.proto -o:NFMsgPreGame.cs
-protogen -i:NFMsgShare.proto -o:NFMsgShare.cs
-
-pause
