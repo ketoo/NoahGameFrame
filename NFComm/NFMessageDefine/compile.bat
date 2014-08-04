4ccd8e9b8091cf048989baa7d7d82339240d2099
kytooooo <kytooooo>
2014-08-04 14:11:24 +0800

    remove unused file

--- NFComm/NFMessageDefine/compile.bat
+++
@@ -1,8 +0,0 @@
-
-protoc.exe -I=./ --cpp_out=./ ./NFDefine.proto 
-protoc.exe -I=./ --cpp_out=./ ./NFMsgBase.proto 
-protoc.exe -I=./ --cpp_out=./ ./NFMsgBaseEx.proto 
-protoc.exe -I=./ --cpp_out=./ ./NFMsgPreGame.proto 
-protoc.exe -I=./ --cpp_out=./ ./NFMsgShare.proto 
-
-pause
