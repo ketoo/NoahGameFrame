48687d1ea0cd7175b72bffda66921c78ad53cd6b
kytooooo <kytooooo>
2014-09-15 20:23:30 +0800

    for building slg plugin
    Former-commit-id: ae3440438c5cfa6a5b0a0b3a155dee9e5c9cecff [formerly 5b7c60b36841bb263e55a6872ea83fe279d5d493]
    Former-commit-id: c36bf65421557ae35ebd7eda5338d3b2ec2ac888

---
+++ NFComm/NFPluginModule/NFISLGModule.h
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------
+//    @FileName      :    NFISLGModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2012-12-15
+//    @Module           :    NFISLGModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef _NFI_SLG_MODULE_H_
+#define _NFI_SLG_MODULE_H_
+
+#include "NFILogicModule.h"
+
+class NFISLGModule
+    : public NFILogicModule
+{
+public:
+
+protected:
+private:
+};
+
+#endif
