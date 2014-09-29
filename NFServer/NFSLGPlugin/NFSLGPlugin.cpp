9a92695720c84030bbc248312a770d2506183f74
eliteYang <elite_yang@163.com>
2014-09-29 14:18:35 +0800

    add SLG Server

--- NFServer/NFSLGPlugin/NFSLGPlugin.cpp
+++ NFServer/NFSLGPlugin/NFSLGPlugin.cpp
@@ -1,70 +1,40 @@
-// -------------------------------------------------------------------------
-//    @FileName      :    NFGameServerPlugin.cpp
-//    @Author           :    LvSheng.Huang
-//    @Date             :    2012-07-14 08:51
-//    @Module           :    NFGameServerPlugin
-//
-// -------------------------------------------------------------------------
-
-
-////#include "stdafx.h"
-#include "NFGameServerPlugin.h"
-#include "NFCGameServerModule.h"
-#include "NFCSceneProcessModule.h"
-#include "NFCPropertyModule.h"
-#include "NFCLevelModule.h"
-#include "NFCPropertyConfigModule.h"
-#include "NFCDataProcessModule.h"
+#include "NFSLGPlugin.h"
+#include "NFCSLGModule.h"
 
 #ifdef NF_DYNAMIC_PLUGIN
 
 extern "C"  __declspec( dllexport ) void DllStartPlugin( NFIPluginManager* pm )
 {
-	SetConsoleTitle( "NFGameServer" );
+	SetConsoleTitle( "NFSLG" );
 
-    CREATE_PLUGIN( pm, NFGameServerPlugin )
+    CREATE_PLUGIN( pm, NFSLGPlugin )
 
 };
 
 extern "C" __declspec( dllexport ) void DllStopPlugin( NFIPluginManager* pm )
 {
-    DESTROY_PLUGIN( pm, NFGameServerPlugin )
+    DESTROY_PLUGIN( pm, NFSLGPlugin )
 };
 
 #endif
 //////////////////////////////////////////////////////////////////////////
 
-const int NFGameServerPlugin::GetPluginVersion()
+const int NFSLGPlugin::GetPluginVersion()
 {
     return 0;
 }
 
-const std::string NFGameServerPlugin::GetPluginName()
+const std::string NFSLGPlugin::GetPluginName()
 {
-    GET_PLUGIN_NAME( NFGameServerPlugin )
+    GET_PLUGIN_NAME( NFSLGPlugin )
 }
 
-void NFGameServerPlugin::Install()
+void NFSLGPlugin::Install()
 {
-
-    REGISTER_MODULE( pPluginManager, NFCGameServerModule )
-    REGISTER_MODULE( pPluginManager, NFCSceneProcessModule )
-    REGISTER_MODULE( pPluginManager, NFCDataProcessModule )
-
-    REGISTER_MODULE( pPluginManager, NFCPropertyModule )
-    REGISTER_MODULE( pPluginManager, NFCLevelModule )
-    REGISTER_MODULE( pPluginManager, NFCPropertyConfigModule )
-
+    REGISTER_MODULE( pPluginManager, NFCSLGModule )
 }
 
-void NFGameServerPlugin::Uninstall()
+void NFSLGPlugin::Uninstall()
 {
-	
-    UNREGISTER_MODULE( pPluginManager, NFCPropertyConfigModule )
-    UNREGISTER_MODULE( pPluginManager, NFCLevelModule )
-    UNREGISTER_MODULE( pPluginManager, NFCPropertyModule )
-
-    UNREGISTER_MODULE( pPluginManager, NFCDataProcessModule )
-    UNREGISTER_MODULE( pPluginManager, NFCSceneProcessModule )
-    UNREGISTER_MODULE( pPluginManager, NFCGameServerModule )
+    UNREGISTER_MODULE( pPluginManager, NFCSLGModule )
 }
