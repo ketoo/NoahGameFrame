1511b392ccbd120d29d030e81e09b157664cbdff
eliteYang <elite_yang@163.com>
2014-09-29 14:18:35 +0800

    add SLG Server

--- NFServer/NFSLGPlugin/NFCSLGModule.cpp
+++ NFServer/NFSLGPlugin/NFCSLGModule.cpp
@@ -1,65 +1,38 @@
-// -------------------------------------------------------------------------
-//    @FileName      :    NFCGameServerModule.cpp
-//    @Author           :    LvSheng.Huang
-//    @Date             :    2013-01-02
-//    @Module           :    NFCGameServerModule
-//    @Desc             :
-// -------------------------------------------------------------------------
-
-////#include "stdafx.h"
-#include "NFGameServerPlugin.h"
-#include "NFCGameServerModule.h"
+#include "NFSLGPlugin.h"
+#include "NFCSLGModule.h"
 #include "NFComm/Config/NFConfig.h"
 
 
-bool NFCGameServerModule::Init()
+bool NFCSLGModule::Init()
 {
     return true;
 }
 
-bool NFCGameServerModule::Shut()
+bool NFCSLGModule::Shut()
 {
 
     return true;
 }
 
-bool NFCGameServerModule::Execute( const float fLasFrametime, const float fStartedTime )
+bool NFCSLGModule::Execute( const float fLasFrametime, const float fStartedTime )
 {
-#ifdef _DEBUG
-    /*
-    char szContent[MAX_PATH] = { 0 };
-    if (kbhit() && gets(szContent))
-    {
-        NFCDataList val(szContent, ",");
-        if (val.GetCount() > 0)
-        {
-            //const char* pstrCmd = val.String( 0 );
-            m_pKernelModule->Command(val);
-
-        }
-    }
-    */
-#endif
-
-
     return true;
 }
 
-bool NFCGameServerModule::AfterInit()
+bool NFCSLGModule::AfterInit()
 {
     m_pEventProcessModule = dynamic_cast<NFIEventProcessModule*>( pPluginManager->FindModule( "NFCEventProcessModule" ) );
     m_pKernelModule = dynamic_cast<NFIKernelModule*>( pPluginManager->FindModule( "NFCKernelModule" ) );
-    m_pLogicClassModule = dynamic_cast<NFILogicClassModule*>( pPluginManager->FindModule( "NFCLogicClassModule" ) );
+    m_pNoSqlModule = dynamic_cast<NFIDataNoSqlModule*>( pPluginManager->FindModule( "NFCDataNoSqlModule" ) );
 
     assert( NULL != m_pEventProcessModule );
     assert( NULL != m_pKernelModule );
-    assert( NULL != m_pLogicClassModule );
+    assert(NULL != m_pNoSqlModule);
 
     return true;
 }
 
-bool NFCGameServerModule::BeforeShut()
+bool NFCSLGModule::BeforeShut()
 {
-
     return true;
 }
