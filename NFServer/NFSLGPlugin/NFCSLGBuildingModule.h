06acec8b3be83d6f307b51309db6a5968d9bb233
eliteYang <elite_yang@163.com>
2014-09-30 17:05:03 +0800

    add building module
    Former-commit-id: 20d30592bea8161a3b986a978586cbc2897698ce [formerly c9ffd6584314d69a16026816d5868d6672000bb3]
    Former-commit-id: 76f14ae3177662dd4e17c03164bb65f5efbb48dd

--- NFServer/NFSLGPlugin/NFCSLGBuildingModule.h
+++ NFServer/NFSLGPlugin/NFCSLGBuildingModule.h
@@ -5,3 +5,28 @@
 //    @Module           £º    NFCSLGBuildingModule
 //
 // -------------------------------------------------------------------------
+#ifndef __NFC_SLG_BUILDING_MODULE_H__
+#define __NFC_SLG_BUILDING_MODULE_H__
+
+#include "NFISLGBuildingModule.h"
+
+class NFCSLGBuildingModule : public NFISLGBuildingModule
+{
+public:
+    NFCSLGBuildingModule(NFIPluginManager* p);
+    virtual ~NFCSLGBuildingModule(){}
+
+    virtual bool Init();
+    virtual bool AfterInit();
+    virtual bool CheckConfig();
+    virtual bool BeforeShut();
+    virtual bool Shut();
+    virtual bool Execute(const float fLasFrametime, const float fStartedTime);
+
+public:
+    virtual int AddBuilding(const NFIDENTID& self, const std::string& strBuilding);
+private:
+
+};
+
+#endif // !__NFC_SLG_BUILDING_MODULE_H__
