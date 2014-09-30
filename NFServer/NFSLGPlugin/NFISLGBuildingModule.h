31f548fe839b1f915f66afe08633d8171806df38
eliteYang <elite_yang@163.com>
2014-09-30 17:05:03 +0800

    add building module

--- NFServer/NFSLGPlugin/NFISLGBuildingModule.h
+++ NFServer/NFSLGPlugin/NFISLGBuildingModule.h
@@ -5,3 +5,16 @@
 //    @Module           £º    NFISLGBuildingModule
 //
 // -------------------------------------------------------------------------
+
+#ifndef __NFI_SLG_BUILDING_MODULE_H__
+#define __NFI_SLG_BUILDING_MODULE_H__
+
+#include "NFComm/NFPluginModule/NFILogicModule.h"
+
+class NFISLGBuildingModule : public NFILogicModule
+{
+public:
+    virtual int AddBuilding(const NFIDENTID& self, const std::string& strBuilding) = 0;
+};
+
+#endif // !__NFI_SLG_BUILDING_MODULE_H__
