e0635db354ebeb391b2530ff8d10cbf8e122a76e
eliteYang <elite_yang@163.com>
2014-10-08 10:13:07 +0800

    fix tool; new config

--- _Out/Server/NFDataCfg/Tool/main.py
+++ _Out/Server/NFDataCfg/Tool/main.py
@@ -3,6 +3,6 @@ from cx_Freeze import setup, Executable
 
 setup(
 name = "xls2xml",
-version = "0.1",
+version = "0.2",
 descrīption = "COPY (C) NoahGameFramework www.yowoyo.com",
 executables = [Executable("xls2xml.py", icon = "setting.ico")])
