9b9d45578ee7eaf5825529efb5a5216ea5e3744b
kytooooo <kytooooo>
2014-10-28 21:04:54 +0800

    modified protocol

--- NFComm/NFMessageDefine/NFMsgBase.pb.h
+++ NFComm/NFMessageDefine/NFMsgBase.pb.h
@@ -61,6 +61,7 @@ class ObjectRecordSwap;
 class ObjectRecordAddRow;
 class ObjectRecordRemove;
 class MsgBase;
+class Position;
 class ReqCommand;
 
 enum ReqCommand_EGameCommandType {
@@ -2805,6 +2806,108 @@ class MsgBase : public ::google::protobuf::Message {
 };
 // -------------------------------------------------------------------
 
+class Position : public ::google::protobuf::Message {
+ public:
+  Position();
+  virtual ~Position();
+
+  Position(const Position& from);
+
+  inline Position& operator=(const Position& from) {
+    CopyFrom(from);
+    return *this;
+  }
+
+  inline const ::google::protobuf::UnknownFieldSet& unknown_fields() const {
+    return _unknown_fields_;
+  }
+
+  inline ::google::protobuf::UnknownFieldSet* mutable_unknown_fields() {
+    return &_unknown_fields_;
+  }
+
+  static const ::google::protobuf::Descriptor* descriptor();
+  static const Position& default_instance();
+
+  void Swap(Position* other);
+
+  // implements Message ----------------------------------------------
+
+  Position* New() const;
+  void CopyFrom(const ::google::protobuf::Message& from);
+  void MergeFrom(const ::google::protobuf::Message& from);
+  void CopyFrom(const Position& from);
+  void MergeFrom(const Position& from);
+  void Clear();
+  bool IsInitialized() const;
+
+  int ByteSize() const;
+  bool MergePartialFromCodedStream(
+      ::google::protobuf::io::CodedInputStream* input);
+  void SerializeWithCachedSizes(
+      ::google::protobuf::io::CodedOutputStream* output) const;
+  ::google::protobuf::uint8* SerializeWithCachedSizesToArray(::google::protobuf::uint8* output) const;
+  int GetCachedSize() const { return _cached_size_; }
+  private:
+  void SharedCtor();
+  void SharedDtor();
+  void SetCachedSize(int size) const;
+  public:
+
+  ::google::protobuf::Metadata GetMetadata() const;
+
+  // nested types ----------------------------------------------------
+
+  // accessors -------------------------------------------------------
+
+  // required float x = 1;
+  inline bool has_x() const;
+  inline void clear_x();
+  static const int kXFieldNumber = 1;
+  inline float x() const;
+  inline void set_x(float value);
+
+  // required float y = 2;
+  inline bool has_y() const;
+  inline void clear_y();
+  static const int kYFieldNumber = 2;
+  inline float y() const;
+  inline void set_y(float value);
+
+  // required float z = 3;
+  inline bool has_z() const;
+  inline void clear_z();
+  static const int kZFieldNumber = 3;
+  inline float z() const;
+  inline void set_z(float value);
+
+  // @@protoc_insertion_point(class_scope:NFMsg.Position)
+ private:
+  inline void set_has_x();
+  inline void clear_has_x();
+  inline void set_has_y();
+  inline void clear_has_y();
+  inline void set_has_z();
+  inline void clear_has_z();
+
+  ::google::protobuf::UnknownFieldSet _unknown_fields_;
+
+  float x_;
+  float y_;
+  float z_;
+
+  mutable int _cached_size_;
+  ::google::protobuf::uint32 _has_bits_[(3 + 31) / 32];
+
+  friend void  protobuf_AddDesc_NFMsgBase_2eproto();
+  friend void protobuf_AssignDesc_NFMsgBase_2eproto();
+  friend void protobuf_ShutdownFile_NFMsgBase_2eproto();
+
+  void InitAsDefaultInstance();
+  static Position* default_instance_;
+};
+// -------------------------------------------------------------------
+
 class ReqCommand : public ::google::protobuf::Message {
  public:
   ReqCommand();
@@ -5426,6 +5529,76 @@ MsgBase::player_fd_list() const {
 inline ::google::protobuf::RepeatedField< ::google::protobuf::int64 >*
 MsgBase::mutable_player_fd_list() {
   return &player_fd_list_;
+}
+
+// -------------------------------------------------------------------
+
+// Position
+
+// required float x = 1;
+inline bool Position::has_x() const {
+  return (_has_bits_[0] & 0x00000001u) != 0;
+}
+inline void Position::set_has_x() {
+  _has_bits_[0] |= 0x00000001u;
+}
+inline void Position::clear_has_x() {
+  _has_bits_[0] &= ~0x00000001u;
+}
+inline void Position::clear_x() {
+  x_ = 0;
+  clear_has_x();
+}
+inline float Position::x() const {
+  return x_;
+}
+inline void Position::set_x(float value) {
+  set_has_x();
+  x_ = value;
+}
+
+// required float y = 2;
+inline bool Position::has_y() const {
+  return (_has_bits_[0] & 0x00000002u) != 0;
+}
+inline void Position::set_has_y() {
+  _has_bits_[0] |= 0x00000002u;
+}
+inline void Position::clear_has_y() {
+  _has_bits_[0] &= ~0x00000002u;
+}
+inline void Position::clear_y() {
+  y_ = 0;
+  clear_has_y();
+}
+inline float Position::y() const {
+  return y_;
+}
+inline void Position::set_y(float value) {
+  set_has_y();
+  y_ = value;
+}
+
+// required float z = 3;
+inline bool Position::has_z() const {
+  return (_has_bits_[0] & 0x00000004u) != 0;
+}
+inline void Position::set_has_z() {
+  _has_bits_[0] |= 0x00000004u;
+}
+inline void Position::clear_has_z() {
+  _has_bits_[0] &= ~0x00000004u;
+}
+inline void Position::clear_z() {
+  z_ = 0;
+  clear_has_z();
+}
+inline float Position::z() const {
+  return z_;
+}
+inline void Position::set_z(float value) {
+  set_has_z();
+  z_ = value;
 }
 
 // -------------------------------------------------------------------
