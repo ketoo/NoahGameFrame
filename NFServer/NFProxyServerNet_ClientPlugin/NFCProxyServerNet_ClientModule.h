1e17474bd943dc172f6987ecec407373c99efe35
eliteYang <elite_yang@163.com>
2014-11-13 20:56:07 +0800

    fix offline between proxy and gameserver

--- NFServer/NFProxyServerNet_ClientPlugin/NFCProxyServerNet_ClientModule.h
+++ NFServer/NFProxyServerNet_ClientPlugin/NFCProxyServerNet_ClientModule.h
@@ -38,6 +38,7 @@ protected:
 
     int OnRecivePack(const NFIPacket& msg);
     int OnSocketEvent(const int nSockIndex, const NF_NET_EVENT eEvent);
+    void KeepAlive(float fLasFrametime);
 
     //连接丢失,删2层(连接对象，帐号对象)
     void OnClientDisconnect(const int nAddress);
@@ -50,6 +51,7 @@ private:
 
     int mnGameServerID;
     int mnSocketFD;
+    float mfLastHBTime;
 
     NFIElementInfoModule* m_pElementInfoModule;
     NFILogModule* m_pLogModule;
