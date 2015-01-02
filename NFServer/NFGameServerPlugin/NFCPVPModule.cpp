94d80487a9831acc225587c597d477b2ac761387
kytooooo <kytooooo>
2015-01-02 22:18:17 +0800

    add pvp module
    Former-commit-id: b247ada3718532104e34aa236133971f352f6b2d [formerly 26999f3f52f514551e0032ae4e8e93c653c83a48]
    Former-commit-id: 93b47b9a954ac7a1323d72cc294c36352870f40d

---
+++ NFServer/NFGameServerPlugin/NFCPVPModule.cpp
@@ -0,0 +1,60 @@
+// -------------------------------------------------------------------------
+//    @FileName      :    NFCPVPModule.cpp
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2015-01-02
+//    @Module           :    NFCPVPModule
+//
+// -------------------------------------------------------------------------
+
+#include "NFCPVPModule.h"
+
+bool NFCPVPModule::Init()
+{
+
+
+    return true;
+}
+
+
+bool NFCPVPModule::Shut()
+{
+    return true;
+}
+
+bool NFCPVPModule::Execute( const float fLasFrametime, const float fStartedTime )
+{
+    //位置呢
+    return true;
+}
+
+bool NFCPVPModule::AfterInit()
+{
+    m_pEventProcessModule = dynamic_cast<NFIEventProcessModule*>( pPluginManager->FindModule( "NFCEventProcessModule" ) );
+    m_pKernelModule = dynamic_cast<NFIKernelModule*>( pPluginManager->FindModule( "NFCKernelModule" ) );
+    m_pLogModule = dynamic_cast<NFILogModule*>( pPluginManager->FindModule( "NFCLogModule" ) );
+
+    assert( NULL != m_pEventProcessModule );
+    assert( NULL != m_pKernelModule );
+    assert( NULL != m_pLogModule );
+
+
+    return true;
+}
+
+bool NFCPVPModule::MatchPVPObject( const NFIDENTID& self )
+{
+	//应记录玩家处于PVP筹备状态
+	return true;
+}
+
+bool NFCPVPModule::StartPVPWar( const NFIDENTID& self )
+{
+	//应记录玩家处于PVP状态
+	return true;
+}
+
+bool NFCPVPModule::ExitPVPWar( const NFIDENTID& self )
+{
+	//还原玩家为自由状态
+	return true;
+}
