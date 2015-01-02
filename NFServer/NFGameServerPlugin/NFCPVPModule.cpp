3720ed12986a12328b0f0a2f3b7c765166a75f17
kytooooo <kytooooo>
2015-01-02 22:45:12 +0800

    add logic state property

--- NFServer/NFGameServerPlugin/NFCPVPModule.cpp
+++ NFServer/NFGameServerPlugin/NFCPVPModule.cpp
@@ -44,17 +44,37 @@ bool NFCPVPModule::AfterInit()
 bool NFCPVPModule::MatchPVPObject( const NFIDENTID& self )
 {
 	//应记录玩家处于PVP筹备状态
+	const NFLogicStateType eLogicState = m_pKernelModule->GetPropertyInt(self, "LogicState");
+	if (eLogicState != NLST_FREE)
+	{
+		return false;
+	}
+
 	return true;
 }
 
 bool NFCPVPModule::StartPVPWar( const NFIDENTID& self )
 {
-	//应记录玩家处于PVP状态
+	//应记录玩家处于PVP状态，或者说模块独占状态
+	const NFLogicStateType eLogicState = m_pKernelModule->GetPropertyInt(self, "LogicState");
+	if (eLogicState != NLST_PVP)
+	{
+		return false;
+	}
+
 	return true;
 }
 
 bool NFCPVPModule::ExitPVPWar( const NFIDENTID& self )
 {
 	//还原玩家为自由状态
-	return true;
+	const NFLogicStateType eLogicState = m_pKernelModule->GetPropertyInt(self, "LogicState");
+	if (eLogicState == NLST_PVP)
+	{
+		m_pKernelModule->SetPropertyInt(self, "LogicState", NLST_FREE);
+	}
+
+	//再有其他的事情，或者事件回调
+
+	return false;
 }
