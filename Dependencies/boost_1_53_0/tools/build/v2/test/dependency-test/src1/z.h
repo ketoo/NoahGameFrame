5d36b6c9d0684be2848283be5773337b273b727d
kytoo <342006@qq.com>
2015-02-02 13:51:52 +0800

    delete unused file

--- Dependencies/boost_1_53_0/tools/build/v2/test/dependency-test/src1/z.h
+++
@@ -1,5 +0,0 @@
-/* Copyright 2003, 2004, 2006 Vladimir Prus */
-/* Distributed under the Boost Software License, Version 1.0. */
-/* (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt) */
-
-extern int dummy_variabled_need_to_suppress_empty_file_warning_on_hp_cxx_compiler;
