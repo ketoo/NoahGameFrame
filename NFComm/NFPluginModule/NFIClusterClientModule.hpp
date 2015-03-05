d69e697148409e0e335e7e006e6a267377b979c4
kytoo <342006@qq.com>
2015-03-05 16:32:28 +0800

    FIXED CLUSTER CLIENT OF NET

--- NFComm/NFPluginModule/NFIClusterClientModule.hpp
+++ NFComm/NFPluginModule/NFIClusterClientModule.hpp
@@ -21,7 +21,6 @@ struct ServerData
 		nPort = 0;
 		strName = "";
 		strIP = "";
-		m_pNetModule = NULL;
 		eServerType = NFST_NONE;
 		eState = NFMsg::EServerState::EST_CRASH;
 	}
@@ -32,19 +31,21 @@ struct ServerData
 	int nPort;
 	std::string strName;
 	NFMsg::EServerState eState;
-	NFINetModule* m_pNetModule;
+	NF_SHARE_PTR<NFINetModule> mxNetModule;
 
 };
 
 class NFIClusterClientModule
 {
 public:
+	virtual void OnNetCreated(NF_SHARE_PTR<ServerData> xServerData) = 0;
+
 	virtual bool Execute(const float fLastFrametime, const float fStartedTime)
 	{
 		ServerData* pServerData = mxServerMap.First();
 		while (pServerData)
 		{
-			pServerData->m_pNetModule.Execute(fLastFrametime, fStartedTime);
+			pServerData->mxNetModule->Execute(fLastFrametime, fStartedTime);
 
 			pServerData = mxServerMap.Next();
 		}
@@ -55,29 +56,41 @@ public:
 
 	void AddServer(const ServerData& xInfo)
 	{
-		ServerData* pServerData = mxServerMap.find(xInfo.nGameID);
-		if (pServerData)
+		NF_SHARE_PTR<ServerData> xServerData = mxServerMap.find(xInfo.nGameID);
+		if (xServerData)
 		{
-			if (EServerState.EST_MAINTEN == pServerData->eState
-				|| EServerState.EST_CRASH == pServerData->eState)
+			if (EServerState.EST_MAINTEN == xServerData->eState
+				|| EServerState.EST_CRASH == xServerData->eState)
 			{
-				//宕了
+				//宕了，进入重连队列
 			}
 			else
 			{
-
+				//正常，修改详细状态
 			}
 		}
 		else
 		{
-			if (EServerState.EST_MAINTEN == pServerData->eState
-				|| EServerState.EST_CRASH == pServerData->eState)
+			if (EServerState.EST_MAINTEN == xInfo->eState
+				|| EServerState.EST_CRASH == xInfo->eState)
 			{
 				//宕了
 			}
 			else
 			{
+				//正常，添加新服务器
+				xServerData = NF_SHARE_PTR<ServerData>(NF_NEW ServerData());
 
+				xServerData->nGameID = xInfo.nGameID;
+				xServerData->eServerType = xInfo->eState;
+				xServerData->strIP = xInfo.strIP;
+				xServerData->strName = xInfo.strName;
+				xServerData->eState = xInfo.eState;
+				xServerData->mxNetModule = NF_SHARE_PTR<NFINetModule>(NF_NEW NFINetModule());
+				OnNetCreated(xServerData);
+				//xServerData->m_pNetModule->Initialization(NFIMsgHead::NF_Head::NF_HEAD_LENGTH, this, &NFCGameServerNet_ServerModule::OnRecivePack, &NFCGameServerNet_ServerModule::OnSocketEvent, nMaxConnect, nPort, nCpus);
+					
+				mxServerMap.AddElement(xInfo.nGameID, xServerData);
 			}
 		}
 	}
@@ -94,5 +107,5 @@ public:
 
 protected:
 private:
-	NFMap<int, ServerData> mxServerMap;
+	NFMapEx<int, ServerData> mxServerMap;
 };
