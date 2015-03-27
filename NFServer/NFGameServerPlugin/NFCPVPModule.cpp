7b7de2852d04fb52e9c7358c01dc6846873de7c4
eliteYang <elite_yang@163.com>
2015-03-27 23:48:22 +0800

    add introduction

--- NFServer/NFGameServerPlugin/NFCPVPModule.cpp
+++ NFServer/NFGameServerPlugin/NFCPVPModule.cpp
@@ -60,6 +60,21 @@ bool NFCPVPModule::StartPVPWar( const NFIDENTID& self )
 		return false;
 	}
 
+    m_pKernelModule->SetPropertyInt(self, "LogicState", (NFLogicStateType)NLST_PVP); // 进入PVP状态
+    mxPVPList.Add(self);
+
+    if (mxPVPList.Count() < PVPCell::PVPCell_Player_Count)
+    {
+        return true;
+    }
+    
+    //record
+    //switch
+
+    //mxPVPList remove
+
+    // TODO:发送次玩家的信息给其他玩家，发送其他玩家的信息给此玩家
+
 	return true;
 }
 
@@ -72,7 +87,8 @@ bool NFCPVPModule::ExitPVPWar( const NFIDENTID& self )
 		m_pKernelModule->SetPropertyInt(self, "LogicState", NLST_FREE);
 	}
 
-	//再有其他的事情，或者事件回调
+	// 移除该玩家
+    mxPVPList.Remove(self);
 
 	return false;
 }
