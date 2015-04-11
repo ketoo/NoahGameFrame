a83d2bd75e5199bfa2ccc0aaa597c5915df86b2a
kytoo <342006@qq.com>
2015-04-11 17:30:09 +0800

    commit actor

---
+++ NFComm/NFPluginLoader/NFCActor.h
@@ -0,0 +1,33 @@
+// -------------------------------------------------------------------------
+//    @FileName      :    Actor.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2014-04-05
+//    @Module           :    Actor
+//
+// -------------------------------------------------------------------------
+
+#ifndef _NFC_ACTOR_H_
+#define _NFC_ACTOR_H_
+
+#include <map>
+#include <string>
+#include "NFComm/NFCore/NFSingleton.h"
+#include "NFComm/NFPluginModule/NFPlatform.h"
+
+#ifdef NF_USE_ACTOR
+
+#include "NFComm/NFPluginModule/NFIActor.h"
+
+class NFCActor
+	: public NFIActor
+{
+public:
+	NFCActor(Theron::Framework &framework, NFIActorManager* pManager) : NFIActor(framework, pManager)
+	{
+
+	}
+
+};
+#endif
+
+#endif
