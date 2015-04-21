337fdfb9f4f3ed617f5ce927e043f2ec6df3ce29
kytoo <342006@qq.com>
2015-04-21 23:06:56 +0800

    add load and save function for player

--- NFComm/NFPluginModule/NFIClusterModule.h
+++ NFComm/NFPluginModule/NFIClusterModule.h
@@ -10,7 +10,6 @@
 #ifndef _NFI_CLUSTER_MODULE_H_
 #define _NFI_CLUSTER_MODULE_H_
 
-#include "NFCMysqlDriver.h"
 #include "NFComm/NFPluginModule/NFPlatform.h"
 #include "NFComm/NFPluginModule/NFIPluginManager.h"
 
@@ -20,23 +19,14 @@ class NFIClusterModule
 public:
 
 
-	virtual bool Updata(const std::string& strKey, const std::vector<std::string>& fieldVec, const std::vector<std::string>& valueVec);
-	virtual bool Query(const std::string& strKey, const std::vector<std::string>& fieldVec, std::vector<std::string>& valueVec);
+	virtual bool Updata(const std::string& strKey, const std::vector<std::string>& fieldVec, const std::vector<std::string>& valueVec) = 0;
+	virtual bool Query(const std::string& strKey, const std::vector<std::string>& fieldVec, std::vector<std::string>& valueVec) = 0;
 	//virtual bool Select(const std::string& strKey, const std::vector<std::string>& fieldVec, std::vector<std::string>& valueVec);
 
-	virtual bool Delete(const std::string& strKey);
-	virtual bool Exists(const std::string& strKey, bool& bExit);
+	virtual bool Delete(const std::string& strKey) = 0;
+	virtual bool Exists(const std::string& strKey, bool& bExit) = 0;
 
 private:
-
-    NFCMysqlDriver* m_pDataBaseDriver;
-
-  
-    otl_connect motlConnect;
-
-    std::string mstrMasterName;
-    std::string mstrUID;
-    std::string mstrPWD;
 
 };
 
