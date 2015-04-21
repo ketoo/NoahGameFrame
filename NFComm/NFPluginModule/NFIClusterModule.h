5f179817b9939416bf1c279e230a5427fe388176
kytoo <342006@qq.com>
2015-04-21 23:14:16 +0800

    modified interface
    Former-commit-id: e9dea25317070544f54a4537a95422816a2ca5f9 [formerly 959a1e80525491b0397c317ab434dadd5df48b62]
    Former-commit-id: ec701a9a15f06c731491c21599662163b57d9242

--- NFComm/NFPluginModule/NFIClusterModule.h
+++ NFComm/NFPluginModule/NFIClusterModule.h
@@ -1,38 +1,24 @@
 // -------------------------------------------------------------------------
-//    @FileName      :    NFCDataBaseModule.h
+//    @FileName      :    NFIClusterModule.h
 //    @Author           :    LvSheng.Huang
 //    @Date             :    2012-12-15
-//    @Module           :    NFCDataBaseModule
+//    @Module           :    NFIClusterModule
 //
-//    数据格式
-//      Property:   HP,1;Mp,2;Exp,3;
-//          属性,属性值;属性,属性值;属性,属性值;属性,属性值;
-//
-//      Record:     Friends,20,5;1,2,3,4,5;11,22,33,44,55;
-//          表名,表行,表列;行1的值(行列的个数);行2的值(行列的个数);
 //
 // -------------------------------------------------------------------------
 
-#ifndef _NFC_DATABASE_MODULE_H_
-#define _NFC_DATABASE_MODULE_H_
+#ifndef _NFI_CLUSTER_MODULE_H_
+#define _NFI_CLUSTER_MODULE_H_
 
 #include "NFCMysqlDriver.h"
 #include "NFComm/NFPluginModule/NFPlatform.h"
 #include "NFComm/NFPluginModule/NFIPluginManager.h"
-#include "NFComm/NFPluginModule/NFIClusterModule.h"
 
-class NFCMysqlClusterModule
-    : public NFIClusterModule
+class NFIClusterModule
+    : public NFILogicModule
 {
 public:
 
-    NFCMysqlClusterModule(NFIPluginManager* p);
-    virtual ~NFCMysqlClusterModule();
-
-    virtual bool Init();
-    virtual bool Shut();
-    virtual bool Execute(const float fLasFrametime, const float fStartedTime);
-    virtual bool AfterInit();
 
 	virtual bool Updata(const std::string& strKey, const std::vector<std::string>& fieldVec, const std::vector<std::string>& valueVec);
 	virtual bool Query(const std::string& strKey, const std::vector<std::string>& fieldVec, std::vector<std::string>& valueVec);
@@ -44,6 +30,9 @@ public:
 private:
 
     NFCMysqlDriver* m_pDataBaseDriver;
+
+  
+    otl_connect motlConnect;
 
     std::string mstrMasterName;
     std::string mstrUID;
