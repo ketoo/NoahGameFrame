576d84ad99b3958d30632d9326bd24f2c2cabbc6
eliteYang <elite_yang@163.com>
2015-04-25 23:37:10 +0800

    add mysqlConnectMgrModule

--- NFComm/NFCluster/NFMysqlClusterPlugin.cpp
+++ NFComm/NFCluster/NFMysqlClusterPlugin.cpp
@@ -1,6 +1,7 @@
 #include "NFMysqlClusterPlugin.h"
 #include "NFCMysqlClusterModule.h"
 #include "NFComm/NFPluginModule/NFIPluginManager.h"
+#include "NFCMysqlConnectMgrModule.h"
 
 #ifdef NF_DYNAMIC_PLUGIN
 
@@ -33,9 +34,11 @@ const std::string NFMysqlClusterPlugin::GetPluginName()
 void NFMysqlClusterPlugin::Install()
 {
     REGISTER_MODULE(pPluginManager, NFCMysqlClusterModule)
+    REGISTER_MODULE(pPluginManager, NFCMysqlConnectMgrModule)
 }
 
 void NFMysqlClusterPlugin::Uninstall()
 {
+    UNREGISTER_MODULE(pPluginManager, NFCMysqlConnectMgrModule)
     UNREGISTER_MODULE(pPluginManager, NFCMysqlClusterModule)
 }
