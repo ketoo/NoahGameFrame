5c5f600b2d19206837e9735b070c25d42942d2b7
eliteYang <elite_yang@163.com>
2015-04-25 23:37:10 +0800

    add mysqlConnectMgrModule

--- NFComm/NFPluginModule/NFIMysqlDriver.h
+++ NFComm/NFPluginModule/NFIMysqlDriver.h
@@ -1,74 +1,61 @@
 // -------------------------------------------------------------------------
-//    @FileName      :    NFCDataBaseDriver.h
-//    @Author           :    LvSheng.Huang
-//    @Date             :    2010-12-19 11:05
-//    @Module           :    NFCDataBaseDriver
-//
+//    @FileName         :    NFIMysqlDriver.h
+//    @Author           :    chuano.guo
+//    @Date             :    2014-07-28
+//    @Module           :    NFIMysqlDriver
+//    @Desc             :
 // -------------------------------------------------------------------------
+#ifndef __NFI_MYSQL_DRIVER_MODULE_H__
+#define __NFI_MYSQL_DRIVER_MODULE_H__
+//#define MS_HIREDIS
+#ifdef _MSC_VER 
+#include <windows.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <stdint.h>
+#pragma comment(lib, "ws2_32.lib")
 
-#ifndef _NFC_DATABASEDRIVER_H_
-#define _NFC_DATABASEDRIVER_H_
+#else
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+//#include <sys/timeb.h>
+#include <sys/types.h>
 
-#include <iostream>
-#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <map>
-#include <list>
-#include "NFComm/NFPluginModule/NFPlatform.h"
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#endif
 
-#define OTL_ODBC_MYSQL // 编译 OTL 4.0/ODBC
-#define OTL_BIGINT unsigned __int64
-#include "otlv4.h" // 包含 OTL 4.0 头文件
+#include <string>
+#include <vector>
+#include <set>
+#include "mysqlpp/mysql++.h"
 
-using namespace std;
-
-class  NFIMysqlDriver
+class NFIMysqlDriver
 {
 public:
+   virtual bool Connect(const std::string& strDBName, const std::string& strDBHost, const int nDBPort, const std::string& strDBUser, const std::string& strDBPwd) = 0;
+   virtual  bool Execute(const float fLasFrametime, const float fStartedTime) = 0;
+   virtual mysqlpp::Connection* GetConnection() = 0;
+   virtual bool Query(const std::string& qstr, mysqlpp::StoreQueryResult& queryResult) = 0;
+   virtual bool Query(const std::string& qstr) = 0;
 
-    //--------------------------------------------------------------------------
-    // 函数名称：
-    //      OTLConnect
-    // 参数：
-    //      const std::string& strUID
-    //      const std::string& strPWD
-    //      const std::string& strDSN
-    //      otl_connect& otlDB
-    // 返回：
-    //      int                 : 目前没用
-    // 说明：
-    //      连接DB
-    //---------------------------------------------------------------------------
-    virtual int OTLConnect(const std::string& strUID, const std::string& strPWD, const std::string& strDSN, otl_connect& otlDB) = 0;
-
-    //--------------------------------------------------------------------------
-    // 函数名称：
-    //      OTLDisconnect
-    // 参数：
-    //      otl_connect& otlDB
-    // 返回：
-    //      int                 : 目前没用
-    // 说明：
-    //      断开DB
-    //---------------------------------------------------------------------------
-    virtual int OTLDisconnect(otl_connect& otlDB) = 0;
-
-    //--------------------------------------------------------------------------
-    // 函数名称：
-    //      OTLDisconnect
-    // 参数：
-    //      const char* pstrSqlInfo
-    //      otl_connect& otlDB
-    // 返回：
-    //      int                     : 目前没用
-    // 说明：
-    //      执行SQL语句,只有命令性质的insert updata才能用这个
-    //---------------------------------------------------------------------------
-    virtual int OTLExec(const char* pstrSqlInfo, otl_connect& otlDB) = 0;
-protected:
-
-private:
-
+   virtual bool Enable() = 0;
+   virtual bool Reconnect() = 0;
+   virtual bool CanReconnect(const float fLasFrametime) = 0;
 };
 
-#endif
+#endif // !__NFI_REDIS_DRIVER_MODULE_H__
