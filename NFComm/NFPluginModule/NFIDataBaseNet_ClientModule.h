b63641354e08cefbd69946245591f169e87b2a25
eliteYang <elite_yang@163.com>
2015-04-25 17:18:05 +0800

    fix Database compile error

---
+++ NFComm/NFPluginModule/NFIDataBaseNet_ClientModule.h
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFIDataBaseNet_ClientModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2013-01-02
+//    @Module           :    NFIDataBaseNet_ClientModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+#ifndef _NFI_DATABASENET_CLIENT_MODULE_H_
+#define _NFI_DATABASENET_CLIENT_MODULE_H_
+
+#include "NFComm/NFPluginModule/NFILogicModule.h"
+
+class NFIDataBaseNet_ClientModule : public NFILogicModule
+{
+public:
+
+};
+
+#endif
--- NFServer/NFDataBaseNet_ClientPlugin/NFCDataBaseNet_ClientModule.h
+++ NFServer/NFDataBaseNet_ClientPlugin/NFCDataBaseNet_ClientModule.h
@@ -9,22 +9,19 @@
 #ifndef _NFC_DATABASENET_CLIENT_MODULE_H_
 #define _NFC_DATABASENET_CLIENT_MODULE_H_
 
-//  the cause of sock'libariy, thenfore "NFCNet.h" much be included first.
 #include "NFComm/NFMessageDefine/NFMsgDefine.h"
-#include "NFComm/NFMessageDefine/NFSvrMsgType.h"
-#include "NFComm/NFMessageDefine/NFClientMsgType.h"
-#include "NFComm/NFPluginModule/NFIShareMemoryModule.h"
 #include "NFComm/NFPluginModule/NFILogicClassModule.h"
 #include "NFComm/NFPluginModule/NFIKernelModule.h"
-#include "NFComm/NFPluginModule/NFIDataBaseModule.h"
-#include "NFComm/NFCore/GTPropertyManager.h"
-#include "NFComm/NFCore/GTRecordManager.h"
+#include "NFComm/NFPluginModule/NFIDataBaseNet_ClientModule.h"
 
-class NFCDataBaseNet_ClientModule
-    : public NFILogicModule,
-  public NFCNet
+class NFCDataBaseNet_ClientModule : public NFIDataBaseNet_ClientModule
 {
 public:
+    NFCDataBaseNet_ClientModule( NFIPluginManager* p )
+    {
+        pPluginManager = p;
+    }
+
     virtual bool Init();
     virtual bool Shut();
     virtual bool Execute(const float fLasFrametime, const float fStartedTime);
@@ -32,42 +29,9 @@ public:
     virtual bool AfterInit();
     virtual bool BeforeShut();
 
-    virtual  bool DispatcherSystemEvents(RakNet::MessageID ucEventID, RakNet::SystemAddress sAddress);
-    virtual  bool DispatcherPacket(NFIPacket* pMsgPacket);
-    virtual  bool DispatcherPacket(RakNet::Packet* pPacket, const std::string& strMsg)
-    {
-        return true;
-    }
-protected:
-
-    void  CreateShareMemory();
-
-    void LoadRoleDataProcess(NFIPacket* pMsgPacket);
-    void SaveRoleDataProcess(NFIPacket* pMsgPacket);
-
-    void Register();
-    void UnRegister();
-
-protected:
-    int LoadRoleData(const std::string& strRoleName);
-
-    int LoadRoleDataProperty(const std::string& strRoleName);
-    int LoadRoleDataRecord(const std::string& strRoleName);
-
-    bool SaveRoleData(const std::string& strRoleName);
-
-
 private:
-
-    static int mnDataBaseID;
-    static char* mstrsShareName;
-
-    static NFIShareMemoryModule* m_pShareMemoryModule;
-    static NFILogicClassModule* m_pLogicClassModule;
-    static NFIDataBaseModule* m_pDataBaseModule;
-    static NFIKernelModule* m_pKernelModule;
-
-    static NFCDataBaseNet_ClientModule* m_pThis;
+    NFILogicClassModule* m_pLogicClassModule;
+    NFIKernelModule* m_pKernelModule;
 };
 
 #endif
