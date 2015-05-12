772f7d2b85b157a96e06b956869d6cc5b1b3c1b8
kytoo <342006@qq.com>
2015-05-12 23:15:44 +0800

    modified function name

--- NFServer/NFWorldNet_ClientPlugin/NFCWorldToMasterModule.h
+++ NFServer/NFWorldNet_ClientPlugin/NFCWorldToMasterModule.h
@@ -40,8 +40,8 @@ public:
 
 protected:
 
-	int OnRecivePack(const NFIPacket& msg);
-	int OnSocketEvent(const int nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet);
+	int OnReciveMSPack(const NFIPacket& msg);
+	int OnSocketMSEvent(const int nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet);
 
 	//连接丢失,删2层(连接对象，帐号对象)
 	void OnClientDisconnect(const int nAddress);
