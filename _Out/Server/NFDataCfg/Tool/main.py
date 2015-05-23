711b29d5fbfc74dc4aeca96e4af357115e75d4ab
eliteYang <elite_yang@163.com>
2015-05-23 15:31:43 +0800

    add FileProcess tool
    Former-commit-id: 63b92444d33a81eb2e9dd0a83cfa59d93d177178 [formerly da77509a7e346cd9b95b2fff47a358809252a60a]
    Former-commit-id: 26fd993286069354abf29baab7e9641a19e84698

--- _Out/Server/NFDataCfg/Tool/main.py
+++ _Out/Server/NFDataCfg/Tool/main.py
@@ -4,5 +4,5 @@ from cx_Freeze import setup, Executable
 setup(
 name = "xls2xml",
 version = "0.2",
-descrīption = "COPY (C) NoahGameFramework www.yowoyo.com",
+description = "COPY (C) NFrame Stuido www.yowoyo.com",
 executables = [Executable("xls2xml.py", icon = "setting.ico")])
