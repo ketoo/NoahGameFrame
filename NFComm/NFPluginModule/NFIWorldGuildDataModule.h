f5f83d3472897e3fd9e8d03cae8fe32b2031d6a3
flyicegood <flyicegood@163.com>
2015-06-06 18:32:46 +0800

    add guild data module

---
+++ NFComm/NFPluginModule/NFIWorldGuildDataModule.h
@@ -0,0 +1,29 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFIWorldGuildDataModule.h
+//    @Author           :    flyicegood
+//    @Date             :    2015-06-06 16:51
+//    @Module           :    NFIWorldGuildDataModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef _NFI_WORLD_GUILD_DATA_MODULE_H_
+#define _NFI_WORLD_GUILD_DATA_MODULE_H_
+
+#include "NFILogicModule.h"
+
+class NFIWorldGuildDataModule
+    : public NFILogicModule
+{
+public:
+
+	virtual bool ExitGuild(const NFIDENTID& self, const std::string& strName, bool& bExit) = 0;
+    virtual void CheckLoadGuild(const NFIDENTID& self, const NFIDENTID& xGuild) = 0;
+
+    virtual const NFIDENTID CreateGuild(const NFIDENTID& xPlayeID, const std::string& strName) = 0;
+    virtual const bool DeleteGuild(const NFIDENTID& xGuild) = 0;
+
+protected:
+private:
+};
+
+#endif
---
+++ NFServer/NFWorldGuildPlugin/NFCWorldGuildDataModule.h
@@ -0,0 +1,79 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFCWorldGuildDataModule.h
+//    @Author           :    chuanbo.guo
+//    @Date             :    2015-06-06 16:51
+//    @Module           :    NFCWorldGuildDataModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef _NFC_WORLD_GUILD_DATA_MODULE_H_
+#define _NFC_WORLD_GUILD_DATA_MODULE_H_
+
+#include "NFComm/NFCore/NFMap.h"
+#include "NFComm/NFPluginModule/NFIKernelModule.h"
+#include "NFComm/NFPluginModule/NFIWorldGuildDataModule.h"
+#include "NFComm/NFPluginModule/NFIEventProcessModule.h"
+#include "NFComm/NFPluginModule/NFIUUIDModule.h"
+#include "NFComm/NFPluginModule/NFIClusterModule.h"
+#include "NFComm/NFPluginModule/NFIGameLogicModule.h"
+
+class NFCWorldGuildDataModule
+    : public NFIWorldGuildDataModule
+{
+public:
+    NFCWorldGuildDataModule(NFIPluginManager* p)
+    {
+        pPluginManager = p;
+
+        mstrGuildTalble = "GuildInfo";
+        mstrGuildNameTalble = "GuildName";
+    }
+
+    virtual bool Init();
+    virtual bool Shut();
+    virtual bool Execute(const float fLasFrametime, const float fStartedTime);
+
+    virtual bool AfterInit();
+
+    virtual bool ExitGuild(const NFIDENTID& self, const std::string& strName, bool& bExit);
+    virtual void  CheckLoadGuild(const NFIDENTID& self, const NFIDENTID& xGuild);
+
+    virtual const NFIDENTID CreateGuild(const NFIDENTID& xPlayeID, const std::string& strName);
+    virtual const bool DeleteGuild(const NFIDENTID& xGuild);
+
+protected:
+    int OnGuildClassEvent( const NFIDENTID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& var );
+
+protected:
+   const bool LoadDataFormNoSql( const NFIDENTID& self );
+   const bool SaveDataToNoSql( const NFIDENTID& self);
+   const bool AttachData( const NFIDENTID& self );
+
+protected:
+    NFIEventProcessModule* m_pEventProcessModule;
+    NFIKernelModule* m_pKernelModule;
+    NFIUUIDModule* m_pUUIDModule;
+    NFIClusterModule* m_pClusterSQLModule;
+    NFIGameLogicModule* m_pGameLogicModule;
+    
+private:
+    struct RoleData 
+    {
+        RoleData()
+        {
+            strProperty = "";
+            strRecord = "";
+        }
+
+        std::string strProperty;
+        std::string strRecord;
+    };
+
+    NFMapEx<std::string, NFIDENTID> mmGuidMap; // GuidName<----> GuidID;
+    NFMapEx<NFIDENTID, RoleData> mxRoleDataMap;
+
+    std::string mstrGuildTalble;
+    std::string mstrGuildNameTalble;
+};
+
+#endif
