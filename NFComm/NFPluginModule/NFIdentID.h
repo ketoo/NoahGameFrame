1f77355403058ca0c84c0e6f44326ed543acc816
eliteYang <362148418@qq.com>
2015-06-08 22:06:47 +0800

    fix compile error in linux

--- NFComm/NFPluginModule/NFIdentID.h
+++ NFComm/NFPluginModule/NFIdentID.h
@@ -9,64 +9,56 @@
 #ifndef _NF_IDENTID_H_
 #define _NF_IDENTID_H_
 
-#include "NFComm/NFPluginModule/NFPlatform.h"
+#include "NFPlatform.h"
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <boost/lexical_cast.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
-//#pragma pack(push, 1)
 
 struct NFIDENTID
 {
-	NFINT64 nHead64;
-	NFINT64 nData64;
-    //boost::multiprecision::int128_t xID;
-
+    NFINT64 nData64;
+    NFINT64 nHead64;
 
     NFIDENTID()
     {
-		nHead64 = 0;
         nData64 = 0;
+        nHead64 = 0;
     }
 
-	NFIDENTID(NFINT64 nData)
-	{
-		nHead64 = 0;
-		nData64 = nData;
-	}
+    NFIDENTID(NFINT64 nHeadData, NFINT64 nData)
+    {
+        nHead64 = nHeadData;
+        nData64 = nData;
+    }
 
-	NFIDENTID(NFINT64 nSvr, NFINT64 nData)
-	{
-		nHead64 = nSvr;
-		nData64 = nData;
-	}
+    NFIDENTID(const NFIDENTID& xData)
+    {
+        nHead64 = xData.nHead64;
+        nData64 = xData.nData64;
+    }
 
-	NFIDENTID operator=(const NFINT64 src)
-	{
-		nHead64 = 0;
-		nData64 = src;
-	}
+    NFIDENTID& operator=(const NFIDENTID& xData)
+    {
+        nHead64 = xData.nHead64;
+        nData64 = xData.nData64;
 
-	NFIDENTID operator=(const int src)
-	{
-		nHead64 = 0;
-		nData64 = src;
-	}
+        return *this;
+    }
 
     bool IsNull() const
     {
-        return (0 == nData64) && (0 == nHead64);
+        return 0 == nData64 && 0 == nHead64;
     }
 
     bool operator == (const NFIDENTID& id) const
     {
-        return (this->nData64 == id.nData64) && (this->nHead64 == id.nHead64);
+        return this->nData64 == id.nData64 && this->nHead64 == id.nHead64;
     }
 
     bool operator != (const NFIDENTID& id) const
     {
-        return (this->nData64 != id.nData64) || (this->nHead64 != id.nHead64);
+        return this->nData64 != id.nData64 || this->nHead64 != id.nHead64;
     }
 
     bool operator < (const NFIDENTID& id) const
@@ -81,24 +73,24 @@ struct NFIDENTID
 
     std::string ToString() const
     {
-        return boost::lexical_cast<std::string>(this->nHead64) + "-" + boost::lexical_cast<std::string>(this->nData64);
+        return boost::lexical_cast<std::string>(nHead64) + "-" + boost::lexical_cast<std::string>(nData64);
     }
 
-    bool FormString(const std::string& strID)
-    {
-        size_t nStrLength = strID.length();
-        size_t nPos = strID.find('-');
-        if (nPos < 0)
-        {
-            return false;
-        }
+	bool FromString(const std::string& strID)
+	{
+		size_t nStrLength = strID.length();
+		size_t nPos = strID.find('-');
+		if (nPos == std::string::npos)
+		{
+			return false;
+		}
 
-        std::string strHead = strID.substr(0, nPos);
-        std::string strData = "";
-        if (nPos + 1 < nStrLength)
-        {
-            strData = strID.substr(nPos + 1, nStrLength - nPos);
-        }
+		std::string strHead = strID.substr(0, nPos);
+		std::string strData = "";
+		if (nPos + 1 < nStrLength)
+		{
+			strData = strID.substr(nPos + 1, nStrLength - nPos);
+		}
 
         try
         {
@@ -111,7 +103,7 @@ struct NFIDENTID
         {
             return false;
         }
-    }
+	}
 };
-//#pragma pack(pop)
+
 #endif
