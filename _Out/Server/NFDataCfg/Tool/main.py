f3759662acb678c7c2dff7e7c9772327c8c196a0
eliteYang <elite_yang@163.com>
2015-06-15 17:19:30 +0800

    new tool, add function creating record's enum; delete old xml tool; create new xml with component node

--- _Out/Server/NFDataCfg/Tool/main.py
+++
@@ -1,8 +0,0 @@
-# encoding:utf-8
-from cx_Freeze import setup, Executable
-
-setup(
-name = "xls2xml",
-version = "0.2",
-description = "COPY (C) NFrame Stuido www.yowoyo.com",
-executables = [Executable("xls2xml.py", icon = "setting.ico")])
