8a3e0b17e35a41c9872393689607d37ac48ef73c
kytooooo <342006@qq.com>
2015-07-01 22:42:39 +0800

    replace net module by cluster client
    Former-commit-id: f228a50709e0cc8f5359531cd50a22b3dc1072fb [formerly ee4a6806641f36ae8a922168dcfa8cd84cd8874e]
    Former-commit-id: a424605e181427e499d4c91a8d12677c9ed80e2d

--- NFServer/NFGameServerNet_ClientPlugin/NFCGameServerToWorldModule.h
+++ NFServer/NFGameServerNet_ClientPlugin/NFCGameServerToWorldModule.h
@@ -11,6 +11,7 @@
 
 //  the cause of sock'libariy, thenfore "NFCNet.h" much be included first.
 #include "NFComm/NFMessageDefine/NFMsgDefine.h"
+#include "NFComm/NFPluginModule/NFIClusterClientModule.hpp"
 #include "NFComm/NFPluginModule/NFIGameServerNet_ClientModule.h"
 #include "NFComm/NFPluginModule/NFIGameServerNet_ServerModule.h"
 #include "NFComm/NFPluginModule/NFIEventProcessModule.h"
@@ -21,64 +22,64 @@
 #include "NFComm/NFPluginModule/NFIElementInfoModule.h"
 #include "NFComm/NFPluginModule/NFILogModule.h"
 
-class NFCGameServerToWorldModule : public NFINetModule
+class NFCGameServerToWorldModule : public NFIClusterClientModule
 {
 public:
-    NFCGameServerToWorldModule(NFIPluginManager* p)
-    {
-        pPluginManager = p;
-    }
-    virtual bool Init();
-    virtual bool Shut();
-    virtual bool Execute(const float fLasFrametime, const float fStartedTime);
+	NFCGameServerToWorldModule(NFIPluginManager* p)
+	{
+		pPluginManager = p;
+	}
+	virtual bool Init();
+	virtual bool Shut();
+	virtual bool Execute(const float fLasFrametime, const float fStartedTime);
 
-    virtual bool AfterInit();
+	virtual bool AfterInit();
 
 
-    virtual void LogRecive(const char* str){}
-    virtual void LogSend(const char* str){}
-
-protected:
-
-    int OnReciveWSPack(const NFIPacket& msg);
-    int OnSocketWSEvent(const int nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet);
-
-    //连接丢失,删2层(连接对象，帐号对象)
-    void OnClientDisconnect(const int nAddress);
-    //有连接
-    void OnClientConnected(const int nAddress);
+	virtual void LogRecive(const char* str){}
+	virtual void LogSend(const char* str){}
 
 protected:
-    void Register();
-    void UnRegister();
-    void RefreshWorldInfo();
 
-    int OnLoadRoleDataBeginProcess(const NFIPacket& msg);
+	int OnReciveWSPack(const NFIPacket& msg);
+	int OnSocketWSEvent(const int nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet);
 
-    int OnLoadRoleDataFinalProcess(const NFIPacket& msg);
+	//连接丢失,删2层(连接对象，帐号对象)
+	void OnClientDisconnect(const int nAddress);
+	//有连接
+	void OnClientConnected(const int nAddress);
 
-    int OnEnquireSceneInfoProcess(const NFIPacket& msg);
+protected:
+	void Register();
+	void UnRegister();
+	void RefreshWorldInfo();
 
-    int OnSwapGSProcess(const NFIPacket& msg);
-    int OnAckCreateGuildProcess(const NFIPacket& msg);
-    int OnAckJoinGuildProcess(const NFIPacket& msg);
-    int OnAckLeaveGuildProcess(const NFIPacket& msg);
+	int OnLoadRoleDataBeginProcess(const NFIPacket& msg);
 
-    int OnDataLoadBeginEvent(const NFIDENTID& object, const int nEventID, const NFIDataList& var);
+	int OnLoadRoleDataFinalProcess(const NFIPacket& msg);
 
-    //int OnToWorldEvent( const NFIDENTID& object, const int nEventID, const NFIDataList& var );
+	int OnEnquireSceneInfoProcess(const NFIPacket& msg);
 
-    int OnSwapGSEvent(const NFIDENTID& object, const int nEventID, const NFIDataList& var);
+	int OnSwapGSProcess(const NFIPacket& msg);
+	int OnAckCreateGuildProcess(const NFIPacket& msg);
+	int OnAckJoinGuildProcess(const NFIPacket& msg);
+	int OnAckLeaveGuildProcess(const NFIPacket& msg);
 
-    int OnClassCommonEvent(const NFIDENTID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& var);
+	int OnDataLoadBeginEvent(const NFIDENTID& object, const int nEventID, const NFIDataList& var);
+
+	//int OnToWorldEvent( const NFIDENTID& object, const int nEventID, const NFIDataList& var );
+
+	int OnSwapGSEvent(const NFIDENTID& object, const int nEventID, const NFIDataList& var);
+
+	int OnClassCommonEvent(const NFIDENTID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& var);
 
 private:
 
-    NFILogModule* m_pLogModule;
-    NFIKernelModule* m_pKernelModule;
-    NFIEventProcessModule* m_pEventProcessModule;
-    NFILogicClassModule* m_pLogicClassModule;
-    NFIElementInfoModule* m_pElementInfoModule;
+	NFILogModule* m_pLogModule;
+	NFIKernelModule* m_pKernelModule;
+	NFIEventProcessModule* m_pEventProcessModule;
+	NFILogicClassModule* m_pLogicClassModule;
+	NFIElementInfoModule* m_pElementInfoModule;
 };
 
 #endif
