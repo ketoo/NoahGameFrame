affc172318119976373f2efecc1e8f91aa61a7d7
kytooooo <342006@qq.com>
2015-07-02 21:17:02 +0800

    fixed heartbeat

--- NFComm/NFPluginModule/NFIKernelModule.h
+++ NFComm/NFPluginModule/NFIKernelModule.h
@@ -43,9 +43,9 @@ public:
     }
 
     template<typename BaseType>
-    bool AddPropertyCallBack(const NFIDENTID& self, const std::string& strPropertyName, BaseType* pBase, int (BaseType::*handler)(const NFIDENTID&, const std::string&, const NFIDataList&, const NFIDataList&, const NFIDataList&))
+    bool AddPropertyCallBack(const NFIDENTID& self, const std::string& strPropertyName, BaseType* pBase, int (BaseType::*handler)(const NFIDENTID&, const std::string&, const NFIDataList&, const NFIDataList&))
     {
-        PROPERTY_EVENT_FUNCTOR functor = boost::bind(handler, pBase, _1, _2, _3, _4, _5);
+        PROPERTY_EVENT_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
         PROPERTY_EVENT_FUNCTOR_PTR functorPtr(new PROPERTY_EVENT_FUNCTOR(functor));
         return AddPropertyCallBack(self, strPropertyName, functorPtr);
     }
