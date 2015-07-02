a478953cb55629f7ed48b4ec69f75c080828c5ea
kytooooo <342006@qq.com>
2015-07-02 21:11:57 +0800

    replace boost::function by std::function
    Former-commit-id: 7d29a04e847d1d50c7dc7954303021acc37d9c9a [formerly 5bf295307963131e56254b9ba1565410683cf388]
    Former-commit-id: 1ad50eeb12c9718271352863fb007fe9c77ebf02

--- NFServer/NFGameLogicPlugin/NFCEctypeModule.cpp
+++ NFServer/NFGameLogicPlugin/NFCEctypeModule.cpp
@@ -203,7 +203,7 @@ bool NFCEctypeModule::CompleteEctypeMode(const NFIDENTID self, const int nContai
     return true;
 }
 
-int NFCEctypeModule::OnObjectGroupIDEvent(const NFIDENTID& self, const std::string& strPropertyName, const NFIDataList& oldVar, const NFIDataList& newVar, const NFIDataList& argVar)
+int NFCEctypeModule::OnObjectGroupIDEvent(const NFIDENTID& self, const std::string& strPropertyName, const NFIDataList& oldVar, const NFIDataList& newVar)
 {
 
     return 0;
