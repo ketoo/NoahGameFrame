35959b49db9033d0922e30b56b9fc7c4014a7d0e
eliteYang <362148418@qq.com>
2015-07-08 01:02:18 -0700

    modify shell

--- _Out/Server/Debug/rund.sh
+++ _Out/Server/Debug/rund.sh
@@ -1,32 +1,39 @@
+cp -a ../../Comm/Debug/NFPluginLoader_d ./NFMasterServer/NFMasterServer_d
+cp -a ../../Comm/Debug/NFPluginLoader_d ./NFWorldServer/NFWorldServer_d
+cp -a ../../Comm/Debug/NFPluginLoader_d ./NFLoginServer/NFLoginServer_d
+cp -a ../../Comm/Debug/NFPluginLoader_d ./NFGameServer1/NFGameServer_d
+cp -a ../../Comm/Debug/NFPluginLoader_d ./NFProxyServer1/NFProxyServer_d
+
 export LC_ALL="C"
+export LD_LIBRARY_PATH="../../Comm/Debug/"
 
 cd ./NFMasterServer
-chmod -R 777  NFPluginLoader_d 
-./NFPluginLoader_d &
+chmod -R 777  NFMasterServer_d
+./NFMasterServer_d -d
 cd ../
 
 cd ./NFWorldServer
-chmod -R 777  NFPluginLoader_d 
-./NFPluginLoader_d &
+chmod -R 777  NFWorldServer_d
+./NFWorldServer_d -d
 cd ../
 
 
 cd ./NFLoginServer
-chmod -R 777  NFPluginLoader_d 
-./NFPluginLoader_d &
+chmod -R 777  NFLoginServer_d
+./NFLoginServer_d -d
 cd ../
 
 
 
 cd ./NFGameServer1
-chmod -R 777  NFPluginLoader_d 
-./NFPluginLoader_d &
+chmod -R 777  NFGameServer_d
+./NFGameServer_d -d
 cd ../
 
 
 cd ./NFProxyServer1
-chmod -R 777  NFPluginLoader_d 
-./NFPluginLoader_d &
+chmod -R 777  NFProxyServer_d
+./NFProxyServer_d -d
 cd ../
 
 
