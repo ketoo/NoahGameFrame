cdadf0617aa39572e1adec265b1c7917974c011a
kytooooo <342006@qq.com>
2015-07-08 23:37:22 +0800

    optimize the interface[for net module]

--- NFComm/NFPluginModule/NFIProxyServerToWorldModule.h
+++ NFComm/NFPluginModule/NFIProxyServerToWorldModule.h
@@ -11,41 +11,15 @@
 
 #include <iostream>
 #include "NFILogicModule.h"
-#include "NFINetModule.h"
+#include "NFIClusterClientModule.hpp"
 
 class NFIProxyServerToWorldModule
-    : public  NFINetModule, public NFMap<int64_t, NFINetModule>
+    : public  NFIClusterClientModule
 {
-public:
 
-    struct GameData 
-    {
-        GameData()
-        {
-            nGameID = 0;
-            strIP = "";
-            nPort = 0;
-            strName = "";
-            eState = NFMsg::EServerState::EST_CRASH;
-        }
-
-        int nGameID;
-        std::string strIP;
-        int nPort;
-        std::string strName;
-        NFMsg::EServerState eState;
-    };
-
-    
-    typedef NFMap<int, GameData> GameDataMap;
 
 public:
-    virtual int Transpond(int nGameServerID, const NFIPacket& msg) = 0;
-
     virtual bool VerifyConnectData(const std::string& strAccount, const std::string& strKey) = 0;
-
-    virtual GameData* GetGameData(int nGameID) = 0;
-    virtual GameDataMap& GetGameDataMap() = 0;
 };
 
 #endif
