b62e8b259f007719a5eeb934bc44dd9ae05bd390
kytooooo <342006@qq.com>
2015-08-31 00:36:53 +0800

    remove async event(instead by actor model)
    Former-commit-id: dc91cc228b57bee2b13a8ce0b60203f6938fec4b [formerly de0545756f19dd43abfd0d4fb17bdeffa00dce88]
    Former-commit-id: 4d66065a966746dcee67770a060e212a516e4d4c

--- Tutorial/Tutorial5/HelloWorld5Module.cpp
+++ Tutorial/Tutorial5/HelloWorld5Module.cpp
@@ -24,10 +24,11 @@ bool HelloWorld5Module::AfterInit()
     m_pElementInfoModule = dynamic_cast<NFIElementInfoModule*>(pPluginManager->FindModule("NFCElementInfoModule"));
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-	for (int i = 0; i < 500; ++i)
+	for (int i = 0; i < 50000; ++i)
 	{
-		int nActorID = m_pEventProcessModule->AddActorEventCallBack<NFCTestComponent>(this, &HelloWorld5Module::OnSyncEvent);
-		m_pEventProcessModule->SendActorMsg(nActorID, 555, NFIDENTID(), boost::lexical_cast<std::string>(i));
+		int nActorID = pPluginManager->GetActorManager()->RequireActor<NFCTestComponent>(this, &HelloWorld5Module::OnSyncEvent);
+
+		pPluginManager->GetActorManager()->SendMsgToActor(nActorID, NFIDENTID(), 555, boost::lexical_cast<std::string>(i));
 	}
 
     std::cout << "End Test Actor, ThreadID: " << std::this_thread::get_id() << std::endl;
