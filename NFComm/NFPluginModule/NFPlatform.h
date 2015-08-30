08efbb3dfeb0952685b33c43c417fca698c7257b
kytooooo <kytooooo>
2015-08-30 22:49:57 +0800

    add macro for THERON_DEBUG

--- NFComm/NFPluginModule/NFPlatform.h
+++ NFComm/NFPluginModule/NFPlatform.h
@@ -301,7 +301,7 @@ inline unsigned long NF_GetTickCount()
 
 
 //use actor mode--begin
-#define NF_ACTOR_THREAD_COUNT 16
+#define NF_ACTOR_THREAD_COUNT 1
 
 #ifndef NF_DYNAMIC_PLUGIN
 #define NF_DYNAMIC_PLUGIN
@@ -312,6 +312,12 @@ inline unsigned long NF_GetTickCount()
 #endif
 
 #ifdef NF_USE_ACTOR
+
+#ifdef NF_DEBUG_MODE
+#define THERON_DEBUG 1
+#else
+#define THERON_DEBUG 0
+#endif
 
 #ifndef THERON_CPP11
 #define THERON_CPP11 1
