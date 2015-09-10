ccb888097ea8eab38942d4e55d732bbeb82102e8
ketoo <ketoo>
2015-09-10 22:06:06 +0800

    rename function for get plugin class name

--- Tutorial/Tutorial5/Tutorial5Plugin.cpp
+++ Tutorial/Tutorial5/Tutorial5Plugin.cpp
@@ -27,7 +27,7 @@ const int Tutorial5Plugin::GetPluginVersion()
 
 const std::string Tutorial5Plugin::GetPluginName()
 {
-    GET_PLUGIN_NAME(Tutorial5Plugin)
+    GET_CLASS_NAME(Tutorial5Plugin)
 }
 
 void Tutorial5Plugin::Install()
