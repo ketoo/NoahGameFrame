075807ac73c83fb2f2c93f7f8c358188a3321551
ketoo <ketoo>
2015-09-10 22:03:21 +0800

    restructure interface for actor model[to get set property component]
    Former-commit-id: fd2b27ccab250d8dadaf573160dea4803f59701e [formerly 47c49d9707823e519c32e5828b77f4767dd23d97]
    Former-commit-id: e0a5e6574853853eb3e14c693d564d2ea906b92d

--- NFComm/NFPluginLoader/NFCActor.h
+++ NFComm/NFPluginLoader/NFCActor.h
@@ -41,6 +41,48 @@ public:
 	virtual void AddComponent(NF_SHARE_PTR<NFIComponent> pComponent);
 	virtual bool AddEndFunc(EVENT_ASYNC_PROCESS_END_FUNCTOR_PTR functorPtr_end);
 
+	//////////////////////////////////////////////////////////////////////////
+	virtual NFIDENTID Self();
+	virtual NF_SHARE_PTR<NFIActor> GetActor(const NFIDENTID& self);
+
+	virtual bool SetPropertyInt(const NFIDENTID& self, const std::string& strPropertyName, const NFINT64 nValue);
+	virtual bool SetPropertyFloat(const NFIDENTID& self, const std::string& strPropertyName,  const float fValue);
+	virtual bool SetPropertyDouble(const NFIDENTID& self, const std::string& strPropertyName, const double dwValue);
+	virtual bool SetPropertyString(const NFIDENTID& self, const std::string& strPropertyName, const std::string& strValue);
+	virtual bool SetPropertyObject(const NFIDENTID& self, const std::string& strPropertyName, const NFIDENTID& obj);
+
+	virtual NFINT64 GetPropertyInt(const NFIDENTID& self, const std::string& strPropertyName);
+	virtual float GetPropertyFloat(const NFIDENTID& self, const std::string& strPropertyName);
+	virtual double GetPropertyDouble(const NFIDENTID& self, const std::string& strPropertyName);
+	virtual const std::string& GetPropertyString(const NFIDENTID& self, const std::string& strPropertyName);
+	virtual NFIDENTID GetPropertyObject(const NFIDENTID& self, const std::string& strPropertyName);
+
+	virtual bool SetRecordInt(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol, const NFINT64 nValue);
+	virtual bool SetRecordFloat(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol,  const float fValue);
+	virtual bool SetRecordDouble(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol, const double dwValue);
+	virtual bool SetRecordString(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol, const std::string& strValue);
+	virtual bool SetRecordObject(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol, const NFIDENTID& obj);
+
+	virtual bool SetRecordInt(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag, const NFINT64 value);
+	virtual bool SetRecordFloat(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag, const float value);
+	virtual bool SetRecordDouble(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag, const double value);
+	virtual bool SetRecordString(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag, const std::string& value);
+	virtual bool SetRecordObject(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag, const NFIDENTID& value);
+
+	virtual NFINT64 GetRecordInt(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol);
+	virtual float GetRecordFloat(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol);
+	virtual double GetRecordDouble(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol);
+	virtual const std::string& GetRecordString(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol);
+	virtual NFIDENTID GetRecordObject(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const int nCol);
+
+	virtual NFINT64 GetRecordInt(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag);
+	virtual float GetRecordFloat(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag);
+	virtual double GetRecordDouble(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag);
+	virtual const std::string& GetRecordString(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag);
+	virtual NFIDENTID GetRecordObject(const NFIDENTID& self, const std::string& strRecordName, const int nRow, const std::string& strColTag);
+	//////////////////////////////////////////////////////////////////////////
+
+
 protected:
 	NF_SHARE_PTR<NFIComponent> m_pComponent;
 	EVENT_ASYNC_PROCESS_END_FUNCTOR_PTR mxFunctorEndPtr;
