3dc0d089789a3d59166fd6a3c4a4d2b17e121200
kytooooo <342006@qq.com>
2015-10-08 20:21:39 +0800

    remove unused file
    Former-commit-id: e8267835cb6345a7bca30b6baedfe0ac72c26065 [formerly 1bed8b4f90c99e1cf2168493dea82ce30db6fb0d]
    Former-commit-id: 9e41ef7341aeb315cfc5ce4ff986220357663ae4

--- NFServer/NFWorldGuildPlugin/NFCWorldGuildDataModule.h
+++ NFServer/NFWorldGuildPlugin/NFCWorldGuildDataModule.h
@@ -17,7 +17,7 @@
 #include "NFComm/NFPluginModule/NFIClusterModule.h"
 #include "NFComm/NFPluginModule/NFIGameLogicModule.h"
 #include "NFComm/NFMessageDefine/NFMsgBase.pb.h"
-#include "NFComm/NFPluginModule/NFIObjectSaveModule.h"
+#include "NFComm/NFPluginModule/NFIDataProcessModule.h"
 
 class NFCWorldGuildDataModule
     : public NFIWorldGuildDataModule
@@ -61,7 +61,7 @@ protected:
     NFIKernelModule* m_pKernelModule;
     NFIUUIDModule* m_pUUIDModule;
     NFIClusterModule* m_pClusterSQLModule;    
-    NFIObjectSaveModule* m_pObjectSaveModule;
+    NFIDataProcessModule* m_pDataProcessModule;
     
 private:
     std::string mstrGuildTalble;
