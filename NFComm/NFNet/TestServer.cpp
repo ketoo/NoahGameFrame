d60500325c91b7dae3df0ced566fe415ee2fd0bb
eliteYang <elite_yang@163.com>
2015-12-06 03:36:51 +0800

    Remove libevent IOCP in windows platform, use SELECT default
    Former-commit-id: 8b5c16fb457d0e30e1921866d4086baec32dc53c [formerly 1b97281e5d74faa34ba7f4db1e2fd9b4eca12c94]
    Former-commit-id: 11b570a1f9c912c2759117bf36925fb5a3e81392

--- NFComm/NFNet/TestServer.cpp
+++ NFComm/NFNet/TestServer.cpp
@@ -2,8 +2,9 @@
 #include "NFCNet.h"
 #include <thread>
 #include <string>
+#include <processthreadsapi.h>
 #pragma comment(lib,"ws2_32.lib")
-#pragma  comment(lib,"NFNet_d.lib")
+#pragma comment(lib,"NFNet_d.lib")
 
 class TestServerClass
 {
@@ -19,14 +20,13 @@ public:
 		std::string str;
 		str.assign(msg, nLen);
 
-		pNet->SendMsgWithOutHead(nMsgID, msg, nLen, nSockIndex);
-		//std::cout << " fd: " << nSockIndex << " msg_id: " << nMsgID <<  " data: " << str << " thread_id: " << std::this_thread::get_id() << std::endl;
-	};
+		//pNet->SendMsgWithOutHead(nMsgID, msg, nLen, nSockIndex);
+		std::cout << " fd: " << nSockIndex << " msg_id: " << nMsgID /*<<  " data: " << str*/ << " thread_id: " << GetCurrentThreadId() << std::endl;
+	}
 
 	void EventHandler(const int nSockIndex, const NF_NET_EVENT e, NFINet* p)
 	{
-		//std::cout << " fd: " << nSockIndex << " event_id: " << e << " thread_id: " << std::this_thread::get_id() << std::endl;
-
+		std::cout << " fd: " << nSockIndex << " event_id: " << e << " thread_id: " << std::this_thread::get_id() << std::endl;
 	}
 
 	void Execute()
@@ -36,16 +36,14 @@ public:
 
 protected:
 	NFINet* pNet;
-private:
 };
 
 int main(int argc, char** argv)
 {
 	TestServerClass x;
 
-	while (1)	{		//Sleep(1);		x.Execute();
-	}
-
+	while (1)	{		x.Execute();
+	}
 
 	return 0;
 }
