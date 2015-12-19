8539d7d2578af4a15be05f2eecdb9d61d06b160c
flyicegood <flyicegood@163.com>
2015-12-19 19:42:04 +0800

    add url module;

---
+++ Tutorial/Tutorial7/HelloWorld7.cpp
@@ -0,0 +1,46 @@
+#include "HelloWorld7.h"
+
+
+bool HelloWorld7::Init()
+{
+    //初始化
+    std::cout << "Hello, world1, Init" << std::endl;
+
+    return true;
+}
+
+bool HelloWorld7::AfterInit()
+{
+    //初始化完毕
+    std::cout << "Hello, world1, AfterInit" << std::endl;
+
+    return true;
+}
+
+bool HelloWorld7::Execute( const float fLasFrametime, const float fStartedTime )
+{
+    //每帧执行
+    //std::cout << "Hello, world1, Execute" << std::endl;
+
+    return true;
+}
+
+bool HelloWorld7::BeforeShut()
+{
+    std::cout << "Hello, world1, BeforeShut1111" << std::endl;
+
+    system("PAUSE");
+
+    //反初始化之前
+    std::cout << "Hello, world1, BeforeShut" << std::endl;
+
+    return true;
+}
+
+bool HelloWorld7::Shut()
+{
+    //反初始化
+    std::cout << "Hello, world1, Shut" << std::endl;
+
+    return true;
+}
