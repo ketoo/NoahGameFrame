84abd4cf7634f77b3adb9c64935a919beccf30fc
flyicegood <flyicegood@163.com>
2015-12-19 19:42:04 +0800

    add url module;

---
+++ NFComm/NFUrlPlugin/dllmain.cpp
@@ -0,0 +1,45 @@
+#include "NFComm/NFPluginModule/NFPlatform.h"
+
+
+#if NF_PLATFORM == NF_PLATFORM_WIN
+#pragma  comment(lib,"ws2_32.lib")
+#pragma  comment(lib,"wsock32.lib")
+#pragma  comment(lib, "Wldap32.lib")
+#endif
+
+
+#pragma comment( lib, "Dbghelp.lib" )
+
+#ifdef NF_DEBUG_MODE
+
+#if NF_PLATFORM == NF_PLATFORM_WIN
+
+#ifdef NF_DYNAMIC_PLUGIN
+#pragma  comment(lib, "libcurld.lib")
+
+#else
+#pragma  comment(lib, "libcurl.lib")
+#endif
+
+//#pragma comment( lib, "Theron_d.lib" )
+#elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
+#pragma comment( lib, "libcurl.a" )
+#elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
+#endif
+
+#else
+
+#if NF_PLATFORM == NF_PLATFORM_WIN
+#ifdef NF_DYNAMIC_PLUGIN
+#pragma comment( lib, "libcurl.lib" )
+#else
+#pragma comment( lib, "libcurl.lib" )
+#endif
+
+//#pragma comment( lib, "Theron.lib" )
+#elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
+#pragma comment( lib, "libcurl.a" )
+#elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
+#endif
+
+#endif
