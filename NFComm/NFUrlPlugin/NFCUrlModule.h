d0cdb649c2b442fabe403c9f5b68d17f87736f9c
flyicegood <flyicegood@163.com>
2015-12-20 12:33:45 +0800

    add url module;

--- NFComm/NFUrlPlugin/NFCUrlModule.h
+++ NFComm/NFUrlPlugin/NFCUrlModule.h
@@ -11,6 +11,34 @@
 
 #include "curl/curl.h"
 #include "NFComm/NFPluginModule/NFIUrlModule.h"
+#include "NFComm/NFCore/NFIComponent.h"
+
+class NFCURLComponent : public NFIComponent
+{
+public:
+    NFCURLComponent(NFGUID self):NFIComponent(self)
+    {
+    }
+
+    virtual ~NFCURLComponent()
+    {
+
+    }
+
+    virtual const std::string GetComponentName() const ;
+    virtual int OnASyncEvent(const NFGUID& self, const int event, std::string& arg);
+
+protected:
+    virtual NF_SHARE_PTR<NFIComponent> CreateNewInstance();
+
+private:
+    std::string mstrUrl;
+    std::string mstrGetParams;
+    std::string mstrBodyData;
+    std::string mxCookies;
+    float mfTimeOutSec;
+    std::string mstrRsp;
+};
 
 class NFCUrlModule
     : public NFIUrlModule
@@ -65,7 +93,15 @@ public:
     virtual int HttpRequest(const std::string& strUrl, const std::map<std::string, std::string>& mxGetParams, const std::map<std::string, std::string>& mxPostParams,const std::map<std::string, std::string>& mxCookies, const float fTimeOutSec, std::string& strRsp);    
     virtual int HttpRequest(const std::string& strUrl, const std::map<std::string, std::string>& mxGetParams, const std::string& strBodyData,const std::map<std::string, std::string>& mxCookies, const float fTimeOutSec, std::string& strRsp);    
 
-    virtual bool Test();
+    virtual int HttpRequestAs(const std::string& strUrl, const std::map<std::string, std::string>& mxGetParams, const std::string& strBodyData,const std::map<std::string, std::string>& mxCookies, const float fTimeOutSec, std::string& strRsp);    
+
+    //“Ï≤Ω
+    virtual bool StartActorPool(const int nCount);
+    virtual bool CloseActorPool();
+
+    int HttpRequestAsyEnd(const NFGUID& self, const int nFormActor, const int nSubMsgID, const std::string& strData);
+
+    static int HttpReq(const std::string& strUrl, const std::string& strGetParams, const std::string& strBodyData,const std::string& mxCookies, const float fTimeOutSec, std::string& strRsp);    
 
 protected:
     static size_t RecvHttpData(void* buffer, size_t size, size_t nmemb, std::string* strRecveData);
@@ -73,8 +109,7 @@ protected:
     std::string CompositeCookies( const std::map<std::string,std::string>& params );
 
 protected:
-    std::string mstrHost;
-
+    NFMapEx<int, int> mActorList; //actorid <-->Used
 };
 
 #endif
