5b6cd46c92736b893e35b683fdf7fbe684ce6bea
flyicegood <flyicegood@163.com>
2015-12-29 21:01:47 +0800

    modify NFUrlPlugin compile error

--- NFComm/NFUrlPlugin/dllmain.cpp
+++ NFComm/NFUrlPlugin/dllmain.cpp
@@ -1,52 +1,36 @@
 #include "NFComm/NFPluginModule/NFPlatform.h"
 
+#ifdef NF_DEBUG_MODE
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma  comment(lib,"ws2_32.lib")
 #pragma  comment(lib,"wsock32.lib")
 #pragma  comment(lib, "Wldap32.lib")
-#endif
-
-
 #pragma comment( lib, "Dbghelp.lib" )
-
-#ifdef NF_DEBUG_MODE
-
-#if NF_PLATFORM == NF_PLATFORM_WIN
-
-#ifdef NF_DYNAMIC_PLUGIN
-#pragma  comment(lib, "libcurld.lib")
+#pragma comment( lib, "libcurld.lib" )
 #pragma comment( lib, "libprotobuf_d.lib" )
 #pragma comment( lib, "NFMessageDefine_d.lib" )
-
-#else
-#pragma  comment(lib, "libcurl.lib")
-#pragma comment( lib, "libprotobuf.lib" )
-#pragma comment( lib, "NFMessageDefine.lib" )
-#endif
-
-//#pragma comment( lib, "Theron_d.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "libcurl.a" )
+#pragma comment( lib, "libcurld.a" )
+#pragma comment( lib, "libprotobuf_d.a" )
+#pragma comment( lib, "NFMessageDefine_d.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
 #else
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
-#ifdef NF_DYNAMIC_PLUGIN
-#pragma  comment(lib, "libcurld.lib")
-#pragma comment( lib, "libprotobuf_d.lib" )
-#pragma comment( lib, "NFMessageDefine_d.lib" )
-#else
-#pragma  comment(lib, "libcurl.lib")
+#pragma  comment(lib,"ws2_32.lib")
+#pragma  comment(lib,"wsock32.lib")
+#pragma  comment(lib, "Wldap32.lib")
+#pragma comment( lib, "Dbghelp.lib" )
+#pragma comment( lib, "libcurl.lib" )
 #pragma comment( lib, "libprotobuf.lib" )
 #pragma comment( lib, "NFMessageDefine.lib" )
-#endif
-
-//#pragma comment( lib, "Theron.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "libcurl.a" )
+#pragma comment( lib, "libcurld.a" )
+#pragma comment( lib, "libprotobuf_d.a" )
+#pragma comment( lib, "NFMessageDefine.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
