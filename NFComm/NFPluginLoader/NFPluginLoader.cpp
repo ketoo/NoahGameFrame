ff2aa02431427423ae683bbd81d196fd14727cc6
kytoo <342006@qq.com>
2016-01-01 15:44:56 +0800

    fixed for compiler

--- NFComm/NFPluginLoader/NFPluginLoader.cpp
+++ NFComm/NFPluginLoader/NFPluginLoader.cpp
@@ -53,9 +53,9 @@ long ApplicationCrashHandler(EXCEPTION_POINTERS* pException)
 {
     time_t t = time(0);
     char szDmupName[MAX_PATH];
-    tm* ptm = localtime(&t);
+    tm* ptm = localtime_s(&t);
 
-    sprintf(szDmupName, "%04d_%02d_%02d_%02d_%02d_%02d.dmp",  ptm->tm_year + 1900, ptm->tm_mon, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
+    sprintf_s(szDmupName, "%04d_%02d_%02d_%02d_%02d_%02d.dmp",  ptm->tm_year + 1900, ptm->tm_mon, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
     CreateDumpFile(szDmupName, pException);
 
     FatalAppExit(-1,  szDmupName);
