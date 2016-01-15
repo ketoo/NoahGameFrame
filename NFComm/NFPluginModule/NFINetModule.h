e4d69b0c264f09f554d4d74ab3f001eed32ef6e6
ifivebig <fivebig@gmail.com>
2016-01-15 15:24:58 +0800

    remove client servertype

--- NFComm/NFPluginModule/NFINetModule.h
+++ NFComm/NFPluginModule/NFINetModule.h
@@ -23,14 +23,13 @@
 enum NF_SERVER_TYPES
 {
 	NF_ST_NONE			= 0,    // NONE
-	NF_ST_CLIENT		= 1,    // client
-	NF_ST_REDIS			= 2,    //
-	NF_ST_MYSQL			= 3,    //
-	NF_ST_MASTER		= 4,    //
-	NF_ST_LOGIN			= 5,    //
-	NF_ST_PROXY			= 6,    //
-	NF_ST_GAME			= 7,    //
-	NF_ST_WORLD			= 8,    //
+	NF_ST_REDIS			= 1,    //
+	NF_ST_MYSQL			= 2,    //
+	NF_ST_MASTER		= 3,    //
+	NF_ST_LOGIN			= 4,    //
+	NF_ST_PROXY			= 5,    //
+	NF_ST_GAME			= 6,    //
+	NF_ST_WORLD			= 7,    //
 
 };
 
