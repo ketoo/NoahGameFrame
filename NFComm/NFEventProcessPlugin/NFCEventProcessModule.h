8e89c091283e4620014594214085849150277e54
kytooooo <342006@qq.com>
2016-01-16 17:55:06 +0800

    move class callback function to logic class module

--- NFComm/NFEventProcessPlugin/NFCEventProcessModule.h
+++ NFComm/NFEventProcessPlugin/NFCEventProcessModule.h
@@ -25,24 +25,19 @@ public:
 
     virtual bool Execute();
 
-    virtual bool RemoveEvent(const NFGUID& objectID);
     virtual bool RemoveEventCallBack(const NFGUID& objectID, const int nEventID);
 
     virtual bool DoEvent(const NFGUID& objectID, const int nEventID, const NFIDataList& valueList);
-    virtual bool DoEvent(const NFGUID& objectID,  const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& valueList);
 
     virtual bool AddEventCallBack(const NFGUID& objectID, const int nEventID, const EVENT_PROCESS_FUNCTOR_PTR& cb);
-    virtual bool AddClassCallBack(const std::string& strClassName, const CLASS_EVENT_FUNCTOR_PTR& cb);
 
 protected:
 	virtual bool HasEventCallBack(const NFGUID& objectID, const int nEventID);
 
 private:
 
-    NFList<NFGUID> mRemoveObjectListEx;
     NFMapEx<NFGUID, NFList<int> > mRemoveEventListEx;
 
-	NFMapEx<std::string, NFCClassEventList> mxClassEventInfoEx;
     NFMapEx<NFGUID, NFCObjectEventInfo> mObjectEventInfoMapEx;
 };
 
