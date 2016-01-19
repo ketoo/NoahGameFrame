8e095742e9dad5893a8ba0d689e26cc36457f9ed
kytooooo <342006@qq.com>
2016-01-19 20:53:45 +0800

    reconsitution remove some unused file and code

--- NFServer/NFGameServerPlugin/NFCPropertyTrailModule.cpp
+++ NFServer/NFGameServerPlugin/NFCPropertyTrailModule.cpp
@@ -32,13 +32,11 @@ bool NFCPropertyTrailModule::AfterInit()
 	m_pKernelModule = pPluginManager->FindModule<NFIKernelModule>( "NFCKernelModule" );
 	m_pElementInfoModule = pPluginManager->FindModule<NFIElementInfoModule>( "NFCElementInfoModule" );
 	m_pLogicClassModule = pPluginManager->FindModule<NFILogicClassModule>( "NFCLogicClassModule" );
-	m_pPropertyConfigModule = pPluginManager->FindModule<NFIPropertyConfigModule>( "NFCPropertyConfigModule" );
 	m_pLogModule = pPluginManager->FindModule<NFILogModule>( "NFCLogModule" );
 	
 	assert( NULL != m_pKernelModule );
 	assert( NULL != m_pElementInfoModule );
 	assert( NULL != m_pLogicClassModule );
-	assert( NULL != m_pPropertyConfigModule );
 	assert( NULL != m_pLogModule );
 
 
