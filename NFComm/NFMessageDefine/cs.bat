8c64d0de8e3aa69e4981d56beed6718fbd4230f4
kytooooo <342006@qq.com>
2016-01-21 00:20:05 +0800

    add protocol for mining stone ad npc configuration

--- NFComm/NFMessageDefine/cs.bat
+++ NFComm/NFMessageDefine/cs.bat
@@ -5,6 +5,6 @@ protogen -i:NFMsgPreGame.proto -o:NFMsgPreGame.cs
 protogen -i:NFMsgShare.proto -o:NFMsgShare.cs
 protogen -i:NFSLGDefine.proto -o:NFSLGDefine.cs
 protogen -i:NFFleetingDefine.proto -o:NFFleetingDefine.cs
-protogen -i:NFRecordDefine.proto -o:NFRecordDefine.cs
+protogen -i:NFMsgURl.proto -o:NFMsgURl.cs
 
 pause
