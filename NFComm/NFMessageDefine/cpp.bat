9c3ab9071d8d2a4ec81c7c631f6e99bf34b8dba6
kytooooo <342006@qq.com>
2016-01-21 00:20:05 +0800

    add protocol for mining stone ad npc configuration

--- NFComm/NFMessageDefine/cpp.bat
+++ NFComm/NFMessageDefine/cpp.bat
@@ -4,8 +4,7 @@ protoc.exe -I=./ --cpp_out=./ ./NFMsgBase.proto
 protoc.exe -I=./ --cpp_out=./ ./NFMsgPreGame.proto 
 protoc.exe -I=./ --cpp_out=./ ./NFMsgShare.proto  
 protoc.exe -I=./ --cpp_out=./ ./NFSLGDefine.proto 
-protoc.exe -I=./ --cpp_out=./ ./NFFleetingDefine.proto 
-protoc.exe -I=./ --cpp_out=./ ./NFRecordDefine.proto 
+protoc.exe -I=./ --cpp_out=./ ./NFFleetingDefine.proto  
 protoc.exe -I=./ --cpp_out=./ ./NFMsgURl.proto 
 
 pause
