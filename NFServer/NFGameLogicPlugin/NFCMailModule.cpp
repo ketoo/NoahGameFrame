96cc55ecc890a6a18054c6f0caef661cc27561a8
eliteYang <elite_yang@163.com>
2016-01-20 16:40:43 +0800

    add MailModule & RankModule

---
+++ NFServer/NFGameLogicPlugin/NFCMailModule.cpp
@@ -0,0 +1,45 @@
+#include "NFCMailModule.h"
+
+bool NFCMailModule::Init()
+{
+    return true;
+}
+
+bool NFCMailModule::Shut()
+{
+    return true;
+}
+
+bool NFCMailModule::Execute()
+{
+    return true;
+}
+
+bool NFCMailModule::AfterInit()
+{
+    m_pKernelModule = pPluginManager->FindModule<NFIKernelModule>("NFCKernelModule");
+
+    assert(NULL != m_pKernelModule);
+
+    return true;
+}
+
+bool NFCMailModule::SendMail(const NFGUID& reciever, const int nMailType, const NFIDataList& xItemList, const std::string& strNailContent/* = NULL_STR*/)
+{
+    return true;
+}
+
+int NFCMailModule::OnDrawMail(const NFGUID& self, const NFIDataList& var)
+{
+    return 0;
+}
+
+int NFCMailModule::OnOpenMail(const NFGUID& self, const NFIDataList& var)
+{
+    return 0;
+}
+
+int NFCMailModule::OnDeleteMail(const NFGUID& self, const NFIDataList& var)
+{
+    return 0;
+}
