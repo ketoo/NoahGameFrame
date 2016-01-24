5ceed92dc69874fc1a95b734a60da24234e6ced3
flyicegood <flyicegood@163.com>
2016-01-24 21:24:26 +0800

    add guild ectype

---
+++ NFComm/NFPluginModule/NFIWorldGuildEctypeModule.h
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------
+//    @FileName      :    NFIWorldGuildModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2015-05-24 08:51
+//    @Module           :   NFIWorldGuildModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef NFI_WORLD_GUILD_ECTYP_MODULE_H
+#define NFI_WORLD_GUILD_ECTYP_MODULE_H
+
+#include "NFILogicModule.h"
+
+class NFIWorldGuildEctypeModule
+    : public NFILogicModule
+{
+public:
+
+private:
+};
+
+#endif
