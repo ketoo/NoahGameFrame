17458bc11c6ebf26d5a83f3469602cb2ffa03bdf
kytooooo <342006@qq.com>
2016-01-29 16:05:59 +0800

    AddConsistentHash
    Former-commit-id: d52e6619753fc2b8a62bd792f5d7e8d2cb1b66de [formerly c03581ca72ccf76737aa37985189172b8cf73a4f]
    Former-commit-id: b7d54d330bdb9143f8804db10ca1a02ad7b54d3f

--- NFComm/NFCore/NFCAddConsistentHash.hpp
+++ NFComm/NFCore/NFCAddConsistentHash.hpp
@@ -33,7 +33,35 @@ public:
     }
 
 public:
-   
+   //添加备胎
+	void AddCandidateMachine(const int nServerID) 
+	{
+		std::list<NFCMachineNode> xNodeList;
+		GetNodeList(xNodeList);
+
+		//把所有的真实主机作为备份
+		NFCMachineNode xNode;
+		xNode.nMachineID = nServerID;
+		xNode.strIP = "";
+		xNode.nPort = 0;
+		xNode.bCandidate = true;
+
+		for (std::list<NFCMachineNode>::iterator it = xNodeList.begin(); it != xNodeList.end(); ++it)
+		{
+			NFIVirtualNode&  xRealNode = *it;
+			if (!xRealNode.Candidate())
+			{
+				xNode.xRealMachine.push_back(xRealNode);
+			}
+		}
+
+		Insert(xNode);
+	}
+	//实例话一个备胎
+	void IntanceCandidateMachine()
+	{
+
+	}
 
 };
 
