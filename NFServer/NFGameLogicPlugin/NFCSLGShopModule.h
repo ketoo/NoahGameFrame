08e013243222ea3efb74c3c5fefa487e22fb322d
flyicegood <flyicegood@163.com>
2016-01-30 00:15:30 +0800

    move module

--- NFServer/NFGameLogicPlugin/NFCSLGShopModule.h
+++ NFServer/NFGameLogicPlugin/NFCSLGShopModule.h
@@ -16,6 +16,7 @@
 #include "NFComm/NFPluginModule/NFIElementInfoModule.h"
 #include "NFComm/NFPluginModule/NFIPropertyModule.h"
 #include "NFComm/NFPluginModule/NFIPluginManager.h"
+#include "NFComm/NFPluginModule/NFIGameServerNet_ServerModule.h"
 
 class NFCSLGShopModule
     : public NFISLGShopModule
@@ -34,12 +35,17 @@ public:
 
 	virtual bool ReqBuyItem(const NFGUID& self, const std::string& strID, float fX, float fY, float fZ);
 
+protected:
+	/////////SLG_START/////////////////////////////////////////////////////////////////
+	void OnSLGClienBuyItem(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
+
 private:
-    NFIElementInfoModule* m_pElementInfoModule;
+    NFIElementInfoModule* m_pEslementInfoModule;
     NFIKernelModule* m_pKernelModule;
     NFILogModule* m_pLogModule;
     NFISLGBuildingModule* m_pSLGBuildingModule;
     NFIPropertyModule* m_pPropertyModule;
+	NFIGameServerNet_ServerModule* m_pGameServerNet_ServerModule;
 };
 
 
