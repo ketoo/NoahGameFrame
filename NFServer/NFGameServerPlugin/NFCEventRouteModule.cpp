edbccc8eb4965918107d8c8bf2c4722d1ce07651
kytooooo <342006@qq.com>
2016-02-04 07:38:18 +0800

    remove logic plugin

---
+++ NFServer/NFGameServerPlugin/NFCEventRouteModule.cpp
@@ -0,0 +1,53 @@
+// -------------------------------------------------------------------------
+//    @FileName      :    NFCEventRouteModule.cpp
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2015-01-02
+//    @Module           :    NFCEventRouteModule
+//
+// -------------------------------------------------------------------------
+
+#include "NFCEventRouteModule.h"
+
+bool NFCEventRouteModule::Init()
+{
+
+
+    return true;
+}
+
+
+bool NFCEventRouteModule::Shut()
+{
+    return true;
+}
+
+bool NFCEventRouteModule::Execute( const float fLasFrametime, const float fStartedTime )
+{
+    //Œª÷√ƒÿ
+    return true;
+}
+
+bool NFCEventRouteModule::AfterInit()
+{
+    m_pEventProcessModule = dynamic_cast<NFIEventProcessModule*>( pPluginManager->FindModule( "NFCEventProcessModule" ) );
+    m_pKernelModule = dynamic_cast<NFIKernelModule*>( pPluginManager->FindModule( "NFCKernelModule" ) );
+    m_pLogModule = dynamic_cast<NFILogModule*>( pPluginManager->FindModule( "NFCLogModule" ) );
+
+    assert( NULL != m_pEventProcessModule );
+    assert( NULL != m_pKernelModule );
+    assert( NULL != m_pLogModule );
+
+
+    return true;
+}
+
+bool NFCEventRouteModule::StartRouteEvent( const NFIDENTID& self, const NFIDataList& xDataList )
+{
+	return false;
+}
+
+bool NFCEventRouteModule::ReceiveRouteEvent( const NFIDENTID& self, const NFIDataList& xDataList )
+{
+	return false;
+}
+
