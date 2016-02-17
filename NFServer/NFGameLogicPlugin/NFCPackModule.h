4c34107711398de713a36519944d272a47a9d2ee
kytooooo <342006@qq.com>
2016-02-17 10:31:29 +0800

    remove boost lib dep

--- NFServer/NFGameLogicPlugin/NFCPackModule.h
+++ NFServer/NFGameLogicPlugin/NFCPackModule.h
@@ -9,9 +9,6 @@
 #ifndef NFC_PACK_MODULE_H
 #define NFC_PACK_MODULE_H
 
-#include <boost/static_assert.hpp>
-#include <boost/date_time/gregorian/gregorian.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
 #include "NFComm/NFPluginModule/NFIKernelModule.h"
 #include "NFComm/NFPluginModule/NFIGameLogicModule.h"
 #include "NFComm/NFPluginModule/NFIPackModule.h"
