ef47048e929489e5a0d8c990aff74c7313cd8faf
eliteYang <362148418@qq.com>
2016-02-20 15:29:28 +0800

    remove libaries version in linux project
    Former-commit-id: 0fdacb66d0a96c488566d67088bf71c5bd3dade2 [formerly e3a681b2e2690dc076b9196f56e4be751a687b0c]
    Former-commit-id: 62a32cadc8aaf4b640e3da4f0cd9038e4f25a02a

--- NFComm/NFKernelPlugin/NFKernelModule/NFCKernelModule.h
+++ NFComm/NFKernelPlugin/NFKernelModule/NFCKernelModule.h
@@ -50,7 +50,7 @@ public:
 
     virtual NF_SHARE_PTR<NFIObject> GetObject(const NFGUID& ident);
     virtual NF_SHARE_PTR<NFIObject> CreateObject(const NFGUID& self, const int nSceneID, const int nGroupID, const std::string& strClassName, const std::string& strConfigIndex, const NFIDataList& arg);
-    
+
 	virtual bool DestroyAll();
     virtual bool DestroySelf(const NFGUID& self);
     virtual bool DestroyObject(const NFGUID& self);
