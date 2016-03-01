037ec3662939405f19b80bb20dd2c54e6c7ef183
kytooooo <342006@qq.com>
2016-03-01 22:57:46 +0800

    reserved identifier violation #4

--- NFComm/NFCore/NFIPropertyManager.h
+++ NFComm/NFCore/NFIPropertyManager.h
@@ -6,8 +6,8 @@
 //
 // -------------------------------------------------------------------------
 
-#ifndef _NFI_PROPERTYMANAGER_H
-#define _NFI_PROPERTYMANAGER_H
+#ifndef NFI_PROPERTYMANAGER_H
+#define NFI_PROPERTYMANAGER_H
 
 #include "NFMap.h"
 #include "NFIProperty.h"
