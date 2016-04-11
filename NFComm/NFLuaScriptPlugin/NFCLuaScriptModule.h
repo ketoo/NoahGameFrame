9bef545e5398c8966df8943ee5e7bd293f2b04b1
tangyu1018@163.com <tangyu1018@163.com>
2016-04-11 14:54:48 +0800

    update NFLuaScriptModule

--- NFComm/NFLuaScriptPlugin/NFCLuaScriptModule.h
+++ NFComm/NFLuaScriptPlugin/NFCLuaScriptModule.h
@@ -16,44 +16,44 @@
 #include "NFComm/NFPluginModule/NFIScriptModule.h"
 
 class NFCLuaScriptModule
-    : public NFILuaScriptModule
+	: public NFILuaScriptModule
 {
 public:
-    NFCLuaScriptModule(NFIPluginManager* p)
-    {
-        pPluginManager = p;
-    }
+	NFCLuaScriptModule(NFIPluginManager* p)
+	{
+		pPluginManager = p;
+	}
 
-    virtual bool Init();
-    virtual bool Shut();
-    virtual bool Execute();
+	virtual bool Init();
+	virtual bool Shut();
+	virtual bool Execute();
 
-    virtual bool AfterInit();
-    virtual bool BeforeShut();
+	virtual bool AfterInit();
+	virtual bool BeforeShut();
 
-    //call script
-    virtual int DoScript(const NFGUID& self, const std::string& strComponentName, const std::string& strFunction, const NFCDataList& arg);
-    virtual int DoClassCommonScript(const NFGUID& self, const std::string& strComponentName, const std::string& strFunction);
+	//call script
+	virtual int DoScript(const NFGUID& self, const std::string& strComponentName, const std::string& strFunction, const NFCDataList& arg);
+	virtual int DoClassCommonScript(const NFGUID& self, const std::string& strComponentName, const std::string& strClassNameconst, CLASS_OBJECT_EVENT eClassEvent);
 
-    virtual int DoEventScript(const NFGUID& self, const int nEventID, const std::string& strComponentName, const std::string& strFunction, const NFCDataList& arg);
-    virtual int DoHeartBeatScript(const NFGUID& self, const std::string& strHeartBeat, const float fTime, const int nCount, std::string& strComponentName, const std::string& strFunction);
+	virtual int DoEventScript(const NFGUID& self, const int nEventID, const std::string& strComponentName, const std::string& strFunction, const NFCDataList& arg);
+	virtual int DoHeartBeatScript(const NFGUID& self, const std::string& strHeartBeat, const float fTime, const int nCount, std::string& strComponentName, const std::string& strFunction);
 
-    virtual int DoScriptPropertyCallBack(const NFGUID& self, const std::string& strPropertyName, const std::string& strComponentName, const std::string& strFunction, const NFIDataList::TData& oldVar, const NFIDataList::TData& neVar);
-    virtual int DoScriptRecordCallBack(const NFGUID& self, const std::string& strRecordName, const std::string& strComponentName, const std::string& strFunction, const int nOpType, const int nRow, const int nCol, const NFCDataList::TData& oldVar, const NFCDataList::TData& newVar);
-
-protected:
-
-    int OnPropertyCommEvent(const NFGUID& self, const std::string& strPropertyName, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
-    int OnRecordCommonEvent(const NFGUID& self, const RECORD_EVENT_DATA& xEventData, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
-    int OnClassCommonEvent(const NFGUID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& var);
+	virtual int DoScriptPropertyCallBack(const NFGUID& self, const std::string& strPropertyName, const std::string& strComponentName, const std::string& strFunction, const NFIDataList::TData& oldVar, const NFIDataList::TData& neVar);
+	virtual int DoScriptRecordCallBack(const NFGUID& self, const std::string& strRecordName, const std::string& strComponentName, const std::string& strFunction, const int nOpType, const int nRow, const int nCol, const NFCDataList::TData& oldVar, const NFCDataList::TData& newVar);
 
 protected:
-    bool Regisger();
+
+	int OnPropertyCommEvent(const NFGUID& self, const std::string& strPropertyName, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
+	int OnRecordCommonEvent(const NFGUID& self, const RECORD_EVENT_DATA& xEventData, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
+	int OnClassCommonEvent(const NFGUID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& var);
 
 protected:
-    NFIElementInfoModule* m_pElementInfoModule;
-    NFIKernelModule* m_pKernelModule;
-    NFILogicClassModule* m_pLogicClassModule;
+	bool Regisger();
+
+protected:
+	NFIElementInfoModule* m_pElementInfoModule;
+	NFIKernelModule* m_pKernelModule;
+	NFILogicClassModule* m_pLogicClassModule;
 
 protected:
 	luacpp::luaWrapper lw;
