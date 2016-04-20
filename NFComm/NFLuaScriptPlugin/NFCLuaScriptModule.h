38886db560c3e70da130a21d284ce0f815be3ab6
tangyu1018@163.com <tangyu1018@163.com>
2016-04-20 22:47:37 +0800

    update NFCLuaScriptModule & fixed a typo (Resgister to Register)

--- NFComm/NFLuaScriptPlugin/NFCLuaScriptModule.h
+++ NFComm/NFLuaScriptPlugin/NFCLuaScriptModule.h
@@ -50,16 +50,22 @@ public:
 	bool AddEventCallBack(const NFGUID& self, const int nEventID, std::string& luaFunc);
 	bool AddHeartBeat(const NFGUID& self, std::string& strHeartBeatName, std::string& luaFunc, const float fTime, const int nCount);
 	int AddRow(const NFGUID& self, std::string& strRecordName, const NFCDataList& var);
-	bool DoEvent(const NFGUID& self, const int nEventID, const NFCDataList& valueList);
 
 	int OnLuaPropertyCB(const NFGUID& self, const std::string& strPropertyName, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
 	int OnLuaRecordCB(const NFGUID& self, const RECORD_EVENT_DATA& xEventData, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
 	int OnLuaHeartBeatCB(const NFGUID& self, const std::string& strHeartBeatName, const float fTime, const int nCount);
 	int OnLuaEventCB(const NFGUID& self, const int nEventID, const NFIDataList& argVar);
 	template<typename T>
-	bool AddLuaFuncToMap(NFMap<T, NFMap<NFGUID, NFList<string>>>& funcMap, const NFGUID& self, T key, string& luaFunc);
+	bool AddLuaFuncToMap(NFMap<T, NFMap<NFGUID, NFList<string>>>& funcMap, const NFGUID& self, T key, std::string& luaFunc);
 	template<typename T1, typename... T2>
 	bool CallLuaFuncFromMap(NFMap<T1, NFMap<NFGUID, NFList<string>>>& funcMap, T1 key, const NFGUID& self, T2... arg);
+
+	bool RegisterCommonPropertyEvent(std::string& funcName);
+	bool RegisterCommonRecordEvent(std::string& funcName);
+	bool RegisterCommonClassEvent(std::string& funcName);
+	bool AddLuaFuncToCommonMap(NFList<std::string>& commonList, std::string& funcName);
+	template<typename ...T>
+	bool CallLuaFuncFromCommonMap(NFList<std::string>& commonList, const NFGUID& self, T... arg);
 protected:
 	int OnPropertyCommEvent(const NFGUID& self, const std::string& strPropertyName, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
 	int OnRecordCommonEvent(const NFGUID& self, const RECORD_EVENT_DATA& xEventData, const NFIDataList::TData& oldVar, const NFIDataList::TData& newVar);
@@ -77,10 +83,14 @@ protected:
 	//luacpp::luaWrapper lw;
 	LuaContext l;
 	int64_t mnTime;
-	NFMap<string, NFMap<NFGUID, NFList<string>>> m_luaPropertyCallBackFuncMap;
-	NFMap<string, NFMap<NFGUID, NFList<string>>> m_luaRecordCallBackFuncMap;
-	NFMap<int, NFMap<NFGUID, NFList<string>>> m_luaEventCallBackFuncMap;
-	NFMap<string, NFMap<NFGUID, NFList<string>>> m_luaHeartBeatCallBackFuncMap;
+	NFMap<std::string, NFMap<NFGUID, NFList<std::string>>> m_luaPropertyCallBackFuncMap;
+	NFMap<std::string, NFMap<NFGUID, NFList<std::string>>> m_luaRecordCallBackFuncMap;
+	NFMap<int, NFMap<NFGUID, NFList<std::string>>> m_luaEventCallBackFuncMap;
+	NFMap<std::string, NFMap<NFGUID, NFList<std::string>>> m_luaHeartBeatCallBackFuncMap;
+
+	NFList<std::string> m_CommonPropertyEventFuncList;
+	NFList<std::string> m_CommonRecordEventFuncList;
+	NFList<std::string> m_CommonClassEventFuncList;
 };
 
 #endif
--- NFComm/NFPluginModule/NFIKernelModule.h
+++ NFComm/NFPluginModule/NFIKernelModule.h
@@ -85,29 +85,29 @@ public:
     //////////////////////////////////////////////////////////////////////////
     //只能网络模块注册，回调用来同步对象类事件,所有的类对象都会回调
     template<typename BaseType>
-    bool ResgisterCommonClassEvent(BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const std::string&, const CLASS_OBJECT_EVENT, const NFIDataList&))
+    bool RegisterCommonClassEvent(BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const std::string&, const CLASS_OBJECT_EVENT, const NFIDataList&))
     {
         CLASS_EVENT_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
         CLASS_EVENT_FUNCTOR_PTR functorPtr(new CLASS_EVENT_FUNCTOR(functor));
-        return ResgisterCommonClassEvent(functorPtr);
+        return RegisterCommonClassEvent(functorPtr);
     }
 
     //只能网络模块注册，回调用来同步对象属性事件,所有的类属性都会回调
     template<typename BaseType>
-    bool ResgisterCommonPropertyEvent(BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const std::string&, const NFIDataList::TData&, const NFIDataList::TData&))
+    bool RegisterCommonPropertyEvent(BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const std::string&, const NFIDataList::TData&, const NFIDataList::TData&))
     {
         PROPERTY_EVENT_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
         PROPERTY_EVENT_FUNCTOR_PTR functorPtr(new PROPERTY_EVENT_FUNCTOR(functor));
-        return ResgisterCommonPropertyEvent(functorPtr);
+        return RegisterCommonPropertyEvent(functorPtr);
     }
 
     //只能网络模块注册，回调用来同步对象类表事件,所有的类表都会回调
     template<typename BaseType>
-    bool ResgisterCommonRecordEvent(BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const RECORD_EVENT_DATA&, const NFIDataList::TData&, const NFIDataList::TData&))
+    bool RegisterCommonRecordEvent(BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const RECORD_EVENT_DATA&, const NFIDataList::TData&, const NFIDataList::TData&))
     {
         RECORD_EVENT_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
         RECORD_EVENT_FUNCTOR_PTR functorPtr(new RECORD_EVENT_FUNCTOR(functor));
-        return ResgisterCommonRecordEvent(functorPtr);
+        return RegisterCommonRecordEvent(functorPtr);
     }
 
 
@@ -191,13 +191,13 @@ protected:
 protected:
 
     //只能网络模块注册，回调用来同步对象类事件,所有的类对象都会回调
-    virtual bool ResgisterCommonClassEvent(const CLASS_EVENT_FUNCTOR_PTR& cb) = 0;
+    virtual bool RegisterCommonClassEvent(const CLASS_EVENT_FUNCTOR_PTR& cb) = 0;
 
     //只能网络模块注册，回调用来同步对象属性事件,所有的类属性都会回调
-    virtual bool ResgisterCommonPropertyEvent(const PROPERTY_EVENT_FUNCTOR_PTR& cb) = 0;
+    virtual bool RegisterCommonPropertyEvent(const PROPERTY_EVENT_FUNCTOR_PTR& cb) = 0;
 
     //只能网络模块注册，回调用来同步对象类表事件,所有的类表都会回调
-    virtual bool ResgisterCommonRecordEvent(const RECORD_EVENT_FUNCTOR_PTR& cb) = 0;
+    virtual bool RegisterCommonRecordEvent(const RECORD_EVENT_FUNCTOR_PTR& cb) = 0;
 };
 
 #endif
