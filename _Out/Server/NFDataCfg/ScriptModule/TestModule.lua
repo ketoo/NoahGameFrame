eab834e65b0e34d78bb320c0ea6a36899771190f
tangyu1018@163.com <tangyu1018@163.com>
2016-04-20 22:47:37 +0800

    update NFCLuaScriptModule & fixed a typo (Resgister to Register)

--- _Out/Server/NFDataCfg/ScriptModule/TestModule.lua
+++ _Out/Server/NFDataCfg/ScriptModule/TestModule.lua
@@ -19,6 +19,10 @@ end
 function TestModule:AfterInit()
 	io.write("Lua AfterInit!" .. tostring(pLuaScriptModule) .. "\n");
 
+	pLuaScriptModule:RegisterCommonPropertyEvent("TestModule.OnPropertyCommEvent");
+	pLuaScriptModule:RegisterCommonRecordEvent("TestModule.OnRecordCommonEvent");
+	pLuaScriptModule:RegisterCommonClassEvent("TestModule.OnClassCommonEvent");
+
 	local pKernelModule = pPluginManager:FindKernelModule("NFCKernelModule");
 	pKernelModule:CreateScene(1);
 
@@ -54,7 +58,7 @@ function TestModule:AfterInit()
 
 	obj:AddObject(ident);
 
-	pLuaScriptModule:DoEvent(OID, 1, obj);
+	pKernelModule:DoEvent(OID, 1, obj);
 
 	--Hearback
 	pLuaScriptModule:AddHeartBeat(OID, "strHeartBeatName", "TestModule.HearCallBack", 2, 55555);
@@ -65,7 +69,6 @@ function TestModule.MaxPropertyCallBack(self, propertyName, oldVar, newVar)
 	local nNewVar = newVar:GetInt();
 
 	local obj = NFCDataList();
-	--io.write("Hello Lua MaxPropertyCallBack oldVar:".. nOldVar .." newVar:" .. nNewVar .. "\n");
 	io.write("Hello Lua MaxPropertyCallBack oldVar:" .. tostring(nOldVar) .. " newVar:" .. tostring(nNewVar) .. "\n");
 end
 
@@ -113,15 +116,15 @@ function TestModule:HearCallBack(self, strHeartBeat, fTime, nCount)
 end
 
 function TestModule.OnClassCommonEvent(self, strClassName, eventID, varData)
-	--io.write("onClassCommonEvent, ClassName: " .. tostring(strClassName) .. " EventID: " .. tostring(eventID) .. "\n");
+	io.write("onClassCommonEvent, ClassName: " .. tostring(strClassName) .. " EventID: " .. tostring(eventID) .. "\n");
 end
 
-function TestModule.OnRecordCommonEvent(self, xEventData, oldVar, newVar)
-	--io.write("OnRecordCommonEvent, self: " .. tostring(self) .. " xEventData: " .. tostring(xEventData) .. " oldVar: " .. tostring(oldVar) .. " newVar: " .. tostring(newVar) .. "\n");
+function TestModule.OnRecordCommonEvent(self, recordName, nOpType, nRow, nCol, oldVar, newVar)
+	io.write("OnRecordCommonEvent, self: " .. tostring(self) .. " nOpType: " .. tostring(nOpType) .. " oldVar: " .. tostring(oldVar) .. " newVar: " .. tostring(newVar) .. "\n");
 end
 
 function TestModule.OnPropertyCommEvent(self, strPropertyName, oldVar, newVar)
-	--io.write("OnPropertyCommEvent, self: " .. tostring(self) .. " strPropertyName: " .. tostring(strPropertyName) .. " oldVar: " .. tostring(oldVar) .. " newVar: " .. tostring(newVar) .. "\n");
+	io.write("OnPropertyCommEvent, self: " .. tostring(self) .. " strPropertyName: " .. tostring(strPropertyName) .. " oldVar: " .. tostring(oldVar) .. " newVar: " .. tostring(newVar) .. "\n");
 end
 
 function TestModule:Execute()
@@ -135,59 +138,3 @@ end
 function TestModule:Shut()
 	io.write("Lua Shut!\n");
 end
---[[
-function TestModule:AfterInit(kernel, self)
-	io.write("Hello Lua AfterInit!\n");
-
-	if kernel == nil then
-		io.write("kernel is nil\n");
-	end
-
-	if self == nil then
-		io.write("self is nil\n");
-	end
-
-	io.write("Start use C++ functions\n");
-	nMaxhp = GetPropertyInt(kernel, self, "MAXHP");
-	--io.write("Hello Lua MAXHP:" ..nMaxhp .. "\n");
-
-	--property callback
-	AddPropertyCallBack(kernel, self, "MAXHP", "TestModule", "MaxPropertyCallBack");
-	local maxHP = 400;
-	SetPropertyInt(kernel, self, "MAXHP", maxHP);
-
-	nMaxhp = GetPropertyInt(kernel, self, "MAXHP");
-	--io.write("Hello Lua GetPropertyInt MAXHP:" ..nMaxhp .. "\n");
-
-	--record callback
-	AddRecordCallBack(kernel, self, "TaskList","TestModule", "TaskListCallBack");
-
-	local varTask =  NFCDataList();
-	varTask:AddString("Task_From_Lua");
-	varTask:AddInt(1);
-	varTask:AddInt(1);
-
-	AddRow(kernel, self, "TaskList", varTask);
-	SetRecordInt(kernel, self, "TaskList", 0, 1, 3);
-	SetRecordString(kernel, self, "TaskList", 0, 0, "NewStr_Task_From_Lua");
-	--event callback
-	AddEventCallBack(kernel, self, 1, "TestModule", "EventCallBack");
-
-	local obj = NFCDataList();
-	obj:AddInt(21);
-	obj:AddFloat(22.5);
-	obj:AddString("str23");
-
-	local ident = NFGUID();
-	ident:SetHead(241);
-	ident:SetData(242);
-
-	obj:AddObject(ident);
-
-	io.write("aaaaaaaaaaaaaaaaaa\n");
-	DoEvent(kernel, self, 1, obj);
-
-	--Hearback
-	AddHeartBeat(kernel, self, "strHeartBeatName","TestModule", "HearCallBack", 1, 55);
-end
---]]
