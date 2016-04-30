926cf5bf503db8983a695d1dea8c98978de48ddf
tangyu1018@163.com <tangyu1018@163.com>
2016-05-01 02:20:29 +0800

    update NFFileProcess
    fixed bugs and support UTF8 for reading xml files.

--- NFTools/NFFileProcess/main.cpp
+++ NFTools/NFFileProcess/main.cpp
@@ -2,9 +2,7 @@
 #include <iostream>
 #include <chrono>
 #include <ctime>
-#include <cstdint>
-#include <vector>
-#include <string>
+
 
 int main(int argc, const char *argv[])
 {
@@ -16,12 +14,9 @@ int main(int argc, const char *argv[])
 		fp->OnCreateMysqlFile();
 		auto t2 = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 		std::cout << "Generate All Done for Good!!!" << std::endl;
-		std::cout << "Timespan:" << t2 - t1 << "us" << std::endl;
+		std::cout << "Timespan: " << t2 - t1 << " us (" << (t2 - t1) / 1000000.0f << "s)" << std::endl;
 		delete fp;
 	}
-
-
-
 	std::cin.ignore();
 	return 0;
 }
