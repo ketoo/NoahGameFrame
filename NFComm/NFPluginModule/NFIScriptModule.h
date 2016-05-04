b63f19e40427ba991b6a5d1c23aec7076a3c136d
kytooooo <342006@qq.com>
2016-05-04 17:48:53 +0800

    remove unused code

--- NFComm/NFPluginModule/NFIScriptModule.h
+++ NFComm/NFPluginModule/NFIScriptModule.h
@@ -19,59 +19,8 @@
 #include "NFComm/NFPluginModule/NFILuaScriptModule.h"
 #include "NFComm/NFPluginModule/NFIPluginManager.h"
 
-class NFCScriptName
-{
-public:
-	bool operator < (const NFCScriptName& id) const
-	{
-		return this->strComponentName.length() < id.strComponentName.length() && this->strFunctionName.length() < id.strFunctionName.length();
-	}
-
-	bool operator == (const NFCScriptName& id) const
-	{
-		if (this->strComponentName != id.strComponentName
-			|| this->strFunctionName != id.strFunctionName)
-		{
-			return false;
-		}
-
-		return true;
-	}
-
-	NFCScriptName() {}
-
-	NFCScriptName(const std::string& strComponentName, const std::string& strFunctionName)
-	{
-		this->strComponentName = strComponentName;
-		this->strFunctionName = strFunctionName;
-	}
-
-	std::string strComponentName;
-	std::string strFunctionName;
-};
-
-
-class NFCSriptData
-{
-public:
-	//eventlist: id->ScriptName,FunctionName
-	NFMap<int, NFList<NFCScriptName>> mxEventData;
-
-	//heartbeatlist: name->ScriptName,FunctionName
-	NFMap<std::string, NFList<NFCScriptName>> mxHeartBeatData;
-
-	//propertycblist: name->ScriptName,FunctionName
-	NFMap<std::string, NFList<NFCScriptName>> mxPropertyCallBackData;
-
-	//recordcblist: name->ScriptName,FunctionName
-	NFMap<std::string, NFList<NFCScriptName>> mxRecordCallBackData;
-
-protected:
-private:
-};
-
 class NFIScriptModule
-	: public NFILogicModule, public NFMap<NFGUID, NFCSriptData>
+	: public NFILogicModule
 {
 
 public:
