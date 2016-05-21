b725b732ba99747330be1663da9e92dcbde1b5a8
flyicegood <flyicegood@163.com>
2016-05-21 18:37:25 +0800

    Add Redis Module

--- NFComm/NFDataNoSqlPlugin/dllmain.cpp
+++ NFComm/NFDataNoSqlPlugin/dllmain.cpp
@@ -1,4 +1,4 @@
-#include "NFComm/NFCore/NFPlatform.h"
+//#include "NFComm/NFCore/NFPlatform.h"
 
 /*
 int main()
@@ -104,23 +104,27 @@ std::cout << valueRoleRecordValueList.String(i) << std::endl;
 return 0;
 }
 */
-#ifdef NF_DEBUG_MODE
+
+#ifdef _DEBUG
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
 #pragma comment( lib, "NFCore_d.lib" )
 #pragma comment( lib, "anet_win64_d.lib" )
+
+
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
 #pragma comment( lib, "NFCore_d.a" )
+
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
 #else
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
-#pragma comment( lib, "ws2_32" )
 #pragma comment( lib, "NFCore.lib" )
 #pragma comment( lib, "anet_win64.lib" )
+
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
 #pragma comment( lib, "NFCore.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
