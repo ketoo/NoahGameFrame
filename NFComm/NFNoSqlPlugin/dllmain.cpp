49e5614afaf74bab325d1fae1e81e275f0cfd206
NickYang <elite_yang@163.com>
2016-06-01 10:42:30 +0800

    remove lib

--- NFComm/NFNoSqlPlugin/dllmain.cpp
+++ NFComm/NFNoSqlPlugin/dllmain.cpp
@@ -109,12 +109,12 @@ return 0;
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "NFCore_d.lib" )
+//#pragma comment( lib, "NFCore_d.lib" )
 #pragma comment( lib, "anet_win64_d.lib" )
 
 
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "NFCore_d.a" )
+//#pragma comment( lib, "NFCore_d.a" )
 
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
@@ -122,11 +122,11 @@ return 0;
 #else
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
-#pragma comment( lib, "NFCore.lib" )
+//#pragma comment( lib, "NFCore.lib" )
 #pragma comment( lib, "anet_win64.lib" )
 
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "NFCore.a" )
+//#pragma comment( lib, "NFCore.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
