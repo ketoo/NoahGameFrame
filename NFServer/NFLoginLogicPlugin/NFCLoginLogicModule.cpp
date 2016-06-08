8b3a148b992bbd00b7bbef7b6e7ff1311677d503
kytooooo <342006@qq.com>
2016-06-08 18:53:37 +0800

    remove unused plugin--->dev remove unused module-->dev

--- NFServer/NFLoginLogicPlugin/NFCLoginLogicModule.cpp
+++ NFServer/NFLoginLogicPlugin/NFCLoginLogicModule.cpp
@@ -23,20 +23,7 @@ bool NFCLoginLogicModule::Shut()
 
 int NFCLoginLogicModule::OnLoginProcess(const NFGUID& object, const std::string& strAccount, const std::string& strPwd)
 {
-    int64_t nState = -1;
-
-    std::vector<std::string> xFieldVec;
-    std::vector<std::string> xValueVec;
-    xFieldVec.push_back("Password");
-
-    if (m_pClusterSqlModule->Query("AccountInfo", strAccount, xFieldVec, xValueVec)
-        && xFieldVec.size() == xValueVec.size()
-        && strPwd == xValueVec[0])
-    {
-        nState = 0;
-    }
-
-    return nState;
+	return 0;
 }
 
 bool NFCLoginLogicModule::Execute()
@@ -49,10 +36,8 @@ bool NFCLoginLogicModule::Execute()
 
 bool NFCLoginLogicModule::AfterInit()
 {
-    m_pClusterSqlModule = pPluginManager->FindModule<NFIMysqlModule>("NFCMysqlModule");
     m_pLoginNet_ServerModule = pPluginManager->FindModule<NFILoginNet_ServerModule>("NFCLoginNet_ServerModule");
 
-    assert(NULL != m_pClusterSqlModule);
     assert(NULL != m_pLoginNet_ServerModule);
 
     return true;
