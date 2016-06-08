345e9be41a49d240dbe849d6d7f77a21d63c5424
tangyu1018@163.com <tangyu1018@163.com>
2016-06-08 22:07:42 +0800

    update

--- NFTools/NFFileProcess/FileProcess.h
+++ NFTools/NFFileProcess/FileProcess.h
@@ -24,20 +24,6 @@
 #include <cstdio>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <termios.h>
-
-int getch() {
-	struct termios oldt,
-		newt;
-	int ch;
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	ch = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	return ch;
-}
 #endif
 
 
