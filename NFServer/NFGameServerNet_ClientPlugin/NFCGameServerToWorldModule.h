258912c93b95cb98db536d62d0de4d9de7d44186
flyicegood <flyicegood@163.com>
2016-06-09 12:21:03 +0800

    remove unuse code

--- NFServer/NFGameServerNet_ClientPlugin/NFCGameServerToWorldModule.h
+++ NFServer/NFGameServerNet_ClientPlugin/NFCGameServerToWorldModule.h
@@ -36,10 +36,9 @@ public:
 
     virtual bool AfterInit();
 
+    virtual void SendBySuit(const int& nHashKey, const int nMsgID, const char* msg, const uint32_t nLen);
 
-    virtual void LogReceive(const char* str) {}
-    virtual void LogSend(const char* str) {}
-
+    virtual NFIClusterClientModule* GetClusterClientModule();
 protected:
 
     void OnSocketWSEvent(const int nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet);
@@ -48,51 +47,13 @@ protected:
     void Register(NFINet* pNet);
     void RefreshWorldInfo();
 
-    void OnLoadRoleDataBeginProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-
-    void OnLoadRoleDataFinalProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-
-    void OnEnquireSceneInfoProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-
-    void OnSwapGSProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-    void OnAckCreateGuildProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-    void OnAckJoinGuildProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-    void OnAckLeaveGuildProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-
-    void OnAckCreateChatGroupProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-    void OnAckJoinChatGroupProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-    void OnAckQuitChatGroupProcess(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-
-
-    int OnDataLoadBeginEvent(const NFGUID& object, const int nEventID, const NFIDataList& var);
-
-    //int OnToWorldEvent( const NFGUID& object, const int nEventID, const NFIDataList& var );
-
-    int OnSwapGSEvent(const NFGUID& object, const int nEventID, const NFIDataList& var);
-
-    int OnClassCommonEvent(const NFGUID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& var);
-
-
     int OnObjectClassEvent(const NFGUID& self, const std::string& strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFIDataList& var);
 
-    //     template<class PBClass>
-    //     int TransPBToProxy(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
     void TransPBToProxy(const int nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
-
-	virtual void SendBySuit(const int& nHashKey, const int nMsgID, const char* msg, const uint32_t nLen);
-
-    virtual void LogServerInfo(const std::string& strServerInfo);
 
 private:
     void SendOnline(const NFGUID& self);
     void SendOffline(const NFGUID& self);
-
-    void CreateChatGroup(const NFGUID& self, const int nChatType, const std::string& strName);
-    void JoinChatGroup(const NFGUID& self, const NFGUID& xGroup, const int nChatType);
-    void QuitChatGroup(const NFGUID& self, const NFGUID& xGroup);
-    void SubscriptionChatGroup(const NFGUID& self, const NFGUID& xGroup);
-    void CancelSubscriptionChatGroup(const NFGUID& self, const NFGUID& xGroup);
-
 
 private:
 
