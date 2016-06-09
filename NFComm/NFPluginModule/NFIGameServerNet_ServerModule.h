da0993bed26d025ee04cfb2009560cbb9128729d
flyicegood <flyicegood@163.com>
2016-06-09 14:14:17 +0800

    modify NFCGameServerNet_ServerModule

--- NFComm/NFPluginModule/NFIGameServerNet_ServerModule.h
+++ NFComm/NFPluginModule/NFIGameServerNet_ServerModule.h
@@ -17,9 +17,41 @@ class NFIGameServerNet_ServerModule
     : public NFILogicModule
 {
 public:
+    //要管理当前所有的对象所在的actor,gateid,fd等
+    struct GateBaseInfo
+    {
+        GateBaseInfo()
+        {
+            nActorID = 0;
+            nGateID = 0;
+        }
+
+        GateBaseInfo(const int gateID, const NFGUID xIdent)
+        {
+            nActorID = 0;
+            nGateID = gateID;
+            xClientID = xIdent;
+        }
+
+        int nActorID;
+        int nGateID;
+        NFGUID xClientID;
+    };
+
+    struct GateServerInfo
+    {
+        ServerData xServerData;
+        //此网关上所有的对象<角色ID,gate_FD>
+        std::map<NFGUID, int> xRoleInfo;
+    };
+
+public:
 	virtual NFINetModule* GetNetModule() = 0;
 	virtual void SendMsgPBToGate(const uint16_t nMsgID, google::protobuf::Message& xMsg, const NFGUID& self) = 0;
 	virtual void SendMsgPBToGate(const uint16_t nMsgID, const std::string& strMsg, const NFGUID& self) = 0;
+    virtual bool AddPlayerGateInfo(const NFGUID& nRoleID, const NFGUID& nClientID, const int nGateID) = 0;
+    virtual bool RemovePlayerGateInfo(const NFGUID& nRoleID) = 0;
+    virtual NF_SHARE_PTR<GateBaseInfo> GetPlayerGateInfo(const NFGUID& nRoleID) = 0;
 };
 
 #endif
