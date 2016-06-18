75c2103862935201cf053a0a303c5203e4462082
tangyu1018@163.com <tangyu1018@163.com>
2016-06-18 16:00:34 +0800

    update

--- install.bat
+++ install.bat
@@ -1,5 +1,11 @@
 cd Dependencies
 call build_dep.bat
 
+call BuildNF.CMake.bat
+cd ..
+call BuildNF.VS2015.Debug.X64.bat
+cd ..
+
 cd _Out\Server\NFDataCfg\tool\
+start NFAutoCopyDll.exe
 start NFFileProcess.exe
