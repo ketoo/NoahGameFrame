fdf4c1d3d30a0f06e4a94dd56d52bf9205191dde
tangyu1018@163.com <tangyu1018@163.com>
2016-06-20 15:45:53 +0800

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,20 @@ chmod -R 755 build_dep.sh
 ./build_dep.sh
 
 cd ../
+
+cd ./cmake.linux/
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+make
+
+cd ..
+cd _Out/Server/NFDataCfg/Tool/
+./NFFileProcess
+./NFAutoCopyDll
+
+cd ..
+cd ..
+cd ..
+cd ..
 pwd
 
 # generate makefile
