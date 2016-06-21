ab66cb253ff1eca706cfb12a246a5fcc939ba695
tangyu1018@163.com <tangyu1018@163.com>
2016-06-21 13:53:02 +0800

    update NFFileProcess

--- NFTools/NFFileProcess/FileProcess.cpp
+++ NFTools/NFFileProcess/FileProcess.cpp
@@ -523,7 +523,9 @@ bool FileProcess::CreateStructXML(std::string strFile, std::string strFileName)
 
 				if (nExcelColsNum != nRealColNum)
 				{
-					printf_s("This Excel[%s]'s format is something wrong, Record[%s] field \"col\"==%d not equal the real cols==%d!", strFile.c_str(), strRecordName.c_str(), nExcelColsNum, nRealColNum);
+					printf_s("This Excel[%s]'s format is something wrong, Record[%s] field \"col\"==%d not equal the real cols==%d!\n", strFile.c_str(), strRecordName.c_str(), nExcelColsNum, nRealColNum);
+					printf_s("Press any key to exit!");
+					getch();
 					exit(1);
 				}
 
