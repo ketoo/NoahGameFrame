10daf7caa42a0a3d6546b8be5645fef9f4dd5ae7
tangyu1018@163.com <tangyu1018@163.com>
2016-06-22 12:02:24 +0800

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ chmod -R 755 build_dep.sh
 ./build_dep.sh
 cd ../
 
+cd BuildScript
+chmod -R 755 ./BuildNF.CMake.Tools.sh
+chmod -R 755 ./BuildNF.Tools.Linux.Debug.sh
+./BuildNF.CMake.Tools.sh
+./BuildNF.Tools.Linux.Debug.sh
+cd ..
+
 cd _Out/Server/NFDataCfg/Tool/
 chmod 755 ./NFFileProcess
 chmod 755 ./copy_files.sh
