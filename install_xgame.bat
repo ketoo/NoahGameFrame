96d611e7f3be641b75499360851df1a4f283e82a
tangyu1018@163.com <tangyu1018@163.com>
2016-06-25 23:50:47 +0800

    fixed install_xgame.bat

--- install_xgame.bat
+++ install_xgame.bat
@@ -1,1 +1,23 @@
 xcopy cmake.dir\x-game\*.* *.txt /e /y
+
+cd Dependencies
+call build_dep.bat
+
+cd BuildScript
+call BuildNF.CMake.Tools.bat
+call BuildNF.Tools.VS2015.Debug.X64.bat
+cd ..
+
+cd _Out\Server\NFDataCfg\tool\
+call copy_files.bat 1
+cd ..
+cd ..
+cd ..
+cd ..
+
+cd BuildScript
+call BuildNF.CMake.bat
+call BuildNF.VS2015.Debug.X64.bat
+
+call BuildNF.CopyDll.bat
+cd ..
