942fc246cac453bff2325e194815a279e31935f5
tangyu1018 <tangyu1018@163.com>
2016-07-01 15:38:04 +0800

    fixed some error

--- NFComm/NFCore/NFIObject.h
+++ NFComm/NFCore/NFIObject.h
@@ -66,7 +66,7 @@ public:
     template <typename T>
     NF_SHARE_PTR<T> AddComponent(const std::string& strComponentName)
     {
-		NF_SHARE_PTR<T> pComponent = FindComponent(strComponentName);
+		NFIComponent* pComponent = GetComponentManager()->FindComponent<NFIComponent>(strComponentName);
         if (pComponent)
         {
             NF_SHARE_PTR<T> pNewCOmponent = pComponent->CreateNewInstance<T>();
