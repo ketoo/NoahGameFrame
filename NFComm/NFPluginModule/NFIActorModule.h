a48b26f244913f45712e1297ad1b5141dfd4567a
kytooooo <342006@qq.com>
2016-07-03 17:54:05 +1200

    actor

---
+++ NFComm/NFPluginModule/NFIActorModule.h
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFCActorModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2012-12-15
+//    @Module           :    NFCActorModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+#ifndef NFI_ACTOR_MODULE_H
+#define NFI_ACTOR_MODULE_H
+
+#include "NFILogicModule.h"
+
+class NFIActorModule
+    : public NFILogicModule
+{
+public:
+};
+
+#endif // !NFI_UUID_MODULE_H
