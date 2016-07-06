34ca33909f2b778087897c52957911763c7c4f9e
tangyu1018@163.com <tangyu1018@163.com>
2016-07-06 16:28:18 +0800

    install_xgame.sh support centos

--- install_xgame.sh
+++ install_xgame.sh
@@ -10,7 +10,7 @@ fi
 rm -rf ./cmake.dir/tmp
 mkdir ./cmake.dir/tmp
 cp -R -f ./cmake.dir/X-GAME/* ./cmake.dir/tmp/
-find ./cmake.dir/tmp -name "*.text" -exec rename -f 's/.text/.txt/' {} \;
+python -mplatform | grep -qi Ubuntu && find ./cmake.dir/tmp -name "*.text" -exec rename -f 's/.text/.txt/' {} \; || find ./cmake.dir/tmp -name "*.text" -exec rename .text .txt {} \;
 cp -R -f ./cmake.dir/tmp/* ./
 
 # compile dep libraries
