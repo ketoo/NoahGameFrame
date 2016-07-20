e4a25b2e396d374f5030bc8cbaa579843589a89a
ketoo <342006@qq.com>
2016-07-20 17:05:18 +1200

    add shell for vs and cb

--- install4vs.bat
+++ install4vs.bat
@@ -1,23 +1,7 @@
-xcopy cmake.dir\develop\*.* *.txt /e /y
-
 cd Dependencies
 call build_dep.bat
-
-cd BuildScript
-call BuildNF.CMake.Tools.bat
-call BuildNF.Tools.VS2015.Debug.X64.bat
 cd ..
-
-cd _Out\Server\NFDataCfg\tool\
-call copy_files.bat 1
+cd _Out\Server\NFDataCfg\Tool\
+start NFFileProcess.exe
 cd ..
-cd ..
-cd ..
-cd ..
-
-cd BuildScript
-call BuildNF.CMake.Debug.bat
-call BuildNF.VS2015.Debug.X64.bat
-
-call BuildNF.CopyDll.bat
-cd ..
+call GenerateConfigXML.bat
