68194c250688e05bca5c5d0e8c4aec8e51c07bff
ketoo <342006@qq.com>
2016-08-10 19:55:45 +1200

    remove unused files

--- NFComm/NFPluginLoader/NFCPluginActor.cpp
+++
@@ -1,9 +0,0 @@
-// -------------------------------------------------------------------------
-//    @FileName      :    NFCPluginActor.cpp
-//    @Author           :    LvSheng.Huang
-//    @Date             :    2012-12-15
-//    @Module           :    NFCPluginActor
-//
-// -------------------------------------------------------------------------
-
-#include "NFCPluginActor.h"
