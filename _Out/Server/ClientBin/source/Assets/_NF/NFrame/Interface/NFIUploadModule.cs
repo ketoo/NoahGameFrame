b212432c52d8595e3bf804e228967663603a7db0
cherish <windzxa@gmail.com>
2016-08-26 14:07:15 +0800

    NFCUploadModule

---
+++ _Out/Server/ClientBin/source/Assets/_NF/NFrame/Interface/NFIUploadModule.cs
@@ -0,0 +1,17 @@
+//-----------------------------------------------------------------------
+// <copyright file="NFIElementModule.cs">
+//     Copyright (C) 2015-2015 lvsheng.huang <https://github.com/ketoo/NFrame>
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NFrame
+{
+    public abstract class NFIUploadModule : NFILogicModule
+    {
+
+    }
+}
