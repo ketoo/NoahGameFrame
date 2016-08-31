9bff77448ea0522d22ccf5b214d29ed5a54bba48
cherish <windzxa@gmail.com>
2016-08-31 10:57:34 +0800

    server:upload funciton

--- NFComm/NFKernelPlugin/NFKernelModule/NFCKernelModule.cpp
+++ NFComm/NFKernelPlugin/NFKernelModule/NFCKernelModule.cpp
@@ -202,6 +202,7 @@ NF_SHARE_PTR<NFIObject> NFCKernelModule::CreateObject(const NFGUID& self, const 
              xRecord->SetPrivate(pConfigRecordInfo->GetPrivate());
              xRecord->SetSave(pConfigRecordInfo->GetSave());
              xRecord->SetCache(pConfigRecordInfo->GetCache());
+			 xRecord->SetUpload(pConfigRecordInfo->GetUpload());
 
 
 
