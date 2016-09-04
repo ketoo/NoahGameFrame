09cc9552eda7c55777018d5987ba39713e742f2b
cherish <windzxa@gmail.com>
2016-09-04 17:35:20 +0800

    nftime optimize

--- NFComm/NFCore/NFCalendar.h
+++ NFComm/NFCore/NFCalendar.h
@@ -5,38 +5,59 @@
 #include <string>
 #include "NFComm/NFCore/NFIObject.h"
 
+enum CalendarType
+{
+	//-----------BASE-----------
+	YEAR,
+	MONTH,//start by 0
+	DAY,
+	HOUR,
+	MINUTE,
+	SECOND,
+	MILLISECOND,
+	//------------EX-------------
+	WEEK_SINCE_EPOCH,//start by 0
+	DAY_SINCE_EPOCH,//start by 0
+	HOUR_SINCE_EPOCH,//start by 0
+	DAY_OF_WEEK,//start by 1 ,1 means Sunday
+};
 class NFCalendar
 {
 public:
-	enum TYPE
-	{
-		//-----------BASE-----------
-		YEAR,
-		MONTH,//start by 0
-		DAY,
-		HOUR,
-		MINUTE,
-		SECOND,
-		MILLISECOND,
-		//------------EX-------------
-		WEEK_SINCE_EPOCH,//start by 0
-		DAY_SINCE_EPOCH,//start by 0
-		HOUR_SINCE_EPOCH,//start by 0
-		DAY_OF_WEEK,//start by 1 ,1 means Sunday
-	};
+
+private:
+	static const NFINT64 WEEK = 7 * 24 * 60 * 60 * 1000L;
+	static const NFINT64 DAY = 24 * 60 * 60 * 1000L;
+	static const NFINT64 HOUR = 60 * 60 * 1000L;
+	static const NFINT64 MINUTE = 60 * 1000L;
+	static const NFINT64 SECOND = 1000L;
+	static const NFINT64 MILLISECOND = 1L;
+	static const int mnDefaultTimeZone = 8;
 public:
 	NFCalendar();
 	NFCalendar(NFINT64 nTime);
-	NFCalendar(NFINT64 nTime, NFINT32 nTimeZone);
+	NFCalendar(NFINT64 nTime, int nTimeZone);
+	NFCalendar(std::string strTime);
+	NFCalendar(std::string strTime, int nTimeZone);
 
-	NFINT64 Get(TYPE type);
-private:
-	NFINT64 GetFixTime();;//add timeZone value
-private:
-	static const NFINT32 mnDefaultTimeZone = 8;
 
-	NFINT32 mnTimeZone;
+	NFINT64 GetTime();
+	std::string GetStr();
+	NFINT64 Get(CalendarType type);
+	int GetTimeZone();
+
+	void SetTime(NFINT64 nTime);
+	void SetTimeZone(int nTimeZone);
+
+private:
+	void NFCalendar::InitWithYMDHMSM(std::string strTime);
+	NFINT64 GetFixTime();//add timeZone value
+	bool IsLeapYear(int nYear);
+	int split(const string& str, vector<string>& ret_, string sep);
+
+private:
 	NFINT64 mnTime;
+	int mnTimeZone;
 };
 
 #endif
