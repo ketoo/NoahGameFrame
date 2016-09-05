d94c64fd2a821415c1cd49aaec399708f2b19577
ketoo <342006@qq.com>
2016-09-04 20:36:33 -0700

    readd eventmodule remove g3dlite

---
+++ NFComm/NFKernelPlugin/NFCEventModule.cpp
@@ -0,0 +1,34 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFCEventModule.cpp
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2012-12-15
+//    @Module           :    NFCEventModule
+//
+// -------------------------------------------------------------------------
+
+#include "NFCEventModule.h"
+
+bool NFCEventModule::Init()
+{
+    return true;
+}
+
+bool NFCEventModule::AfterInit()
+{
+    return true;
+}
+
+bool NFCEventModule::BeforeShut()
+{
+    return true;
+}
+
+bool NFCEventModule::Shut()
+{
+    return true;
+}
+
+bool NFCEventModule::Execute()
+{
+    return true;
+}
