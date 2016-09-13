fc52643e7032f0aac8e78b7c8ad22c979494f2f7
cherish <windzxa@gmail.com>
2016-09-13 14:11:35 +0800

    cmake gen codeblock

--- BuildScript/BuildNF.CMake.Tools.sh
+++ BuildScript/BuildNF.CMake.Tools.sh
@@ -1,5 +1,5 @@
 cd ..
 cd ./cmake.tools/
-cmake .. -G "Unix Makefiles" -DBUILD_TOOLS_ONLY=ON -DCMAKE_BUILD_TYPE=Debug
+cmake .. -G "CodeBlocks - Unix Makefiles" -DBUILD_TOOLS_ONLY=ON -DCMAKE_BUILD_TYPE=Debug
 cd ..
 cd BuildScript
