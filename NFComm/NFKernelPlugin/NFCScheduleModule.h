fdfe4f42a7cf8f1084b04b4a2c17c7cc876ed801
ketoo <342006@qq.com>
2016-09-24 17:49:22 -0700

    fixed for event module

--- NFComm/NFKernelPlugin/NFCScheduleModule.h
+++ NFComm/NFKernelPlugin/NFCScheduleModule.h
@@ -7,6 +7,32 @@
 #include "NFComm/NFCore/NFIDataList.h"
 #include "NFIScheduleModule.h"
 
+class  NFCScheduleElement
+	: public NFList<HEART_BEAT_FUNCTOR_PTR>
+{
+public:
+	NFCScheduleElement()
+	{
+		mstrScheduleName = "";
+		mfIntervalTime = 0.0f;
+		mnNextTriggerTime = 0;
+		mnRemainCount = 0;
+	};
+
+	virtual ~NFCScheduleElement()
+	{
+	}
+
+	void DoHeartBeatEvent();
+
+	std::string mstrScheduleName;
+	float mfIntervalTime;
+	NFINT64 mnNextTriggerTime;
+	int mnRemainCount;
+
+	NFGUID self;
+};
+
 class NFCScheduleModule : public NFIScheduleModule
 {
 public:
@@ -19,7 +45,7 @@ public:
 
 	virtual bool AddSchedule(const NFGUID self, const std::string& strScheduleName, const HEART_BEAT_FUNCTOR_PTR& cb, const float fTime, const int nCount);
 	virtual bool RemoveSchedule(const NFGUID self, const std::string& strScheduleName);
-	virtual bool FindSchedule(const NFGUID self, const std::string& strScheduleName);
+	virtual bool ExistSchedule(const NFGUID self, const std::string& strScheduleName);
 
 protected:
 	std::map<std::string, NFMapEx <std::string, NFCScheduleElement >> mScheduleMap;//guid_scheduleName_element
