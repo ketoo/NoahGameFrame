408cc6a67a3b662e55533fdbd7e83053bce10f8a
ketoo <342006@qq.com>
2016-09-24 22:59:04 -0700

    fixed bug for schedule module

--- NFComm/NFKernelPlugin/NFCScheduleModule.cpp
+++ NFComm/NFKernelPlugin/NFCScheduleModule.cpp
@@ -19,7 +19,7 @@ NFCScheduleModule::NFCScheduleModule(NFIPluginManager* p)
 
 NFCScheduleModule::~NFCScheduleModule()
 {
-	mScheduleMap.clear();
+	mScheduleMap.ClearAll();
 }
 
 bool NFCScheduleModule::Init()
@@ -29,10 +29,11 @@ bool NFCScheduleModule::Init()
 bool NFCScheduleModule::Execute()
 {
 	//execute every schedule
-	for (std::map<std::string, NFMapEx <std::string, NFCScheduleElement >>::iterator it = mScheduleMap.begin(); it != mScheduleMap.end(); ++it)
+	NF_SHARE_PTR<NFMapEx <std::string, NFCScheduleElement >> xObjectSchedule = mScheduleMap.First();
+	while (xObjectSchedule)
 	{
-		NFMapEx<std::string, NFCScheduleElement> nameScheduleMap = it->second;
-		NF_SHARE_PTR<NFCScheduleElement> pSchedule = nameScheduleMap.First();
+		std::string str;
+		NF_SHARE_PTR<NFCScheduleElement> pSchedule = xObjectSchedule->First();
 		while (pSchedule)
 		{
 			NFINT64 nNow = NFTime::GetTime();
@@ -40,30 +41,36 @@ bool NFCScheduleModule::Execute()
 			{
 				pSchedule->mnRemainCount--;
 				pSchedule->DoHeartBeatEvent();
+
 				if (pSchedule->mnRemainCount <= 0)
 				{
-					mRemoveList.insert(std::map<std::string, std::string>::value_type(pSchedule->self.ToString(),pSchedule->mstrScheduleName));
+					mRemoveList.insert(std::map<NFGUID, std::string>::value_type(pSchedule->self, pSchedule->mstrScheduleName));
 				}
 				else
 				{
-					pSchedule->mnNextTriggerTime += NFINT64(pSchedule->mfIntervalTime * 1000);
+					NFINT64 nNextCostTime = NFINT64(pSchedule->mfIntervalTime * 1000) * (pSchedule->mnAllCount - pSchedule->mnRemainCount);
+					pSchedule->mnNextTriggerTime = pSchedule->mnStartTime + nNextCostTime;
 				}
 			}
-			pSchedule = nameScheduleMap.Next();
+
+			pSchedule = xObjectSchedule->Next();
 		}
+
+		xObjectSchedule = mScheduleMap.Next();
 	}
+
 	//remove schedule
-	for (std::map<std::string, std::string>::iterator it = mRemoveList.begin(); it != mRemoveList.end(); ++it)
+	for (std::map<NFGUID, std::string>::iterator it = mRemoveList.begin(); it != mRemoveList.end(); ++it)
 	{
-		std::string guid = it->first;
+		NFGUID self = it->first;
 		std::string scheduleName = it->second;
-		auto findIter = mScheduleMap.find(guid);
-		if (findIter != mScheduleMap.end())
+		auto findIter = mScheduleMap.GetElement(self);
+		if (NULL != findIter)
 		{
-			findIter->second.RemoveElement(scheduleName);
-			if (findIter->second.Count() == 0)
+			findIter->RemoveElement(scheduleName);
+			if (findIter->Count() == 0)
 			{
-				mScheduleMap.erase(guid);
+				mScheduleMap.RemoveElement(self);
 			}
 		}
 	}
@@ -72,22 +79,23 @@ bool NFCScheduleModule::Execute()
 	//add schedule
 	for (std::list<NFCScheduleElement>::iterator iter = mAddList.begin(); iter != mAddList.end(); ++iter)
 	{
-		std::string guid = iter->self.ToString();
-		std::string scheduleName = iter->mstrScheduleName;
-		auto findIter = mScheduleMap.find(guid);
-		if (findIter == mScheduleMap.end())
+		NF_SHARE_PTR< NFMapEx <std::string, NFCScheduleElement >> xObjectScheduleMap = mScheduleMap.GetElement(iter->self);
+		if (NULL == xObjectScheduleMap)
 		{
-			NFMapEx <std::string, NFCScheduleElement > nameScheduleMap;
-			mScheduleMap.insert(std::map<std::string, NFMapEx <std::string, NFCScheduleElement >>::value_type(guid, nameScheduleMap));
-			findIter = mScheduleMap.find(guid);
+			xObjectScheduleMap = NF_SHARE_PTR< NFMapEx <std::string, NFCScheduleElement >>(NF_NEW NFMapEx <std::string, NFCScheduleElement >());
+			mScheduleMap.AddElement(iter->self, xObjectScheduleMap);
 		}
-		if (NULL == findIter->second.GetElement(scheduleName))
+
+		NF_SHARE_PTR<NFCScheduleElement> xScheduleElement = xObjectScheduleMap->GetElement(iter->mstrScheduleName);
+		if (NULL == xScheduleElement)
 		{
-			NF_SHARE_PTR<NFCScheduleElement> pSchedule(NF_NEW NFCScheduleElement());
-			*pSchedule = *iter;
-			findIter->second.AddElement(pSchedule->mstrScheduleName, pSchedule);
+			xScheduleElement = NF_SHARE_PTR<NFCScheduleElement>(NF_NEW NFCScheduleElement());
+			*xScheduleElement = *iter;
+
+			xObjectScheduleMap->AddElement(iter->mstrScheduleName, xScheduleElement);
 		}
 	}
+
 	mAddList.clear();
 
 	return true;
@@ -99,30 +107,35 @@ bool NFCScheduleModule::AddSchedule(const NFGUID self, const std::string& strSch
 	xSchedule.mstrScheduleName = strScheduleName;
 	xSchedule.mfIntervalTime = fTime;
 	xSchedule.mnNextTriggerTime = NFTime::GetTime() + (NFINT64)(fTime * 1000);
+	xSchedule.mnStartTime = NFTime::GetTime();
 	xSchedule.mnRemainCount = nCount;
+	xSchedule.mnAllCount = nCount;
 	xSchedule.self = self;
 	xSchedule.Add(cb);
+
 	mAddList.push_back(xSchedule);
 
 	return true;
 }
 
+bool NFCScheduleModule::RemoveSchedule(const NFGUID self)
+{
+	return mScheduleMap.RemoveElement(self);
+}
+
 bool NFCScheduleModule::RemoveSchedule(const NFGUID self, const std::string& strScheduleName)
 {
-	mRemoveList.insert(std::map<std::string, std::string>::value_type(self.ToString(), strScheduleName));
+	mRemoveList.insert(std::map<NFGUID, std::string>::value_type(self, strScheduleName));
 	return true;
 }
 
 bool NFCScheduleModule::ExistSchedule(const NFGUID self, const std::string& strScheduleName)
 {
-	auto findIter = mScheduleMap.find(self.ToString());
-	if (findIter == mScheduleMap.end())
+	NF_SHARE_PTR< NFMapEx <std::string, NFCScheduleElement >> xObjectScheduleMap = mScheduleMap.GetElement(self);
+	if (NULL == xObjectScheduleMap)
 	{
 		return false;
 	}
-	if (NULL == findIter->second.GetElement(strScheduleName))
-	{
-		return false;
-	}
-	return true;
+
+	return xObjectScheduleMap->ExistElement(strScheduleName);
 }
