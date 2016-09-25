0b7454eb024b0fc3a0c45305da4e65547de33a1a
ketoo <342006@qq.com>
2016-09-24 23:21:37 -0700

    add module schedule

--- NFComm/NFPluginModule/NFIScheduleModule.h
+++ NFComm/NFPluginModule/NFIScheduleModule.h
@@ -7,33 +7,33 @@
 #include "NFComm/NFCore/NFCDataList.h"
 #include "NFComm/NFPluginModule/NFIModule.h"
 
-typedef std::function<int(const NFGUID&, const std::string&, const float, const int)> HEART_BEAT_FUNCTOR;
-typedef std::function<void()> MODULE_HEART_BEAT_FUNCTOR;
+typedef std::function<int(const NFGUID&, const std::string&, const float, const int)> OBJECT_SCHEDULE_FUNCTOR;
+typedef std::function<int(const std::string&, const float, const int)> MODULE_SCHEDULE_FUNCTOR;
 
-typedef NF_SHARE_PTR<HEART_BEAT_FUNCTOR> HEART_BEAT_FUNCTOR_PTR;//HEART
-typedef NF_SHARE_PTR<MODULE_HEART_BEAT_FUNCTOR> MODULE_HEART_BEAT_FUNCTOR_PTR;//HEART
+typedef NF_SHARE_PTR<OBJECT_SCHEDULE_FUNCTOR> OBJECT_SCHEDULE_FUNCTOR_PTR;//HEART
+typedef NF_SHARE_PTR<MODULE_SCHEDULE_FUNCTOR> MODULE_SCHEDULE_FUNCTOR_PTR;//HEART
 
 class NFIScheduleModule
 	:public  NFIModule
 {
 public:
 	virtual ~NFIScheduleModule() {}
-	/*
+	
 	///for module
-	virtual bool AddSchedule(const std::string& strScheduleName, const HEART_BEAT_FUNCTOR_PTR& cb, const float fTime, const int nCount) = 0;
+	virtual bool AddSchedule(const std::string& strScheduleName, const MODULE_SCHEDULE_FUNCTOR_PTR& cb, const float fTime, const int nCount) = 0;
 	virtual bool RemoveSchedule(const std::string& strScheduleName) = 0;
 	virtual bool ExistSchedule(const std::string& strScheduleName) = 0;
 
 	template<typename BaseType>
-	bool AddSchedule(const std::string& strScheduleName, BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const std::string&, const float, const int), const float fIntervalTime, const int nCount)
+	bool AddSchedule(const std::string& strScheduleName, BaseType* pBase, int (BaseType::*handler)(const std::string&, const float, const int), const float fIntervalTime, const int nCount)
 	{
-		HEART_BEAT_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
-		HEART_BEAT_FUNCTOR_PTR functorPtr(NF_NEW HEART_BEAT_FUNCTOR(functor));
-		return AddSchedule(self, strScheduleName, functorPtr, fIntervalTime, nCount);
+		MODULE_SCHEDULE_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+		MODULE_SCHEDULE_FUNCTOR_PTR functorPtr(NF_NEW MODULE_SCHEDULE_FUNCTOR(functor));
+		return AddSchedule(strScheduleName, functorPtr, fIntervalTime, nCount);
 	}
-	*/
+	
 	///for object
-	virtual bool AddSchedule(const NFGUID self, const std::string& strScheduleName, const HEART_BEAT_FUNCTOR_PTR& cb, const float fTime, const int nCount) = 0;
+	virtual bool AddSchedule(const NFGUID self, const std::string& strScheduleName, const OBJECT_SCHEDULE_FUNCTOR_PTR& cb, const float fTime, const int nCount) = 0;
 	virtual bool RemoveSchedule(const NFGUID self) = 0; 
 	virtual bool RemoveSchedule(const NFGUID self, const std::string& strScheduleName) = 0;
 	virtual bool ExistSchedule(const NFGUID self, const std::string& strScheduleName) = 0;
@@ -41,8 +41,8 @@ public:
 	template<typename BaseType>
 	bool AddSchedule(const NFGUID self, const std::string& strScheduleName, BaseType* pBase, int (BaseType::*handler)(const NFGUID&, const std::string&, const float, const int), const float fIntervalTime, const int nCount)
 	{
-		HEART_BEAT_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
-		HEART_BEAT_FUNCTOR_PTR functorPtr(NF_NEW HEART_BEAT_FUNCTOR(functor));
+		OBJECT_SCHEDULE_FUNCTOR functor = std::bind(handler, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
+		OBJECT_SCHEDULE_FUNCTOR_PTR functorPtr(NF_NEW OBJECT_SCHEDULE_FUNCTOR(functor));
 		return AddSchedule(self, strScheduleName, functorPtr, fIntervalTime, nCount);
 	}
 };
