ec3125e6c35b187543d6e12356280bccf29ee275
ketoo <342006@qq.com>
2016-10-09 16:47:38 -0700

    add math lib

---
+++ NFComm/NFCore/NFBox.hpp
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFBox.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2016-10-09
+//    @Module           :    NFBox
+//
+// -------------------------------------------------------------------------
+
+#ifndef NF_VETOR2_H
+#define NF_VETOR2_H
+
+#include <iostream>
+#include <string>
+
+
+class NFBox
+{
+};
+
+#endif
---
+++ NFComm/NFCore/NFPlane.hpp
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFPlane.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2016-10-09
+//    @Module           :    NFPlane
+//
+// -------------------------------------------------------------------------
+
+#ifndef NF_PLANE_H
+#define NF_PLANE_H
+
+#include <iostream>
+#include <string>
+
+
+class NFPlane
+{
+};
+
+#endif
