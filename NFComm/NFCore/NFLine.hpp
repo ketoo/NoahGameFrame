71d26b0e99a053a55bbd92845e4f1cda84571ee1
ketoo <342006@qq.com>
2016-10-09 16:47:38 -0700

    add math lib

---
+++ NFComm/NFCore/NFLine.hpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFLine.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2016-10-09
+//    @Module           :    NFLine
+//
+// -------------------------------------------------------------------------
+
+#ifndef NF_VETOR2_H
+#define NF_VETOR2_H
+
+#include <iostream>
+#include <string>
+
+
+class NFLine
+{
+	
+};
+
+#endif
