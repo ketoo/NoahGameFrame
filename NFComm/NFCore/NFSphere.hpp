d398544c94578f49af48fd6269c244b30e7f4153
ketoo <342006@qq.com>
2016-10-09 16:47:38 -0700

    add math lib

---
+++ NFComm/NFCore/NFSphere.hpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFSphere.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2016-10-09
+//    @Module           :    NFSphere
+//
+// -------------------------------------------------------------------------
+
+#ifndef NF_SPHERE_H
+#define NF_SPHERE_H
+
+#include <iostream>
+#include <string>
+
+
+class NFSphere
+{
+
+};
+
+#endif
