8e263e8e0dc0d9f9ef2e245d7854cc64e422d978
kytoo <342006@qq.com>
2016-10-14 17:10:14 +1300

    math

--- NFComm/NFCore/NFLine.hpp
+++ NFComm/NFCore/NFLine.hpp
@@ -15,6 +15,63 @@
 
 class NFLine
 {
+public:
+    NFLine(const NFVector3& vPointStart, const NFVector3& vDirection, const int nLength)
+    {
+    }
+    
+    NFLine(const NFVector3& vPointStart, const NFVector3& vPointEnd)
+    {
+    }
+   
+    NFLine(const NFVector3& vPoint, const NFVector3& vDirection, const int nLength)
+    {
+    }
+
+    NFVector3 PointStart() const
+    {
+        return NFVector3::Zero();
+    }
+
+    NFVector3 Direction() const
+    {
+        return NFVector3::Zero();
+    }
+
+    NFVector3 PointEnd() const
+    {
+        return NFVector3::Zero();
+    }
+    
+    bool Contains(const NFVector3& v)
+    {
+        return false;
+    }
+
+    bool CollideWithLine(const NFLine& l)
+    {
+        return false;
+    }
+    
+    bool CollideWithPlane(const NFPlane& p)
+    {
+        return false;
+    }
+
+    bool CollideWithBox(const NFBox& b)
+    {
+        return false;
+    }
+
+    bool CollideWithSphere(const NFSphere& s)
+    {
+        return false;
+    }
+private:
+    NFVector3 mvStartPoint;
+    NFVector3 mvEndPoint;
+
+
 	
 };
 
