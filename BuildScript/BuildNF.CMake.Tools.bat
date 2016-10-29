079c5eec1e1783334284063c352189061a7bbb06
ketoo <342006@qq.com>
2016-10-29 02:00:53 -0700

    move build script to new folder

--- BuildScript/BuildNF.CMake.Tools.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-cd NFTools
-cd NFFileProcess
-cmake .. -G "Visual Studio 14 2015 Win64" -DBUILD_TOOLS_ONLY=ON
-cd ..
-cd ..
-cd BuildScript
---
+++ BuildScript/win/BuildNF.Tools.VS2015.Debug.X64.bat
@@ -0,0 +1,9 @@
+cd ..\..\
+
+cd NFTools\NFFileProcess\
+
+"%VS140COMNTOOLS%..\IDE\Devenv" NoahFrame.sln /build "Debug|X64"
+
+cd ..\..\
+
+cd BuildScript\win\
