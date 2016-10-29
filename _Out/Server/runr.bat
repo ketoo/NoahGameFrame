d0d2fe8a0ef80739d9e3e764411d3c5dcf030bdd
ketoo <342006@qq.com>
2016-10-28 22:58:40 -0700

    fixed script for run NF

--- _Out/Server/runr.bat
+++ _Out/Server/runr.bat
@@ -1,27 +1,27 @@
-cd Debug
+cd Release
 
 echo Starting NFMasterServer...
-start "NFMasterServer" "NFPluginLoader_r.exe" "Server=MasterServer" "ID=3"
+start "NFMasterServer" "NFPluginLoader.exe" "Server=MasterServer" "ID=3"
 
 choice /t 2 /d y /n >nul
 echo Starting NFWorldServer...
-start "NFWorldServer" "NFPluginLoader_r.exe" "Server=NFWorldServer" "ID=7"
+start "NFWorldServer" "NFPluginLoader.exe" "Server=WorldServer" "ID=7"
 
 
 choice /t 5 /d y /n >nul
 
 echo Starting NFLoginServer...
-start "NFLoginServer" "NFPluginLoader_r.exe" "Server=NFLoginServer" "ID=4"
+start "NFLoginServer" "NFPluginLoader.exe" "Server=LoginServer" "ID=4"
 
 choice /t 2 /d y /n >nul
 
 echo Starting NFGameServer...
-start "NFGameServer" "NFPluginLoader_r.exe" "Server=NFGameServer" "ID=6"
+start "NFGameServer" "NFPluginLoader.exe" "Server=GameServer" "ID=6"
 
 choice /t 4 /d y /n >nul
 
 echo Starting NFProxyServer...
-start "NFProxyServer" "NFPluginLoader_r.exe" "Server=NFProxyServer" "ID=5"
+start "NFProxyServer" "NFPluginLoader.exe" "Server=ProxyServer" "ID=5"
 
 
 
--- _Out/Server/runr.sh
+++ _Out/Server/runr.sh
@@ -6,12 +6,12 @@ chmod -R 777  NFPluginLoader_r
 
 ./NFPluginLoader_r -d Server=MasterServer ID=3
 
-./NFPluginLoader_r -d Server=NFWorldServer ID=7
+./NFPluginLoader_r -d Server=WorldServer ID=7
 
-./NFPluginLoader_r -d Server=NFLoginServer ID=4
+./NFPluginLoader_r -d Server=LoginServer ID=4
 
-./NFPluginLoader_r -d Server=NFGameServer ID=6
+./NFPluginLoader_r -d Server=GameServer ID=6
 
-./NFPluginLoader_r -d Server=NFProxyServer ID=5
+./NFPluginLoader_r -d Server=ProxyServer ID=5
 
 ps -A|grep NF
