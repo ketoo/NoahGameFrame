03e7145be8a693bc2919d761cab98eb392a41100
ketoo <342006@qq.com>
2016-10-29 02:05:39 -0700

    move build script to new folder

--- BuildScript/linux/BuildNF.CMake.Tools.sh
+++ BuildScript/linux/BuildNF.CMake.Tools.sh
@@ -1,8 +1,8 @@
-cd ..
+cd ../../
 
 cd NFTools
 cd NFFileProcess
 cmake .. -G "CodeBlocks - Unix Makefiles" -DBUILD_TOOLS_ONLY=ON -DCMAKE_BUILD_TYPE=Debug
 cd ..
 cd ..
-cd BuildScript
+cd BuildScript/linux
