8f723315f18fa68d5a854428dca3be3650c359b4
ketoo <342006@qq.com>
2016-10-28 22:58:40 -0700

    fixed script for run NF

--- _Out/Server/rund.sh
+++ _Out/Server/rund.sh
@@ -6,12 +6,12 @@ chmod -R 777  NFPluginLoader_d
 
 ./NFPluginLoader_d -d Server=MasterServer ID=3
 
-./NFPluginLoader_d -d Server=NFWorldServer ID=7
+./NFPluginLoader_d -d Server=WorldServer ID=7
 
-./NFPluginLoader_d -d Server=NFLoginServer ID=4
+./NFPluginLoader_d -d Server=LoginServer ID=4
 
-./NFPluginLoader_d -d Server=NFGameServer ID=6
+./NFPluginLoader_d -d Server=GameServer ID=6
 
-./NFPluginLoader_d -d Server=NFProxyServer ID=5
+./NFPluginLoader_d -d Server=ProxyServer ID=5
 
 ps -A|grep NF
