edd171b6d06a49155c7317e6ef879164132a21fc
tangyu1018@163.com <tangyu1018@163.com>
2016-11-03 16:10:05 +0800

    fixed cmake for linux

--- _Out/Server/Debug/RunAll.bat
+++
@@ -1,9 +0,0 @@
-start NFPluginLoader_d.exe ID=3 Server=MasterServer
-choice /t 5 /d y /n >nul
-start NFPluginLoader_d.exe ID=7 Server=WorldServer
-choice /t 5 /d y /n >nul
-start NFPluginLoader_d.exe ID=5 Server=ProxyServer
-choice /t 5 /d y /n >nul
-start NFPluginLoader_d.exe ID=4 Server=LoginServer
-choice /t 5 /d y /n >nul
-start NFPluginLoader_d.exe ID=6 Server=GameServer
