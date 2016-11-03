6cbfbed67ea65c514a450b367bda9518ff89fe7c
tangyu1018@163.com <tangyu1018@163.com>
2016-11-03 15:07:56 +0800

    fixed ServerID in Script

--- _Out/Server/Release/RunAll.bat
+++ _Out/Server/Release/RunAll.bat
@@ -1,9 +1,9 @@
-start NFPluginLoader.exe ID=1 Server=MasterServer
+start NFPluginLoader.exe ID=3 Server=MasterServer
 choice /t 5 /d y /n >nul
-start NFPluginLoader.exe ID=2 Server=WorldServer
+start NFPluginLoader.exe ID=7 Server=WorldServer
 choice /t 5 /d y /n >nul
-start NFPluginLoader.exe ID=3 Server=ProxyServer
+start NFPluginLoader.exe ID=5 Server=ProxyServer
 choice /t 5 /d y /n >nul
 start NFPluginLoader.exe ID=4 Server=LoginServer
 choice /t 5 /d y /n >nul
-start NFPluginLoader.exe ID=5 Server=GameServer
+start NFPluginLoader.exe ID=6 Server=GameServer
