eabe6f07f7502cbbe137df3514217c5e124c6e08
ketoo <342006@qq.com>
2016-11-05 22:03:17 -0700

    fixed tester

--- _Out/Server/Tester/rund_game.sh
+++ _Out/Server/Tester/rund_game.sh
@@ -4,6 +4,6 @@ cd Debug
 
 chmod -R 777  NFPluginLoader_d
 
-./NFPluginLoader_d Server=GameServer ID=6
+../Debug/NFPluginLoader_d Server=GameServer ID=6
 
 ps -A|grep NF
--- _Out/Server/Tester/rund_login.sh
+++ _Out/Server/Tester/rund_login.sh
@@ -4,6 +4,6 @@ cd Debug
 
 chmod -R 777  NFPluginLoader_d
 
-./NFPluginLoader_d Server=LoginServer ID=4
+../Debug/NFPluginLoader_d Server=LoginServer ID=4
 
 ps -A|grep NF
