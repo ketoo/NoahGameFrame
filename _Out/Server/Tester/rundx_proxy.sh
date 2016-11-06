0115c06b49941b6c700cb7f82bc3a98caa71e365
ketoo <342006@qq.com>
2016-11-05 22:09:54 -0700

    fixed debug path for script

--- _Out/Server/Tester/rundx_proxy.sh
+++ _Out/Server/Tester/rundx_proxy.sh
@@ -1,9 +1,9 @@
 export LC_ALL="C"
 
-cd Debug
+cd ../Debug/
 
 chmod -R 777  NFPluginLoader_d
 
-../Debug/NFPluginLoader_d PluginX.xml Server=ProxyServer ID=5
+./NFPluginLoader_d PluginX.xml Server=ProxyServer ID=5
 
 ps -A|grep NF
