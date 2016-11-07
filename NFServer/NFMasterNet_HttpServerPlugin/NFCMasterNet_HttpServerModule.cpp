9a4caff5fb8118da0376290447921ac935fa5d75
yunxi <windzxa@gmail.com>
2016-11-07 20:36:40 +0800

    rapidjson

--- NFServer/NFMasterNet_HttpServerPlugin/NFCMasterNet_HttpServerModule.cpp
+++ NFServer/NFMasterNet_HttpServerPlugin/NFCMasterNet_HttpServerModule.cpp
@@ -17,7 +17,7 @@ bool NFCMasterNet_HttpServerModule::Shut()
 
 bool NFCMasterNet_HttpServerModule::AfterInit()
 {
-	mHttpNetModule->AddReceiveCallBack(Msg::EMI_REQ_QUERY_SERVER_STATUS, this, &NFCMasterNet_HttpServerModule::OnQueryServerStatus);
+	mHttpNetModule->AddReceiveCallBack(NFMsg::EGMI_REQ_QUERY_SERVER_STATUS, this, &NFCMasterNet_HttpServerModule::OnQueryServerStatus);
 	mHttpNetModule->AddNetCommonReceiveCallBack(this, &NFCMasterNet_HttpServerModule::InvalidMessage);
 
 	mHttpNetModule->InitServer(7000);
@@ -32,7 +32,6 @@ bool NFCMasterNet_HttpServerModule::Execute()
 
 void NFCMasterNet_HttpServerModule::OnQueryServerStatus(struct evhttp_request *req, const int msgId, std::map<std::string, std::string>& argMap)
 {
-	pPluginManager->info("Req:QueryServerStatus", __FUNCTION__, __LINE__);
 	std::string str = mMasterServerModule->GetServersStatus();
 	NFCHttpNet::SendMsg(req, str);
 }
