46be0036a7698f6b0e5ab730a2ca2038e9d21cb2
yunxi <windzxa@gmail.com>
2016-11-07 20:18:36 +0800

    MasterHttpPlugin

---
+++ NFServer/NFMasterNet_HttpServerPlugin/NFCMasterNet_HttpServerModule.h
@@ -0,0 +1,36 @@
+#ifndef NFC_MASTERNET_HTTP_MODULE_H
+#define NFC_MASTERNET_HTTP_MODULE_H
+
+#include "NFIMasterNet_HttpServerModule.h"
+#include "NFComm/NFPluginModule/NFIMasterNet_ServerModule.h"
+#include "NFComm/NFPluginModule/NFIKernelModule.h"
+#include "NFComm/NFPluginModule/NFINetModule.h"
+#include "NFComm/NFNet/NFCHttpNetModule.h"
+
+class NFCMasterNet_HttpServerModule
+	: public NFIMasterNet_HttpServerModule
+{
+public:
+	NFCMasterNet_HttpServerModule(NFIPluginManager* p)
+	{
+		pPluginManager = p;
+	}
+
+	virtual bool Init();
+	virtual bool Shut();
+
+	virtual bool AfterInit();
+	virtual bool Execute();
+
+protected:
+	void OnQueryServerStatus(struct evhttp_request *req, const int msgId, std::map<std::string, std::string>& argMap);
+	void InvalidMessage(struct evhttp_request *req, const int msgId, std::map<std::string, std::string>& argMap);
+
+private:
+	NFIKernelModule* mKernelModule;
+	NFIHttpNetModule* mHttpNetModule;
+
+	NFIMasterNet_ServerModule* mMasterServerModule;
+};
+
+#endif
