66598fb8cba79e333c07c1e964bd05f3ddd26d9b
kytoo <342006@qq.com>
2016-11-07 22:20:42 +1300

    add snatcher to process message

--- NFServer/NFLoginLogicPlugin/NFCLoginLogicModule.cpp
+++ NFServer/NFLoginLogicPlugin/NFCLoginLogicModule.cpp
@@ -23,7 +23,56 @@ bool NFCLoginLogicModule::Shut()
 
 int NFCLoginLogicModule::OnLoginProcess(const NFGUID& object, const std::string& strAccount, const std::string& strPwd)
 {
+    NFINetModule* pNetModule = m_pLoginNet_ServerModule->GetNetModule();
+
+    NFGUID nPlayerID;
+	NFMsg::ReqAccountLogin xMsg;
+	if (!pNetModule->ReceivePB(nSockIndex, nMsgID, msg, nLen, xMsg, nPlayerID))
+	{
+		return;
+	}
+
+	NetObject* pNetObject = pNetModule->GetNet()->GetNetObject(nSockIndex);
+	if (pNetObject)
+	{
+		if (pNetObject->GetConnectKeyState() == 0)
+		{
+			//int nState = m_pLoginLogicModule->OnLoginProcess(pNetObject->GetClientID(), xMsg.account(), xMsg.password());
+            int nState = 0;
+			if (0 != nState)
+			{
+				std::ostringstream strLog;
+				strLog << "Check password failed, Account = " << xMsg.account() << " Password = " << xMsg.password();
+				m_pLogModule->LogNormal(NFILogModule::NLL_ERROR_NORMAL, NFGUID(0, nSockIndex), strLog, __FUNCTION__, __LINE__);
+
+				NFMsg::AckEventResult xMsg;
+				xMsg.set_event_code(NFMsg::EGEC_ACCOUNTPWD_INVALID);
+
+				pNetModule->SendMsgPB(NFMsg::EGameMsgID::EGMI_ACK_LOGIN, xMsg, nSockIndex);
+				return;
+			}
+
+			pNetObject->SetConnectKeyState(1);
+			pNetObject->SetAccount(xMsg.account());
+
+			NFMsg::AckEventResult xData;
+			xData.set_event_code(NFMsg::EGEC_ACCOUNT_SUCCESS);
+
+			pNetModule->SendMsgPB(NFMsg::EGameMsgID::EGMI_ACK_LOGIN, xData, nSockIndex);
+
+			m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, nSockIndex), "Login successed :", xMsg.account().c_str());
+		}
+	}
 	return 0;
+}
+
+bool NFCLoginLogicModule::ReadyExecute()
+{
+    NFINetModule* pNetModule = m_pLoginNet_ServerModule->GetNetModule();
+    pNetModule->RemoveReceiveCallBack(NFMsg::EGMI_REQ_LOGIN);
+	pNetModule->AddReceiveCallBack(NFMsg::EGMI_REQ_LOGIN, this, &NFCLoginLogicModule::OnLoginProcess);
+
+    return true;
 }
 
 bool NFCLoginLogicModule::Execute()
