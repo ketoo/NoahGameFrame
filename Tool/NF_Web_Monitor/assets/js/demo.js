08b264fcdcc50bdb9e98ad9ec166d3894084a765
tangyu1018@163.com <tangyu1018@163.com>
2016-11-15 03:30:38 +0800

    update web monitor

--- Tool/NF_Web_Monitor/assets/js/demo.js
+++ Tool/NF_Web_Monitor/assets/js/demo.js
@@ -1,10 +1,10 @@
 type = ['','info','success','warning','danger'];
-    	
+
 
 demo = {
     initPickColor: function(){
         $('.pick-class-label').click(function(){
-            var new_class = $(this).attr('new-class');  
+            var new_class = $(this).attr('new-class');
             var old_class = $('#display-buttons').attr('data-class');
             var display_div = $('#display-buttons');
             if(display_div.length) {
@@ -15,22 +15,22 @@ demo = {
             }
         });
     },
-    
-    initChartist: function(){    
-        
+
+    initChartist: function(){
+
         var dataSales = {
           labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
           series: [
-             [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
-            [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
-            [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
+             [287, 385, 490, 562, 594, 626, 698, 895, 952],
+            [67, 152, 193, 240, 387, 435, 535, 642, 744],
+            [23, 113, 67, 108, 190, 239, 307, 410, 410]
           ]
         };
-        
+
         var optionsSales = {
           lineSmooth: false,
           low: 0,
-          high: 800,
+          high: 1000,
           showArea: true,
           height: "245px",
           axisX: {
@@ -39,10 +39,10 @@ demo = {
           lineSmooth: Chartist.Interpolation.simple({
             divisor: 3
           }),
-          showLine: false,
+          showLine: true,
           showPoint: false,
         };
-        
+
         var responsiveSales = [
           ['screen and (max-width: 640px)', {
             axisX: {
@@ -52,18 +52,18 @@ demo = {
             }
           }]
         ];
-    
+
         Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
-        
-    
+
+
         var data = {
           labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
           series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-            [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
+            [542, 543, 520, 680, 653, 753, 326, 434, 568, 610, 756, 895],
+            [230, 293, 380, 480, 503, 553, 600, 664, 698, 710, 736, 795]
           ]
         };
-        
+
         var options = {
             seriesBarDistance: 10,
             axisX: {
@@ -71,7 +71,7 @@ demo = {
             },
             height: "245px"
         };
-        
+
         var responsiveOptions = [
           ['screen and (max-width: 640px)', {
             seriesBarDistance: 5,
@@ -82,15 +82,15 @@ demo = {
             }
           }]
         ];
-        
-        Chartist.Bar('#chartActivity', data, options, responsiveOptions);
-    
+
+        Chartist.Line('#chartActivity', data, options, responsiveOptions);
+
         var dataPreferences = {
             series: [
                 [25, 30, 20, 25]
             ]
         };
-        
+
         var optionsPreferences = {
             donut: true,
             donutWidth: 40,
@@ -101,15 +101,15 @@ demo = {
                 showGrid: false
             }
         };
-    
+
         Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
-        
+
         Chartist.Pie('#chartPreferences', {
           labels: ['62%','32%','6%'],
           series: [62, 32, 6]
-        });   
+        });
     },
-    
+
     initGoogleMaps: function(){
         var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
         var mapOptions = {
@@ -117,26 +117,26 @@ demo = {
           center: myLatlng,
           scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
           styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
-    
+
         }
         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-        
+
         var marker = new google.maps.Marker({
             position: myLatlng,
             title:"Hello World!"
         });
-        
+
         // To add the marker to the map, call setMap();
         marker.setMap(map);
     },
-    
+
 	showNotification: function(from, align){
     	color = Math.floor((Math.random() * 4) + 1);
-    	
+
     	$.notify({
-        	icon: "pe-7s-gift",
-        	message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
-        	
+        	icon: "ti-gift",
+        	message: "Welcome to <b>Paper Dashboard</b> - a beautiful freebie for every web developer."
+
         },{
             type: type[color],
             timer: 4000,
@@ -147,6 +147,5 @@ demo = {
         });
 	}
 
-    
-}
 
+}
