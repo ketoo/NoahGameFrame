3deac41bf6789fedaa3611b53944bd3b6c483016
tangyu1018@163.com <tangyu1018@163.com>
2016-11-24 15:50:34 +0800

    fixed a typo

--- NFComm/NFPluginModule/NFITeamModule.h
+++ NFComm/NFPluginModule/NFITeamModule.h
@@ -22,7 +22,7 @@ public:
 	virtual bool KickTeamMmember(const NFGUID& self, const NFGUID& xTeamID, const NFGUID& xMmember) = 0;
 
 	virtual bool MemberOnline(const NFGUID& self, const NFGUID& xTeam, const int& nGameID) = 0;
-	virtual bool MemberOffeline(const NFGUID& self, const NFGUID& xTeam) = 0;
+	virtual bool MemberOffline(const NFGUID& self, const NFGUID& xTeam) = 0;
 	virtual bool GetMemberList(const NFGUID& self, const NFGUID& xTeam, std::vector<NFGUID>& xmemberList) = 0;
 	virtual bool BroadcastMsgToTeam(const NFGUID& self, const NFGUID& xTeam, const uint16_t nMsgID, google::protobuf::Message& xData) = 0;
 private:
