d14cc140cdc5af5bc65b208e81ad0565e771c669
ketoo <342006@qq.com>
2016-12-05 13:20:18 -0800

    doxygen

--- Doxygen.bat
+++ Doxygen.bat
@@ -7,6 +7,11 @@ rd /q /s Dependencies
 rd /q /s NFTools
 rd /q /s Tool
 rd /q /s Tutorial
-rd /q /s Dependencies
+rd /q /s NFComm\Config
+rd /q /s NFComm\NFMessageDefine
 
+
+del NFComm\NFLogPlugin\easylogging++.h
+del NFComm\NFConfigPlugin\myrc4.h
+del NFComm\NFConfigPlugin\myrc4.cpp
 echo. & pause 
