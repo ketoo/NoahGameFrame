81fef6d9308b43a84f33e037e3152c4534779190
admin <admin>
2016-12-08 15:00:00 +0800

    fixed for build_dep.sh

--- Dependencies/build_dep.sh
+++ Dependencies/build_dep.sh
@@ -5,7 +5,11 @@ mkdir lib/Debug/
 mkdir lib/Release/
 
 # extract mysql libs
+echo Extract mysql libs
+unzip -o mysql_libs.zip -d ./
 
+echo Extract mysql++ libs
+unzip -o mysqlpp_libs.zip -d ./
 
 # extract libevent libs
 echo Extract libevent libs
@@ -22,9 +26,9 @@ chmod -R 755 *
 ./configure CPPFLAGS=-fPIC --disable-shared --disable-openssl
 make
 
-cp -R -f ./libs/*.a ../lib/Debug/
-cp -R -f ./libs/*.a ../lib/
-cp -R -f ./libs/*.a ../lib/Release/
+cp -R -f ./.libs/*.a ../lib/Debug/
+cp -R -f ./.libs/*.a ../lib/
+cp -R -f ./.libs/*.a ../lib/Release/
 cd ../
 
 # compiling protobuf
@@ -34,10 +38,10 @@ chmod -R 755 *
 make
 make check
 
-cp -r -f ./src/.libs/*.so ../_Out/Debug/
-cp -r -f ./src/.libs/*.so.* ../_Out/Debug/
-cp -r -f ./src/.libs/*.so ../_Out/Release/
-cp -r -f ./src/.libs/*.so.* ../_Out/Release/
+cp -r -f ./src/.libs/*.so ../../_Out/Debug/
+cp -r -f ./src/.libs/*.so.* ../../_Out/Debug/
+cp -r -f ./src/.libs/*.so ../../_Out/Release/
+cp -r -f ./src/.libs/*.so.* ../../_Out/Release/
 
 cd ../
 
@@ -60,8 +64,14 @@ cp -R -f ./lib/Release/libmysqlclient.so ../_Out/Release/
 
 cp -R -f ./lib/Release/libmysqlclient.a ./lib/Debug/
 cp -R -f ./lib/Release/libmysqlpp.a ./lib/Debug/
+
+cp -R -f ./lib/Release/libmysqlclient.so ./lib/
+cp -R -f ./lib/Release/libmysqlclient.so ./lib/
+
+cp -R -f ./lib/Release/libmysqlclient.a ./lib/
+cp -R -f ./lib/Release/libmysqlpp.a ./lib/
+
 # TODO: other libs
 
 # back to main dir
 pwd
-
