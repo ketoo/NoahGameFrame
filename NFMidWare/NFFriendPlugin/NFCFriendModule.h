383910553b73c9fac6a164233abb0cfa4bd63676
Johance2 <wjhc20128@gmail.com>
2016-12-21 11:57:22 +0800

    * modify friend and shop

--- NFMidWare/NFFriendPlugin/NFCFriendModule.h
+++ NFMidWare/NFFriendPlugin/NFCFriendModule.h
@@ -1,13 +1,13 @@
 // -------------------------------------------------------------------------
-//    @FileName			:    NFCChatModule.h
+//    @FileName			:    NFCFriendModule.h
 //    @Author           :    LvSheng.Huang
 //    @Date             :    2016-12-18
-//    @Module           :    NFCChatModule
+//    @Module           :    NFCFriendModule
 //    @Desc             :
 // -------------------------------------------------------------------------
 
-#ifndef NFC_CHAT_MODULE_H
-#define NFC_CHAT_MODULE_H
+#ifndef NFC_FRIEND_MODULE_H
+#define NFC_FRIEND_MODULE_H
 
 #include <memory>
 #include "NFComm/NFMessageDefine/NFMsgDefine.h"
@@ -15,7 +15,7 @@
 #include "NFComm/NFPluginModule/NFILogModule.h"
 #include "NFComm/NFPluginModule/NFIKernelModule.h"
 #include "NFComm/NFPluginModule/NFIClassModule.h"
-#include "NFComm/NFPluginModule/NFIChatModule.h"
+#include "NFComm/NFPluginModule/NFIFriendModule.h"
 #include "NFComm/NFPluginModule/NFIElementModule.h"
 #include "NFComm/NFPluginModule/NFIEventModule.h"
 #include "NFComm/NFPluginModule/NFISceneAOIModule.h"
@@ -23,11 +23,11 @@
 
 
 
-class NFCChatModule
-    : public NFIChatModule
+class NFCFriendModule
+    : public NFIFriendModule
 {
 public:
-	NFCChatModule(NFIPluginManager* p)
+	NFCFriendModule(NFIPluginManager* p)
     {
         pPluginManager = p;
     }
