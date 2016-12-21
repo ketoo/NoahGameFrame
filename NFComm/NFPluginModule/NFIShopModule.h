1ce3c70e336ea3cb3cb0f0495405ed1fc50b90a9
Johance2 <wjhc20128@gmail.com>
2016-12-21 12:12:01 +0800

    * modify friend and shop

---
+++ NFComm/NFPluginModule/NFIShopModule.h
@@ -0,0 +1,21 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFIShopModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2016-12-18
+//    @Module           :    NFIShopModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+
+#ifndef NFI_SHOP_MODULE_H
+#define NFI_SHOP_MODULE_H
+
+#include "NFIModule.h"
+
+class NFIShopModule : public NFIModule
+{
+public:
+
+};
+
+#endif
