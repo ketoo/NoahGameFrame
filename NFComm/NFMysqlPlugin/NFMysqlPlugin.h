ceea004891982d8d801086422babbba6c35359c2
ketoo <342006@qq.com>
2017-02-07 18:36:27 -0800

    change mysql plugin's name

--- NFComm/NFMysqlPlugin/NFMysqlPlugin.h
+++ NFComm/NFMysqlPlugin/NFMysqlPlugin.h
@@ -1,25 +1,19 @@
 //------------------------------------------------------------------------ -
-//    @FileName			:    NFNoSqlPlugin.h
+//    @FileName			:    NFMysqlPlugin.h
 //    @Author           :    LvSheng.Huang
 //    @Date             :    2017-02-08
-//    @Module           :    NFNoSqlPlugin
+//    @Module           :    NFMysqlPlugin
 //
 // -------------------------------------------------------------------------
 
 #include "NFComm/NFPluginModule/NFIPlugin.h"
 #include "NFComm/NFPluginModule/NFIPluginManager.h"
 
-#ifdef NFDATABASEPLUGIN_EXPORTS
-#define NFDATABASEPLUGIN_API __declspec(dllexport)
-#else
-#define NFDATABASEPLUGIN_API __declspec(dllimport)
-#endif
-
 //////////////////////////////////////////////////////////////////////////
-class NFNoSqlPlugin : public NFIPlugin
+class NFMysqlPlugin : public NFIPlugin
 {
 public:
-    NFNoSqlPlugin(NFIPluginManager* p)
+    NFMysqlPlugin(NFIPluginManager* p)
     {
         pPluginManager = p;
     }
