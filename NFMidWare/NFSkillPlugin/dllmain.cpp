031eaeb4e0074fdb16c061d2f5604db049982c2d
Johance2 <wjhc20128@gmail.com>
2017-02-22 18:27:40 +0800

    * remove plugin libevent_core.lib Net.lib

--- NFMidWare/NFSkillPlugin/dllmain.cpp
+++ NFMidWare/NFSkillPlugin/dllmain.cpp
@@ -7,7 +7,6 @@
 #pragma comment( lib, "NFCore_d.lib" )
 #pragma comment( lib, "NFMessageDefine_d.lib" )
 #pragma comment( lib, "libprotobuf_d.lib" )
-#pragma comment( lib, "libevent_core.lib" )
 
 
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
@@ -24,7 +23,6 @@
 #pragma comment( lib, "NFCore.lib" )
 #pragma comment( lib, "NFMessageDefine.lib" )
 #pragma comment( lib, "libprotobuf.lib" )
-#pragma comment( lib, "libevent_core.lib" )
 
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
 #pragma comment( lib, "NFCore.a" )
