a07d949a62728b04563629cdd0fbe80baec3cb8a
Johance2 <wjhc20128@gmail.com>
2017-02-24 15:48:09 +0800

    *remove clock time in ios

--- NFClient/Cocos/Classes/libevent/ios/event2/event-config.h
+++ NFClient/Cocos/Classes/libevent/ios/event2/event-config.h
@@ -32,7 +32,7 @@
 #define _EVENT_HAVE_ARPA_INET_H 1
 
 /* Define to 1 if you have the `clock_gettime' function. */
-#define _EVENT_HAVE_CLOCK_GETTIME 1
+//#define _EVENT_HAVE_CLOCK_GETTIME 1
 
 /* Define to 1 if you have the declaration of `CTL_KERN', and to 0 if you
    don't. */
