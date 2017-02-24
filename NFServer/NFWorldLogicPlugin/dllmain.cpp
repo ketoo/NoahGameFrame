9b124d45be8cfc1edf3acd3767e0174a4bdd2d40
Johance2 <wjhc20128@gmail.com>
2017-02-24 13:28:46 +0800

    * fix linux libevent.a in NetPlugin

--- NFServer/NFWorldLogicPlugin/dllmain.cpp
+++ NFServer/NFWorldLogicPlugin/dllmain.cpp
@@ -6,14 +6,10 @@
 #pragma comment( lib, "NFCore_d.lib" )
 #pragma comment( lib, "NFMessageDefine_d.lib" )
 #pragma comment( lib, "libprotobuf_d.lib" )
-#pragma comment( lib, "libevent_core.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-////#pragma comment( lib, "RakNet_LibStatic_Debug_x64.a" )
-#pragma comment( lib, "NFNet_d.a" )
 #pragma comment( lib, "NFCore_d.a" )
 #pragma comment( lib, "NFMessageDefine_d.a" )
 #pragma comment( lib, "libprotobuf_d.a" )
-#pragma comment( lib, "libevent_core.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
@@ -21,14 +17,10 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "NFNetPlugin.lib" )
 #pragma comment( lib, "NFCore.lib" )
 #pragma comment( lib, "NFMessageDefine.lib" )
 #pragma comment( lib, "libprotobuf.lib" )
-#pragma comment( lib, "libevent_core.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-//#pragma comment( lib, "RakNet_LibStatic_Release_x64.a" )
-#pragma comment( lib, "NFNet.a" )
 #pragma comment( lib, "NFCore.a" )
 #pragma comment( lib, "NFMessageDefine.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
