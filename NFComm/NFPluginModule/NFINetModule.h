c80d9515d221cad621c2fa08aa30328433b21f9d
ketoo <342006@qq.com>
2017-02-25 02:45:52 -0800

    add interface to expand buffer size for libevent

--- NFComm/NFPluginModule/NFINetModule.h
+++ NFComm/NFPluginModule/NFINetModule.h
@@ -214,6 +214,7 @@ public:
 
 	//as server
 	virtual int Initialization(const unsigned int nMaxClient, const unsigned short nPort, const int nCpuCount = 4) = 0;
+	virtual int ExpandBufferSize(const unsigned int size = 1024 * 1024 * 100) = 0;
 
 	virtual void RemoveReceiveCallBack(const int nMsgID) = 0;
 
