3615cff592d2f6d2debd64458896865eaaccd0fa
ketoo <342006@qq.com>
2017-02-26 01:51:41 -0800

    add save/load interface for tiles-data

--- NFServer/NFGameLogicPlugin/NFCTileModule.cpp
+++ NFServer/NFGameLogicPlugin/NFCTileModule.cpp
@@ -18,7 +18,8 @@ bool NFCTileModule::Init()
 	m_pElementModule = pPluginManager->FindModule<NFIElementModule>();
 	m_pGuildRedisModule = pPluginManager->FindModule<NFIGuildRedisModule>();
 	m_pGameServerNet_ServerModule = pPluginManager->FindModule<NFIGameServerNet_ServerModule>();
-
+	m_pPlayerRedisModule = pPluginManager->FindModule<NFIPlayerRedisModule>();
+	
     return true;
 }
 
@@ -34,6 +35,7 @@ bool NFCTileModule::Execute()
 
 bool NFCTileModule::AfterInit()
 {
+	m_pKernelModule->AddClassCallBack(NFrame::Player::ThisName(), this, &NFCTileModule::OnObjectClassEvent);
 
 	if (!m_pNetModule->AddReceiveCallBack(NFMsg::EGameMsgID::EGEC_REQ_MINING_TITLE, this, &NFCTileModule::ReqMineTile)) { return false; }
 
@@ -47,11 +49,11 @@ void NFCTileModule::ReqMineTile(const int nSockIndex, const int nMsgID, const ch
 	int nX = xMsg.x();
 	int nY = xMsg.y();
 	int nOpr = xMsg.opr();
-	if (1 == nOpr)
+	if (1 == nOpr)//add
 	{
 		AddTile(nPlayerID, nX, nY);
 	}
-	else if (0 == nOpr)
+	else if (0 == nOpr)//rem
 	{
 		RemoveTile(nPlayerID, nX, nY);
 	}
@@ -68,12 +70,129 @@ void NFCTileModule::ReqMineTile(const int nSockIndex, const int nMsgID, const ch
 	m_pGameServerNet_ServerModule->SendMsgPBToGate(NFMsg::EGEC_ACK_MINING_TITLE, xData, nPlayerID);
 }
 
-bool NFCTileModule::AddTile(const NFGUID & self, const int nX, const int nY)
+bool NFCTileModule::AddTile(const NFGUID & self, const int nX, const int nY, const int nOpr)
 {
+	NF_SHARE_PTR<TileData> xTileData = mxTileData.GetElement(self);
+	if (!xTileData)
+	{
+		xTileData = NF_SHARE_PTR<TileData>(NF_NEW TileData());
+		mxTileData.AddElement(self, xTileData);
+	}
+
+	NF_SHARE_PTR<NFMapEx<int, TileState>> xStateDataMap = xTileData->mxTileState.GetElement(nX);
+	if (!xStateDataMap)
+	{
+		xStateDataMap = NF_SHARE_PTR<NFMapEx<int, TileState>>(NF_NEW NFMapEx<int, TileState>());
+		xTileData->mxTileState.AddElement(nX, xStateDataMap);
+	}
+
+	NF_SHARE_PTR<TileState> xTileState  = xStateDataMap->GetElement(nY);
+	if (!xTileState)
+	{
+		xTileState = NF_SHARE_PTR<TileState>(NF_NEW TileState());
+		xStateDataMap->AddElement(nY, xTileState);
+	}
+	else
+	{
+		if (nOpr == xTileState->state)
+		{
+			//has be deleted
+			return false;
+		}
+	}
+	xTileState->x = nX;
+	xTileState->y = nY;
+	xTileState->state = nOpr;
+
 	return false;
 }
 
 bool NFCTileModule::RemoveTile(const NFGUID & self, const int nX, const int nY)
 {
+	NF_SHARE_PTR<TileData> xTileData = mxTileData.GetElement(self);
+	if (!xTileData)
+	{
+		return false;
+	}
+
+	NF_SHARE_PTR<NFMapEx<int, TileState>> xStateDataMap = xTileData->mxTileState.GetElement(nX);
+	if (!xStateDataMap)
+	{
+		return false;
+	}
+
+	return xStateDataMap->RemoveElement(nY);
+}
+
+bool NFCTileModule::SaveTileData(const NFGUID & self)
+{
+	NF_SHARE_PTR<TileData> xTileData = mxTileData.GetElement(self);
+	if (!xTileData)
+	{
+		return false;
+	}
+
+	NFMsg::AckMiningTitle xData;
+	NF_SHARE_PTR<NFMapEx<int, TileState>> xStateDataMap = xTileData->mxTileState.First();
+	for (; ; xStateDataMap = xTileData->mxTileState.Next())
+	{
+		NF_SHARE_PTR<TileState> xStateData = xStateDataMap->First();
+		for (; ; xStateData = xStateDataMap->Next())
+		{
+			//pb
+			//xStateData
+			NFMsg::AckMiningTitle::TileState* pTile = xData.add_tile();
+			if (pTile)
+			{
+				pTile->set_x(xStateData->x);
+				pTile->set_y(xStateData->y);
+				pTile->set_opr(xStateData->state);
+			}
+		}
+	}
+	std::string strData;
+	if (xData.SerializeToString(&strData))
+	{
+		return m_pPlayerRedisModule->SavePlayerTileToCache(self, strData);
+	}
+
 	return false;
+}
+
+bool NFCTileModule::LoadTileData(const NFGUID & self)
+{
+	std::string strData;
+	if (m_pPlayerRedisModule->GetPlayerTileFromCache(self, strData))
+	{
+		NFMsg::AckMiningTitle xData;
+		if (xData.ParseFromString(strData))
+		{
+			int nCount = xData.tile_size();
+			for (int i = 0; i < nCount; ++i)
+			{
+				const NFMsg::AckMiningTitle::TileState& xTile = xData.tile(i);
+				AddTile(self, xTile.x(), xTile.y(), xTile.opr());
+			}
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int NFCTileModule::OnObjectClassEvent(const NFGUID & self, const std::string & strClassName, const CLASS_OBJECT_EVENT eClassEvent, const NFDataList & var)
+{
+	if (CLASS_OBJECT_EVENT::COE_DESTROY == eClassEvent)
+	{
+		//save
+		SaveTileData(self);
+	}
+	else if (CLASS_OBJECT_EVENT::COE_CREATE_LOADDATA == eClassEvent)
+	{
+		//load
+		LoadTileData(self);
+	}
+
+	return 0;
 }
