d76d780999a77ca39025ec9e995b97ee44d55669
ketoo <342006@qq.com>
2017-03-07 13:28:50 -0800

    add interface file for coroutine module

---
+++ NFComm/NFPluginModule/NFICoroutineModule.h
@@ -0,0 +1,40 @@
+#ifndef NFI_COROUTINE_MODULE_H
+#define NFI_COROUTINE_MODULE_H
+
+#include <stdint.h>
+#include <type_traits>
+#include <functional>
+#include "NFComm/NFPluginModule/NFPlatform.h"
+#include "NFComm/NFPluginModule/NFIModule.h"
+
+using TaskFunction = std::function<void()>;
+
+class NFICoroutineModule : public NFIModule
+{
+public:
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
