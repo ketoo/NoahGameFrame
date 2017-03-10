e69e520c41dada04c586c565dd2bf1f862e8dcbc
ketoo <342006@qq.com>
2017-03-10 10:41:10 -0800

    move to new plugin folder

--- NFServer/NFGameLogicPlugin/NFGameLogicPlugin.cpp
+++ NFServer/NFGameLogicPlugin/NFGameLogicPlugin.cpp
@@ -9,14 +9,11 @@
 #include "NFGameLogicPlugin.h"
 #include "NFCBuffModule.h"
 #include "NFCSkillModule.h"
-#include "NFCEquipModule.h"
 #include "NFCPVPModule.h"
 #include "NFCCommonConfigModule.h"
 #include "NFCGmModule.h"
 #include "NFCGSSwichServerModule.h"
 #include "NFCMapModule.h"
-#include "NFCEquipPropertyModule.h"
-#include "NFCTaskModule.h"
 #include "NFCCreateRoleModule.h"
 #include "NFCTileModule.h"
 #include "NFCTeamPVPModule.h"
@@ -49,18 +46,14 @@ const std::string NFGameLogicPlugin::GetPluginName()
 
 void NFGameLogicPlugin::Install()
 {
-
     REGISTER_MODULE(pPluginManager, NFIBuffModule, NFCBuffModule)
 	REGISTER_MODULE(pPluginManager, NFISkillModule, NFCSkillModule)
 	REGISTER_MODULE(pPluginManager, NFICreateRoleModule, NFCCreateRoleModule)
-
-	REGISTER_MODULE(pPluginManager, NFIEquipModule, NFCEquipModule)
 	REGISTER_MODULE(pPluginManager, NFIPVPModule, NFCPVPModule)
     REGISTER_MODULE(pPluginManager, NFICommonConfigModule, NFCCommonConfigModule);
     REGISTER_MODULE(pPluginManager, NFIGmModule, NFCGmModule);
 	REGISTER_MODULE(pPluginManager, NFIGSSwichServerModule, NFCGSSwichServerModule);
 	REGISTER_MODULE(pPluginManager, NFIMapModule, NFCMapModule);
-	REGISTER_MODULE(pPluginManager, NFIEquipPropertyModule, NFCEquipPropertyModule);
 	REGISTER_MODULE(pPluginManager, NFITeamPVPModule, NFCTeamPVPModule);
 	REGISTER_MODULE(pPluginManager, NFITileModule, NFCTileModule);
 }
@@ -69,15 +62,11 @@ void NFGameLogicPlugin::Uninstall()
 {
 	UNREGISTER_MODULE(pPluginManager, NFITileModule, NFCTileModule);
 	UNREGISTER_MODULE(pPluginManager, NFITeamPVPModule, NFCTeamPVPModule);
-	UNREGISTER_MODULE(pPluginManager, NFIEquipPropertyModule, NFCEquipPropertyModule);
 	UNREGISTER_MODULE(pPluginManager, NFIMapModule, NFCMapModule);
 	UNREGISTER_MODULE(pPluginManager, NFIGSSwichServerModule, NFCGSSwichServerModule);
     UNREGISTER_MODULE(pPluginManager, NFIGmModule, NFCGmModule);
     UNREGISTER_MODULE(pPluginManager, NFICommonConfigModule, NFCCommonConfigModule);
-    
 	UNREGISTER_MODULE(pPluginManager, NFIPVPModule, NFCPVPModule)
-	UNREGISTER_MODULE(pPluginManager, NFIEquipModule, NFCEquipModule)
-
     UNREGISTER_MODULE(pPluginManager, NFICreateRoleModule, NFCCreateRoleModule)
 	UNREGISTER_MODULE(pPluginManager, NFISkillModule, NFCSkillModule)
     UNREGISTER_MODULE(pPluginManager, NFIBuffModule, NFCBuffModule)
