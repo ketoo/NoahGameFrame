ff99462ac69eae0f2043537a6dab58a7a2043a17
ketoo <342006@qq.com>
2017-03-11 02:17:47 -0800

    user gift plugin

--- NFComm/NFConfigPlugin/NFCClassModule.h
+++ NFComm/NFConfigPlugin/NFCClassModule.h
@@ -105,12 +105,11 @@ public:
 
     const bool AddId(std::string& strId)
     {
-        mIdList.Add(strId);
-
+		mIdList.push_back(strId);
         return true;
     }
 
-    NFList<std::string>& GetIdList()
+	std::vector<std::string>& GetIdList()
     {
         return mIdList;
     }
@@ -135,7 +134,7 @@ private:
     std::string mstrClassName;
     std::string mstrClassInstancePath;
 
-    NFList<std::string> mIdList;
+    std::vector<std::string> mIdList;
 
     NFList<CLASS_EVENT_FUNCTOR_PTR> mxClassEventInfo;
 };
