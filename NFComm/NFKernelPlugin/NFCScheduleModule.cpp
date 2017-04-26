ab1678eac3f25197eae50a1b38f01bc885bec335
ketoo <342006@qq.com>
2017-04-26 03:14:44 -0700

    as a schedule manager, it should has a check when it try to remove a schedule

--- NFComm/NFKernelPlugin/NFCScheduleModule.cpp
+++ NFComm/NFKernelPlugin/NFCScheduleModule.cpp
@@ -59,22 +59,28 @@ bool NFCScheduleModule::Execute()
 			NFINT64 nNow = NFGetTime();
 			if (nNow > pSchedule->mnNextTriggerTime)
 			{
-				if (pSchedule->mnRemainCount > 0 || pSchedule->mbForever == true)
+				std::map<NFGUID, std::string>::iterator itRet = mObjectRemoveList.find(pSchedule->self);
+				if (itRet == mObjectRemoveList.end())
 				{
-					pSchedule->mnRemainCount--;
-					pSchedule->DoHeartBeatEvent();
+					if (itRet->second != pSchedule->mstrScheduleName)
+					{
+						if (pSchedule->mnRemainCount > 0 || pSchedule->mbForever == true)
+						{
+							pSchedule->mnRemainCount--;
+							pSchedule->DoHeartBeatEvent();
 
-					if (pSchedule->mnRemainCount <= 0 && pSchedule->mbForever == false)
-					{
-						mObjectRemoveList.insert(std::map<NFGUID, std::string>::value_type(pSchedule->self, pSchedule->mstrScheduleName));
-					}
-					else
-					{
-						NFINT64 nNextCostTime = NFINT64(pSchedule->mfIntervalTime * 1000) * (pSchedule->mnAllCount - pSchedule->mnRemainCount);
-						pSchedule->mnNextTriggerTime = pSchedule->mnStartTime + nNextCostTime;
+							if (pSchedule->mnRemainCount <= 0 && pSchedule->mbForever == false)
+							{
+								mObjectRemoveList.insert(std::map<NFGUID, std::string>::value_type(pSchedule->self, pSchedule->mstrScheduleName));
+							}
+							else
+							{
+								NFINT64 nNextCostTime = NFINT64(pSchedule->mfIntervalTime * 1000) * (pSchedule->mnAllCount - pSchedule->mnRemainCount);
+								pSchedule->mnNextTriggerTime = pSchedule->mnStartTime + nNextCostTime;
+							}
+						}
 					}
 				}
-				
 			}
 
 			pSchedule = xObjectSchedule->Next();
@@ -145,8 +151,6 @@ bool NFCScheduleModule::Execute()
 					xModuleSchedule->mnNextTriggerTime = xModuleSchedule->mnStartTime + nNextCostTime;
 				}
 			}
-
-			
 		}
 
 		xModuleSchedule = mModuleScheduleMap.Next();
