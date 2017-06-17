699e976aa79d549658a3bc4c0f3b91bd11e4b800
ketoo <342006@qq.com>
2017-06-17 00:34:11 -0700

    HttpServer

--- NFComm/NFNetPlugin/NFCHttpNetModule.h
+++ NFComm/NFNetPlugin/NFCHttpNetModule.h
@@ -30,9 +30,9 @@ public:
 	virtual bool AddMsgCB(const std::string& strCommand, const HTTP_RECEIVE_FUNCTOR_PTR& cb);
 	virtual bool AddComMsgCB(const HTTP_RECEIVE_FUNCTOR_PTR& cb);
 
-	virtual bool SendMsg(const NFHttpRequest & req, const int nCommand, const std::string& strMsg);
-	virtual bool SendMsg(const NFHttpRequest& req, const std::string& strMsg, NFWebStatus code = NFWebStatus::WEB_OK, const std::string& reason = "OK");
-	virtual bool SendFile(const NFHttpRequest& req, const std::string& strPath, const std::string& strFileName);
+	virtual bool ResponseMsg(const NFHttpRequest & req, const int nCommand, const std::string& strMsg);
+	virtual bool ResponseMsg(const NFHttpRequest& req, const std::string& strMsg, NFWebStatus code = NFWebStatus::WEB_OK, const std::string& reason = "OK");
+	virtual bool ResponseFile(const NFHttpRequest& req, const std::string& strPath, const std::string& strFileName);
 
 private:
 	void OnReceiveNetPack(const NFHttpRequest& req, const std::string& strCommand, const std::string& strUrl);
