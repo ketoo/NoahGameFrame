40a11252fa80da3e91abf664be65c1c079a93cc7
ketoo <342006@qq.com>
2017-06-16 21:45:52 -0700

    add files for http net library

---
+++ NFComm/NFPluginModule/NFIMasterNet_HttpServerModule.h
@@ -0,0 +1,14 @@
+#ifndef NFI_MASTER_HTTP_SERVER_MODULE_H
+#define NFI_MASTER_HTTP_SERVER_MODULE_H
+
+#include <iostream>
+#include "NFComm/NFPluginModule/NFIModule.h"
+
+class NFIMasterNet_HttpServerModule
+	: public NFIModule
+{
+
+public:
+};
+
+#endif
