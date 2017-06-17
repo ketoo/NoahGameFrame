c6bf6a9401efb1c25045a1f279f8c4ea7f4cf29d
ketoo <342006@qq.com>
2017-06-16 22:03:21 -0700

    http

--- NFComm/NFNetPlugin/NFIHttpNet.h
+++ NFComm/NFNetPlugin/NFIHttpNet.h
@@ -42,9 +42,12 @@ class NFHttpRequest
 {
 public:
 	void* req;
+	int nCommand;
 	std::string url;
 };
 
+//it should be
+//typedef std::function<void(const NFHttpRequest& req, const int nCommand, const std::string& strData, const std::string& strUrl)> HTTP_RECEIVE_FUNCTOR;
 typedef std::function<void(const NFHttpRequest& req, const std::string& strCommand, const std::string& strUrl)> HTTP_RECEIVE_FUNCTOR;
 typedef std::shared_ptr<HTTP_RECEIVE_FUNCTOR> HTTP_RECEIVE_FUNCTOR_PTR;
 
@@ -56,6 +59,8 @@ public:
 	virtual bool Final() = 0;
 
 public:
+	virtual bool SendMsg(const NFHttpRequest & req, const int nCommand, const std::string& strMsg) = 0;
+	
 	virtual bool SendMsg(const NFHttpRequest & req, const std::string& strMsg, NFWebStatus code, const std::string& strReason = "OK") = 0;
 
 	virtual bool SendFile(const NFHttpRequest& req, const std::string& strPath, const std::string& strFileName) = 0;
