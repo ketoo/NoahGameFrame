b5eb2709928be1bbe4f7002a4250fdba2e800a38
bluesky7290 <liuping1216@vip.qq.com>
2017-06-23 19:56:30 +0800

    fixed Navigation plugin

--- NFComm/NFPluginModule/NFINavigationModule.h
+++ NFComm/NFPluginModule/NFINavigationModule.h
@@ -15,15 +15,15 @@
 class NFINavigationModule : public NFIModule
 {
 public:
-	virtual bool hasNavigation(NFINT64 scendId) = 0;
+	virtual bool ExistNavigation(NFINT64 scendId) = 0;
 
-	virtual bool removeNavigation(NFINT64 scendId) = 0;
+	virtual bool RemoveNavigation(NFINT64 scendId) = 0;
 
-	virtual int findStraightPath(NFINT64 scendId, const NFVector3& start, const NFVector3& end, std::vector<NFVector3>& paths) = 0;
+	virtual int FindPath(NFINT64 scendId, const NFVector3& start, const NFVector3& end, std::vector<NFVector3>& paths) = 0;
 
-	virtual int findRandomPointAroundCircle(NFINT64 scendId, const NFVector3& centerPos, std::vector<NFVector3>& points, NFINT32 max_points, float maxRadius) = 0;
+	virtual int FindRandomPointAroundCircle(NFINT64 scendId, const NFVector3& centerPos, std::vector<NFVector3>& points, NFINT32 max_points, float maxRadius) = 0;
 
-	virtual int raycast(NFINT64 scendId, const NFVector3& start, const NFVector3& end, std::vector<NFVector3>& hitPointVec) = 0;
+	virtual int Raycast(NFINT64 scendId, const NFVector3& start, const NFVector3& end, std::vector<NFVector3>& hitPointVec) = 0;
 };
 
 #endif
