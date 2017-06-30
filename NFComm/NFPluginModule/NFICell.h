84c0d314d5a56d3ff83a12882c6ffee376ea2d22
ketoo <342006@qq.com>
2017-06-30 03:50:54 -0700

    add cell for multiple actor

---
+++ NFComm/NFPluginModule/NFICell.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFICell.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2017-06-30
+//    @Module           :    NFICell
+//
+// -------------------------------------------------------------------------
+
+#ifndef NFI_CELL_H
+#define NFI_CELL_H
+
+#include "NFIModule.h"
+
+class NFICell : public NFIModule
+{
+
+};
+
+#endif
