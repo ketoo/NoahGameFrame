3aca527ca58c5b4fe87714c95db953589d6efb6b
ketoo <342006@qq.com>
2017-07-07 04:36:52 -0700

    compiling

--- NFComm/NFPluginModule/NFIHttpClientModule.h
+++ NFComm/NFPluginModule/NFIHttpClientModule.h
@@ -17,8 +17,24 @@ class NFIHttpClientModule
 {
 public:
 	virtual ~NFIHttpClientModule() {};
+	/*
+	bool PerformGet(const std::string& strUri, const std::string& strUserData,
+		BaseType* pBase,
+		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
+	{
+		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
+		return PerformGet(strUri, strUserData, pd);
+	}
 
-
+	template<typename BaseType>
+	bool PerformPost(const std::string& strUri, const std::string& strUserData, const std::string& strPostData,
+		BaseType* pBase,
+		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
+	{
+		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
+		return PerformPost(strUri, strUserData, strPostData, pd);
+	}
+	*/
 };
 
 #endif
