da18e5a60498793277c6e6cd345728a324ab4db1
ketoo <342006@qq.com>
2017-07-08 01:56:06 -0700

    1

--- NFComm/NFNetPlugin/NFCHttpClient.h
+++ NFComm/NFNetPlugin/NFCHttpClient.h
@@ -60,24 +60,6 @@ public:
 
 	virtual bool PerformPost(const std::string& strUri, const std::string& strUserData, const std::string& strPostData, HTTP_RESP_FUNCTOR_PTR pCB);
 
-	template<typename BaseType>
-	bool PerformGet(const std::string& strUri, const std::string& strUserData,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformGet(strUri, strUserData, pd);
-	}
-
-	template<typename BaseType>
-	bool PerformPost(const std::string& strUri, const std::string& strUserData, const std::string& strPostData,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformPost(strUri, strUserData, strPostData, pd);
-	}
-
 private:
 	static void OnHttpReqDone(struct evhttp_request *req, void *ctx);
 	bool MakeRequest(const std::string& strUri,
@@ -85,7 +67,6 @@ private:
 		const std::string& strPostData,
 		HTTP_RESP_FUNCTOR_PTR pCB,
 		bool bPost = false);
-
 
 private:
 	std::string			m_strUserAgent;
