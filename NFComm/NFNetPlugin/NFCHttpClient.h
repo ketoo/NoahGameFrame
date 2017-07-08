c96a6f4f1bcaef8ea4bd1b72b3ac7c930902ca08
ketoo <342006@qq.com>
2017-07-08 01:54:31 -0700

    add interface for httpclient

--- NFComm/NFNetPlugin/NFCHttpClient.h
+++ NFComm/NFNetPlugin/NFCHttpClient.h
@@ -10,7 +10,34 @@
 #include "NFIHttpClient.h"
 
 #define DEFAULT_USER_AGENT "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36"
-#define EVENT__SIZEOF_SIZE_T
+
+class HttpObject
+{
+public:
+	HttpObject(NFIHttpClient* pNet, struct bufferevent* pBev, const std::string& strUserData, HTTP_RESP_FUNCTOR_PTR pCB)
+	{
+		m_pHttpClient = pNet;
+		m_pBev = pBev;
+		m_strUserData = strUserData;
+		m_pCB = pCB;
+	}
+
+	virtual ~HttpObject()
+	{
+	}
+
+	void SetUri(const std::string& strUri) {
+		m_strUri = strUri;
+	}
+
+	bufferevent*		m_pBev;
+	std::string			m_strUserData;
+	NFIHttpClient*		m_pHttpClient;
+	HTTP_RESP_FUNCTOR_PTR m_pCB;
+	std::string			m_strUri;
+};
+
+
 class NFCHttpClient : public NFIHttpClient
 {
 public:
@@ -28,6 +55,10 @@ public:
 	virtual bool Initialization(const std::string& strUserAgent = std::string(DEFAULT_USER_AGENT));
 
 	virtual bool Final();
+
+	virtual bool PerformGet(const std::string& strUri, const std::string& strUserData, HTTP_RESP_FUNCTOR_PTR pCB);
+
+	virtual bool PerformPost(const std::string& strUri, const std::string& strUserData, const std::string& strPostData, HTTP_RESP_FUNCTOR_PTR pCB);
 
 	template<typename BaseType>
 	bool PerformGet(const std::string& strUri, const std::string& strUserData,
@@ -55,20 +86,17 @@ private:
 		HTTP_RESP_FUNCTOR_PTR pCB,
 		bool bPost = false);
 
-	bool PerformGet(const std::string& strUri, const std::string& strUserData, HTTP_RESP_FUNCTOR_PTR pCB);
-
-	bool PerformPost(const std::string& strUri, const std::string& strUserData, const std::string& strPostData, HTTP_RESP_FUNCTOR_PTR pCB);
 
 private:
 	std::string			m_strUserAgent;
 	struct event_base*	m_pBase = nullptr;
+
+	int					m_nRetry = 2;
+	int					m_nTimeOut = 2;
+
 #if NF_ENABLE_SSL
 	SSL_CTX *			m_pSslCtx = nullptr;
 #endif
-	int					m_nRetry = 2;
-	int					m_nTimeOut = 2;
 };
-
-#pragma pack(pop)
 
 #endif
