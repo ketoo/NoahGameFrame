57c1cd8ce43a7e04929494dc3e050f7e3c6b47ee
stone <xinxinst@163.com>
2017-07-10 18:40:19 +0800

    tidy up the HttpClient module api

--- NFComm/NFNetPlugin/NFCHttpClientModule.h
+++ NFComm/NFNetPlugin/NFCHttpClientModule.h
@@ -27,13 +27,20 @@ public:
 	
 	virtual bool Shut();
 
-	virtual bool PerformGet(const std::string& strUri, const std::string& strUserData, HTTP_RESP_FUNCTOR_PTR pCB);
+	virtual bool PerformGet(const std::string& strUri,
+		HTTP_RESP_FUNCTOR_PTR pCB,
+		const std::string& strUserData = std::string(),
+		const std::map<std::string,std::string>& xHeaders = m_xDefaultHttpHeaders);
 
-	virtual bool PerformPost(const std::string& strUri, const std::string& strUserData, const std::string& strPostData, HTTP_RESP_FUNCTOR_PTR pCB);
-
+	virtual bool PerformPost(const std::string& strUri, 
+		const std::string& strPostData, 
+		HTTP_RESP_FUNCTOR_PTR pCB, 
+		const std::string& strUserData = std::string(),
+		const std::map<std::string, std::string>& xHeaders = m_xDefaultHttpHeaders);
 
 private:
 	NFIHttpClient* m_pHttpClient;
+	static std::map<std::string, std::string> m_xDefaultHttpHeaders;
 };
 
 #endif
