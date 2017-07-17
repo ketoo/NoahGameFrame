b4b14b3c5c9254bf79205845b303ac84a1adb9bd
stone <xinxinst@163.com>
2017-07-17 10:52:02 +0800

    change default headers to subclass

--- NFComm/NFNetPlugin/NFCHttpClientModule.cpp
+++ NFComm/NFNetPlugin/NFCHttpClientModule.cpp
@@ -11,56 +11,57 @@
 
 #define DEFAULT_USER_AGENT "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Mobile Safari/537.36"
 
-NFCHttpClientModule::NFCHttpClientModule(NFIPluginManager * p)
+NFCHttpClientModule::NFCHttpClientModule ( NFIPluginManager * p )
 {
-	pPluginManager = p;
-	m_pHttpClient = new NFCHttpClient();
-	m_xDefaultHttpHeaders = {
-		{ "Connection", "close" },
-		{ "Content-Type", "text/plain;text/html;application/x-www-form-urlencoded;charset=utf-8" },
-		{ "User-Agent", DEFAULT_USER_AGENT },
-		{ "Cache-Control", "no-cache" }
-	};
+    pPluginManager = p;
+    m_pHttpClient = new NFCHttpClient();
+    m_xDefaultHttpHeaders =
+    {
+        { "Connection", "close" },
+        { "Content-Type", "text/plain;text/html;application/x-www-form-urlencoded;charset=utf-8" },
+        { "User-Agent", DEFAULT_USER_AGENT },
+        { "Cache-Control", "no-cache" }
+    };
 }
 
 NFCHttpClientModule::~NFCHttpClientModule()
 {
-	delete m_pHttpClient;
-	m_pHttpClient = NULL;
+    delete m_pHttpClient;
+    m_pHttpClient = NULL;
 }
 
 bool NFCHttpClientModule::Init()
 {
-	m_pHttpClient->Init();
+    m_pHttpClient->Init();
 
-	return true;
+    return true;
 }
 
 bool NFCHttpClientModule::Execute()
 {
-	m_pHttpClient->Execute();
-	return true;
+    m_pHttpClient->Execute();
+    return true;
 }
 
 bool NFCHttpClientModule::Shut()
 {
-	m_pHttpClient->Final();
+    m_pHttpClient->Final();
 
-	return true;
+    return true;
 }
 
-bool NFCHttpClientModule::PerformGet(const std::string& strUri, 
-	HTTP_RESP_FUNCTOR_PTR pCB, 
-	const std::string& strUserData, 
-	const std::map<std::string, std::string>& xHeaders)
+bool NFCHttpClientModule::PerformGet ( const std::string& strUri,
+                                       HTTP_RESP_FUNCTOR_PTR pCB,
+                                       const std::string& strUserData,
+                                       const std::map<std::string, std::string>& xHeaders )
 {
-	return m_pHttpClient->PerformGet(strUri,pCB,strUserData,xHeaders);
+    return m_pHttpClient->PerformGet ( strUri, pCB, strUserData, xHeaders.size() == 0 ? m_xDefaultHttpHeaders : xHeaders );
 }
 
-bool NFCHttpClientModule::PerformPost(const std::string& strUri, const std::string& strPostData, 
-	HTTP_RESP_FUNCTOR_PTR pCB, 
-	const std::string& strUserData, 
-	const std::map<std::string, std::string>& xHeaders)
+bool NFCHttpClientModule::PerformPost ( const std::string& strUri, const std::string& strPostData,
+                                        HTTP_RESP_FUNCTOR_PTR pCB,
+                                        const std::string& strUserData,
+                                        const std::map<std::string, std::string>& xHeaders )
 {
-	return m_pHttpClient->PerformPost(strUri,strPostData,pCB,strUserData,xHeaders);
+    return m_pHttpClient->PerformPost ( strUri, strPostData, pCB, strUserData, xHeaders.size() == 0 ? m_xDefaultHttpHeaders : xHeaders );
 }
