82706c5d95eacf12eab4e836fe5c6b97474ddbc2
stone <xinxinst@163.com>
2017-07-17 10:52:02 +0800

    change default headers to subclass

--- NFComm/NFPluginModule/NFIHttpClientModule.h
+++ NFComm/NFPluginModule/NFIHttpClientModule.h
@@ -13,80 +13,78 @@
 #include "NFComm/NFNetPlugin/NFIHttpClient.h"
 
 class NFIHttpClientModule
-	: public NFIModule
+    : public NFIModule
 {
 public:
-	virtual ~NFIHttpClientModule() {};
+    virtual ~NFIHttpClientModule() {};
 
-	template<typename BaseType>
-	bool PerformGet(const std::string& strUri,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformGet(strUri,pd,std::string(),m_xDefaultHttpHeaders);
-	}
+    template<typename BaseType>
+    bool PerformGet ( const std::string& strUri,
+                      BaseType* pBase,
+                      void ( BaseType::*handleRecieve ) ( const int state_code, const std::string& strRespData, const std::string& strUserData ) )
+    {
+        HTTP_RESP_FUNCTOR_PTR pd ( new HTTP_RESP_FUNCTOR ( std::bind ( handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3 ) ) );
+        return PerformGet ( strUri, pd, std::string(), std::map<std::string, std::string>() );
+    }
 
-	template<typename BaseType>
-	bool PerformGet(const std::string& strUri, const std::string& strUserData,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformGet(strUri, pd,strUserData, m_xDefaultHttpHeaders);
-	}
+    template<typename BaseType>
+    bool PerformGet ( const std::string& strUri, const std::string& strUserData,
+                      BaseType* pBase,
+                      void ( BaseType::*handleRecieve ) ( const int state_code, const std::string& strRespData, const std::string& strUserData ) )
+    {
+        HTTP_RESP_FUNCTOR_PTR pd ( new HTTP_RESP_FUNCTOR ( std::bind ( handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3 ) ) );
+        return PerformGet ( strUri, pd, strUserData, std::map<std::string, std::string>() );
+    }
 
-	template<typename BaseType>
-	bool PerformGet(const std::string& strUri, const std::string& strUserData,const std::map<std::string, std::string>& xHeaders,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformGet(strUri, pd, strUserData, xHeaders);
-	}
+    template<typename BaseType>
+    bool PerformGet ( const std::string& strUri, const std::string& strUserData, const std::map<std::string, std::string>& xHeaders,
+                      BaseType* pBase,
+                      void ( BaseType::*handleRecieve ) ( const int state_code, const std::string& strRespData, const std::string& strUserData ) )
+    {
+        HTTP_RESP_FUNCTOR_PTR pd ( new HTTP_RESP_FUNCTOR ( std::bind ( handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3 ) ) );
+        return PerformGet ( strUri, pd, strUserData, xHeaders );
+    }
 
-	template<typename BaseType>
-	bool PerformPost(const std::string& strUri,const std::string& strPostData,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformPost(strUri, strPostData, pd, std::string(), m_xDefaultHttpHeaders);
-	}
-
-
-	template<typename BaseType>
-	bool PerformPost(const std::string& strUri,const std::string& strPostData, const std::string& strUserData,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformPost(strUri,strPostData, pd,strUserData, m_xDefaultHttpHeaders);
-	}
+    template<typename BaseType>
+    bool PerformPost ( const std::string& strUri, const std::string& strPostData,
+                       BaseType* pBase,
+                       void ( BaseType::*handleRecieve ) ( const int state_code, const std::string& strRespData, const std::string& strUserData ) )
+    {
+        HTTP_RESP_FUNCTOR_PTR pd ( new HTTP_RESP_FUNCTOR ( std::bind ( handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3 ) ) );
+        return PerformPost ( strUri, strPostData, pd, std::string(), std::map<std::string, std::string>() );
+    }
 
 
-	template<typename BaseType>
-	bool PerformPost(const std::string& strUri,const std::string& strPostData, const std::string& strUserData,const std::map<std::string, std::string>& xHeaders,
-		BaseType* pBase,
-		void (BaseType::*handleRecieve)(const int state_code, const std::string& strRespData, const std::string& strUserData))
-	{
-		HTTP_RESP_FUNCTOR_PTR pd(new HTTP_RESP_FUNCTOR(std::bind(handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)));
-		return PerformPost(strUri, strPostData, pd, strUserData, xHeaders);
-	}
+    template<typename BaseType>
+    bool PerformPost ( const std::string& strUri, const std::string& strPostData, const std::string& strUserData,
+                       BaseType* pBase,
+                       void ( BaseType::*handleRecieve ) ( const int state_code, const std::string& strRespData, const std::string& strUserData ) )
+    {
+        HTTP_RESP_FUNCTOR_PTR pd ( new HTTP_RESP_FUNCTOR ( std::bind ( handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3 ) ) );
+        return PerformPost ( strUri, strPostData, pd, strUserData, std::map<std::string, std::string>() );
+    }
+
+
+    template<typename BaseType>
+    bool PerformPost ( const std::string& strUri, const std::string& strPostData, const std::string& strUserData, const std::map<std::string, std::string>& xHeaders,
+                       BaseType* pBase,
+                       void ( BaseType::*handleRecieve ) ( const int state_code, const std::string& strRespData, const std::string& strUserData ) )
+    {
+        HTTP_RESP_FUNCTOR_PTR pd ( new HTTP_RESP_FUNCTOR ( std::bind ( handleRecieve, pBase, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3 ) ) );
+        return PerformPost ( strUri, strPostData, pd, strUserData, xHeaders );
+    }
 
 protected:
-	virtual bool PerformGet(const std::string& strUri,
-		HTTP_RESP_FUNCTOR_PTR pCB,
-		const std::string& strUserData,
-		const std::map<std::string, std::string>& xHeaders) = 0 ;
+    virtual bool PerformGet ( const std::string& strUri,
+                              HTTP_RESP_FUNCTOR_PTR pCB,
+                              const std::string& strUserData,
+                              const std::map<std::string, std::string>& xHeaders ) = 0 ;
 
-	virtual bool PerformPost(const std::string& strUri,
-		const std::string& strPostData,
-		HTTP_RESP_FUNCTOR_PTR pCB,
-		const std::string& strUserData,
-		const std::map<std::string, std::string>& xHeaders) = 0;
-
-	std::map<std::string, std::string> m_xDefaultHttpHeaders;
+    virtual bool PerformPost ( const std::string& strUri,
+                               const std::string& strPostData,
+                               HTTP_RESP_FUNCTOR_PTR pCB,
+                               const std::string& strUserData,
+                               const std::map<std::string, std::string>& xHeaders ) = 0;
 };
 
 #endif
