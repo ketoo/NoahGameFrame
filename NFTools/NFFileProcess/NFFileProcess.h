dac5e38ebf2f98beaace1735776a2221e6d345a3
ketoo <342006@qq.com>
2017-07-19 02:27:13 -0700

    new code for tool

--- NFTools/NFFileProcess/NFFileProcess.h
+++ NFTools/NFFileProcess/NFFileProcess.h
@@ -1,11 +1,6 @@
 #pragma once
 #include "NFComm/NFPluginModule/NFPlatform.h"
-#include "Dependencies/RapidXML/rapidxml.hpp"
-#include "Dependencies/RapidXML/rapidxml_iterators.hpp"
-#include "Dependencies/RapidXML/rapidxml_print.hpp"
-#include "Dependencies/RapidXML/rapidxml_utils.hpp"
 #include "Dependencies/common/lexical_cast.hpp"
-//#include "tinyxml2.h"
 #include "MiniExcelReader.h"
 #include <algorithm>
 #include <fstream>
@@ -71,42 +66,45 @@ public:
 	{
 	}
 	std::string strClassName;
-	std::map<std::string, NFClassProperty*> propertyList;//key, desc
-	std::map<std::string, NFClassRecord*> xObjectRecordList;//name, desc
+	std::map<std::string, NFClassProperty*> xPropertyList;//key, desc
+	std::map<std::string, NFClassRecord*> xRecordList;//name, desc
 };
 
-class NFClassIni
+class NFClassElement
 {
 public:
-	NFClassIni()
+	NFClassElement()
 	{
 	}
 
-	class IniData
+	class ElementData
 	{
 	public:
-		std::map<std::string, std::string> propertyList;
+		std::map<std::string, std::string> xPropertyList;
 	};
 
-	std::map<std::string, IniData*> iniList;//key, iniList
+	std::map<std::string, ElementData*> xElementList;//key, iniList
 };
 
 class ClassData
 {
 public:
 	NFClassStruct xStructData;
-	NFClassIni xIni;
+	NFClassElement xIniData;
 };
 
 class NFFileProcess
 {
 public:
 	NFFileProcess();
-	~NFFileProcess();
-
-	///////////////////////////////////////////////////////////////
+	virtual ~NFFileProcess();
 
 	bool LoadDataFromExcel();
+	bool Save();
+
+	void SetUTF8(const bool b);
+
+private:
 	bool LoadDataFromExcel(const std::string& strFile, const std::string& strFileName);
 	bool LoadDataFromExcel(MiniExcelReader::Sheet& sheet, ClassData* pClassData);
 
@@ -115,39 +113,20 @@ public:
 	bool LoadDataAndProcessComponent(MiniExcelReader::Sheet& sheet, ClassData* pClassData);
 	bool LoadDataAndProcessRecord(MiniExcelReader::Sheet& sheet, ClassData* pClassData);
 
-	///////////////////////////////////////////////////////////////
-
-	bool Save();
 	bool SaveForCPP();
 	bool SaveForCS();
 	bool SaveForJAVA();
 	bool SaveForPB();
 	bool SaveForSQL();
 
+	bool SaveForStruct();
 	bool SaveForIni();
 
-	///////////////////////////////////////////////////////////////
-	void OnCreateXMLFile();
-	void OnCreateMysqlFile();
-	void SetUTF8(const bool b);
-
-private:
-	void CreateProtoFile();
-	void CreateFileHead();
-	void CreateStructThreadFunc();
-	void CreateIniThreadFunc();
-	bool CreateStructForApp(std::string strFile, std::string strFileName);
-	bool CreateIniXML(std::string strFile);
-	bool LoadLogicClass(std::string strFile);
-	bool LoadClass(std::string strFile, std::string strTable);
 
 	std::vector<std::string> GetFileListInFolder(std::string folderPath, int depth);
 	void StringReplace(std::string &strBig, const std::string &strsrc, const std::string &strdst);
-	char * NewChar(const std::string& str);
 
 private:
-
-	//NFMapEx<std::string, NFClassStruct> mxClassStruct;
 
 	bool bConvertIntoUTF8 = false;
 
@@ -156,39 +135,17 @@ private:
 	const int nRecordHeight = 11;//record line
 	const int nRecordDescHeight = 7;//record line
 
-	std::vector<char *> tmpStrList;
 	std::string strCipherCfg = "conf";
 
-	std::string strExecutePath = "NFDataCfg/";
-	std::string strToolBasePath = "../";
-	std::string strRelativePath = "../../";
-	std::string strExcelStructPath = "Excel_Struct/";
-	std::string strXMLStructPath = "Struct/";
+	std::string strExcelIniPath = "../Excel/";
+	std::string strXMLStructPath = "../Struct/";
+	std::string strXMLIniPath = "../Ini/";
 
-	std::string strExcelIniPath = "Excel_Ini/";
-	std::string strXMLIniPath = "Ini/";
-
-	std::string strLogicClassFile = "";
 	std::string strMySQLFile = "../mysql/NFrame.sql";
-	std::string strMySQLClassFile = "../mysql/NFClass.sql";
-
 	std::string strProtoFile = "../proto/NFRecordDefine.proto";
-
 	std::string strHPPFile = "../proto/NFProtocolDefine.hpp";
 	std::string strJavaFile = "../proto/NFProtocolDefine.java";
 	std::string strCSFile = "../proto/NFProtocolDefine.cs";
-
-
-	FILE* mysqlWriter = nullptr;
-	FILE* mysqlClassWriter = nullptr;
-	FILE* protoWriter = nullptr;
-	FILE* hppWriter = nullptr;
-	FILE* javaWriter = nullptr;
-	FILE* csWriter = nullptr;
-
-	std::string strHppIObjectInfo;
-	std::string strJavaIObjectInfo;
-	std::string strCSIObjectInfo;
 
 	std::map<std::string, ClassData*> mxClassData;
 };
