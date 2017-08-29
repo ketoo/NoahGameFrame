3d8df4090280d0628956f39c77e0d5fb66c5de76
James <342006@qq.com>
2017-08-29 15:30:30 +1200

    OPTIMIZE FOR REMOVE THE WARNING

--- NFServer/NFLoginNet_ClientPlugin/NFCLoginToMasterModule.cpp
+++ NFServer/NFLoginNet_ClientPlugin/NFCLoginToMasterModule.cpp
@@ -101,7 +101,7 @@ void NFCLoginToMasterModule::Register(NFINet* pNet)
             {
                 const int nPort = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::Port());
                 const int nMaxConnect = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::MaxOnline());
-                const int nCpus = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::CpuCount());
+                //const int nCpus = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::CpuCount());
                 const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::Name());
                 const std::string& strIP = m_pElementModule->GetPropertyString(strId, NFrame::Server::IP());
 
