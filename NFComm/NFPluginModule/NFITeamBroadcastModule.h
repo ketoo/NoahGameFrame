6f3facd4f9bd1a1c32163c89636c4190ea47fd3d
James <342006@qq.com>
2017-09-25 17:31:02 +1300

    add hiredis & build on mac

---
+++ NFComm/NFPluginModule/NFITeamBroadcastModule.h
@@ -0,0 +1,29 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFITeamBroadcastModule.h
+//    @Author           :    ChuanBo.Guo
+//    @Date             :    2016-01-30 16:51
+//    @Module           :    NFITeamBroadcastModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef NFI_TEAM_BROADCAST_MODULE_H
+#define NFI_TEAM_BROADCAST_MODULE_H
+
+#include "NFIModule.h"
+#include "NFComm/NFMessageDefine/NFProtocolDefine.hpp"
+#ifdef _MSC_VER
+#pragma warning(disable: 4244 4267)
+#endif
+#include "NFComm/NFMessageDefine/NFDefine.pb.h"
+#ifdef _MSC_VER
+#pragma warning(default: 4244 4267)
+#endif
+
+class NFITeamBroadcastModule
+    : public NFIModule
+{
+protected:
+private:
+};
+
+#endif
