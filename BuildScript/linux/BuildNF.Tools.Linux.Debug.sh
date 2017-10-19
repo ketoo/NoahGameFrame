0eb263e6561c3c3b2785b16953b5c24a3a133020
kytoo <342006@qq.com>
2017-10-19 22:07:18 +1300

    new interface class for nodal

--- BuildScript/linux/BuildNF.Tools.Linux.Debug.sh
+++
@@ -1,3 +0,0 @@
-cd Build.Tools
-make
-cd ../
--- install4cmake.sh
+++ install4cmake.sh
@@ -10,14 +10,12 @@ fi
 #compile dep libraries
 cd ./Dependencies
 chmod -R 755 build_dep.sh
-./build_dep.sh
+#./build_dep.sh
 cd ../
 
 cd BuildScript/linux/
 chmod -R 755 ./BuildNF.CMake.Tools.sh
-chmod -R 755 ./BuildNF.Tools.Linux.Debug.sh
 ./BuildNF.CMake.Tools.sh
-./BuildNF.Tools.Linux.Debug.sh
 cd ../../
 
 cd _Out/NFDataCfg/Tool/
@@ -32,17 +30,13 @@ cd BuildScript/linux/
 chmod -R 755 ./BuildNF.CMake.Debug.sh
 chmod -R 755 ./BuildNF.CMake.Release.sh
 ./BuildNF.CMake.Debug.sh
-./BuildNF.CMake.Release.sh
+#./BuildNF.CMake.Release.sh
 cd ../../
 
 cd _Out/
 chmod 777 *.sh
 cd  Tester/
 chmod 777 *.sh
-
 cd ../../
-# generate makefile
-#chmod -R 755 Build4Makefile.sh
-#./Build4Makefile.sh
 
 #pwd
