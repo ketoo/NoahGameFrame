f86cedbc543e135cfde20063d1f1d238e8a8944c
ketoo <342006@qq.com>
2017-10-22 02:49:47 -0700

    redis name

--- NFComm/NFNoSqlPlugin/dllmain.cpp
+++ NFComm/NFNoSqlPlugin/dllmain.cpp
@@ -2,28 +2,27 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "anet_win64_d.lib" )
+#pragma comment( lib, "redisclient_d.lib" )
 #pragma comment( lib, "NFCore_d.lib" )
 #pragma comment( lib, "Theron_d.lib" )
-#elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-
-
-#elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
+#else
+#pragma comment( lib, "redisclient_d.a" )
+#pragma comment( lib, "libtherond.a")
+#pragma comment( lib, "NFCore_d.a")
 #endif
 
 #else
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "anet_win64.lib" )
+#pragma comment( lib, "redisclient.lib" )
 #pragma comment( lib, "Theron.lib" )
 #pragma comment( lib, "NFCore.lib" )
 
-#elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "anet_win64.lib" )
-#pragma comment( lib, "libtherond.a")
+#else
+#pragma comment( lib, "redisclient.a" )
+#pragma comment( lib, "libtheron.a")
 #pragma comment( lib, "NFCore.a")
-#elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
 
 #endif
