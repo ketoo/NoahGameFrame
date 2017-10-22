a2399c5b965102cdf3d9407a6c536cdc896d5ff0
ketoo <342006@qq.com>
2017-10-22 16:30:08 -0700

    added scale plugin

---
+++ NFComm/NFPluginModule/NFIScaleModule.h
@@ -0,0 +1,20 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFCScaleModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2017-10-23 12:22
+//    @Module           :    NFCScaleModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+
+#ifndef NFI_SCALE_MODULE_H
+#define NFI_SCALE_MODULE_H
+
+#include "NFIModule.h"
+
+class NFIScaleModule : public NFIModule
+{
+public:
+};
+
+#endif
