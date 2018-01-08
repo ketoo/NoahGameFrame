921c289eb8c16e75efb9711cbe2ecacbb02f5d63
ketoo <342006@qq.com>
2018-01-07 22:07:12 -0800

    fixed

--- NFServer/NFGameLogicPlugin/NFCSurvivalModule.cpp
+++ NFServer/NFGameLogicPlugin/NFCSurvivalModule.cpp
@@ -1,13 +1,12 @@
 // -------------------------------------------------------------------------
-//    @FileName				:    NFCSurvivalModule.cpp
-//    @Author               :    LvSheng.Huang
-//    @Date                 :    2017-03-29
-//    @Module               :    NFCSurvivalModule
-//    @Desc                 :
+//    @FileName			:    NFCSurvivalModule.cpp
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2017-03-29
+//    @Module           :    NFCSurvivalModule
+//
 // -------------------------------------------------------------------------
 
 #include "NFCSurvivalModule.h"
-#include "NFComm/NFMessageDefine/NFProtocolDefine.hpp"
 
 bool NFCSurvivalModule::Init()
 {
@@ -24,42 +23,35 @@ bool NFCSurvivalModule::Init()
 	m_pLogModule = pPluginManager->FindModule<NFILogModule>();
 	m_pRankModule = pPluginManager->FindModule<NFIRankModule>();
 
-    mnMinPeopleForSurvivalMode = 50;
-
-    return true;
-}
-
-bool NFCSurvivalModule::AfterInit()
-{
-    m_pScheduleModule->AddSchedule("CheckSurvivalList", this, &NFCSurvivalModule::CheckSurvivalList, 10, -1);
-	return true;
-}
-
-bool NFCSurvivalModule::ReadyExecute()
-{
-
+	mnMinPeopleForSurvivalMode = 50;
 
 	return true;
 }
 
 bool NFCSurvivalModule::Shut()
 {
-    return true;
+	return true;
 }
 
 bool NFCSurvivalModule::Execute()
 {
-    return true;
+	return true;
 }
 
-int NFCSurvivalModule::CheckSurvivalList(const std::string& strScheduleName, const float fIntervalTime, const int nCount)
+bool NFCSurvivalModule::AfterInit()
 {
-    m_pLogModule->Debug(strScheduleName);
+	m_pScheduleModule->AddSchedule("CheckSurvivalList", this, &NFCSurvivalModule::CheckSurvivalList, 10, -1);
+	return true;
+}
 
-    if (m_pRankModule->GetRankListCount(NFIRankModule::RANK_TYPE::RT_SURVIVAL_MODE) >= mnMinPeopleForSurvivalMode)
-    {
+int NFCSurvivalModule::CheckSurvivalList(const std::string & strScheduleName, const float fIntervalTime, const int nCount)
+{
+	m_pLogModule->Debug(strScheduleName);
 
-    }
+	if (m_pRankModule->GetRankListCount(NFIRankModule::RANK_TYPE::RT_SURVIVAL_MODE) >= mnMinPeopleForSurvivalMode)
+	{
 
-    return 0;
+	}
+
+	return 0;
 }
