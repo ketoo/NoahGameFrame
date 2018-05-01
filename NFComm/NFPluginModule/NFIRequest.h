72ca267c4f8d6fc65a9858da2ea84ef0da5ad52d
ketoo <342006@qq.com>
2018-05-01 03:45:14 -0700

    http  -> login
    add ajson lib

---
+++ NFComm/NFPluginModule/NFIRequest.h
@@ -0,0 +1,24 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFIRequest.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2018-04-29
+//    @Module           :    NFIRequest
+//
+// -------------------------------------------------------------------------
+
+#ifndef NFI_REQUEST_H
+#define NFI_REQUEST_H
+
+#include <map>
+#include <string>
+#include "Dependencies/ajson/ajson.hpp"
+
+class NFIRequest
+{
+public:
+	
+
+};
+
+
+#endif
