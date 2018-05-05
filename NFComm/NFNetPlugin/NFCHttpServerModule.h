93c7838d7de2babd858513e38154c95dc0f8f286
ketoo <342006@qq.com>
2018-05-04 21:51:52 -0700

    modified filter interface for http module

--- NFComm/NFNetPlugin/NFCHttpServerModule.h
+++ NFComm/NFNetPlugin/NFCHttpServerModule.h
@@ -30,25 +30,25 @@ public:
 
     virtual bool Execute();
 
-    virtual bool AddMsgCB(const std::string& strCommand, const NFHttpType eRequestType, const HTTP_RECEIVE_FUNCTOR_PTR& cb);
-
-
     virtual bool ResponseMsg(const NFHttpRequest& req, const std::string& strMsg, NFWebStatus code = NFWebStatus::WEB_OK,
                 const std::string& reason = "OK");
 
 private:
 	virtual bool OnReceiveNetPack(const NFHttpRequest& req);
+	virtual NFWebStatus OnFilterPack(const NFHttpRequest& req);
+
+	virtual bool AddMsgCB(const std::string& strCommand, const NFHttpType eRequestType, const HTTP_RECEIVE_FUNCTOR_PTR& cb);
+	virtual bool AddFilterCB(const std::string& strCommand, const HTTP_FILTER_FUNCTOR_PTR& cb);
 	virtual bool AddComMsgCB(const HTTP_RECEIVE_FUNCTOR_PTR& cb);
-	virtual bool AddFilterCB(const HTTP_FILTER_FUNCTOR_PTR& cb);
 
 private:
 	NFIHttpServer* m_pHttpServer;
 	NFILogModule* mLogModule;
-	HTTP_FILTER_FUNCTOR_PTR mFilter;
 
 	NFMapEx<NFHttpType, std::map<std::string, HTTP_RECEIVE_FUNCTOR_PTR>> mMsgCBMap;
+	std::map<std::string, HTTP_FILTER_FUNCTOR_PTR> mMsgFliterMap;
 
-    std::list<HTTP_RECEIVE_FUNCTOR_PTR> mComMsgCBList;
+    HTTP_RECEIVE_FUNCTOR_PTR mComMsgCBList;
 };
 
 #endif
