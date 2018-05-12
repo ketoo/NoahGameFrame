dcff56a493256069224ac47c1df65cf8bd1ee862
ketoo <342006@qq.com>
2018-05-12 16:02:19 -0700

    fixed project for 140

--- NFComm/NFNoSqlPlugin/dllmain.cpp
+++ NFComm/NFNoSqlPlugin/dllmain.cpp
@@ -2,29 +2,21 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "redisclient_d.lib" )
-#pragma comment( lib, "NFCore_d.lib" )
-#pragma comment( lib, "Theron_d.lib" )
 #pragma comment( lib, "hiredis_d.lib") 
-//#pragma comment( lib, "Win32_Interop_d.lib") 
+#pragma comment( lib, "Win32_Interop_d.lib") 
 #else
-#pragma comment( lib, "redisclient_d.a" )
-#pragma comment( lib, "libtherond.a")
-#pragma comment( lib, "NFCore_d.a")
+
 #endif
 
 #else
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
-#pragma comment( lib, "redisclient.lib" )
-#pragma comment( lib, "Theron.lib" )
-#pragma comment( lib, "NFCore.lib" )
+#pragma comment( lib, "hiredis.lib") 
+#pragma comment( lib, "Win32_Interop.lib") 
 
 #else
-#pragma comment( lib, "redisclient.a" )
-#pragma comment( lib, "libtheron.a")
-#pragma comment( lib, "NFCore.a")
+
 #endif
 
 #endif
