20d0c683d35869beca34711d0653aed20997346a
xiaolanpingguo <xiaolanpingguo@163.com>
2018-05-12 20:41:13 +0800

    resolved conflict

--- NFComm/NFPluginModule/NFINoSqlModule.h
+++ NFComm/NFPluginModule/NFINoSqlModule.h
@@ -10,12 +10,19 @@
 #define NFI_NOSQL_MODULE_H
 
 #include "NFComm/NFPluginModule/NFIModule.h"
+<<<<<<< Updated upstream
 #include "NFComm/NFPluginModule/NFIRedisClient.h"
 
 
 class NFINoSqlModule
 	: public NFIModule,
 public NFIRedisClient
+=======
+
+
+class NFINoSqlModule
+	: public NFIModule
+>>>>>>> Stashed changes
 {
 public:
 
@@ -24,10 +31,17 @@ public:
 	virtual bool AddConnectSql(const std::string& strID, const std::string& strIP, const int nPort, const std::string& strPass) = 0;
 
 	virtual NFList<std::string> GetDriverIdList() = 0;
+<<<<<<< Updated upstream
 	virtual NF_SHARE_PTR<NFIRedisClient>  GetDriver(const std::string& strID) = 0;
 	virtual NF_SHARE_PTR<NFIRedisClient>  GetDriverBySuitRandom() = 0;
 	virtual NF_SHARE_PTR<NFIRedisClient>  GetDriverBySuitConsistent() = 0;
 	virtual NF_SHARE_PTR<NFIRedisClient>  GetDriverBySuit(const std::string& strHash) = 0;
+=======
+	virtual NF_SHARE_PTR<NFRedisClient>  GetDriver(const std::string& strID) = 0;
+	virtual NF_SHARE_PTR<NFRedisClient>  GetDriverBySuitRandom() = 0;
+	virtual NF_SHARE_PTR<NFRedisClient>  GetDriverBySuitConsistent() = 0;
+	virtual NF_SHARE_PTR<NFRedisClient>  GetDriverBySuit(const std::string& strHash) = 0;
+>>>>>>> Stashed changes
 	//virtual NF_SHARE_PTR<NFINoSqlDriver>  GetDriverBySuit(const int nHash) = 0;
 	virtual bool RemoveConnectSql(const std::string& strID) = 0;
 };
