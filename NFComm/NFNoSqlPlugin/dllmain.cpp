9165f254c1830fe66630e14989b2e27db1e86672
xiaolanpingguo <xiaolanpingguo@163.com>
2018-05-15 16:26:40 +0800

    resolve redis link error for win

--- NFComm/NFNoSqlPlugin/dllmain.cpp
+++ NFComm/NFNoSqlPlugin/dllmain.cpp
@@ -2,8 +2,8 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
+#pragma comment( lib, "libevent.lib" )
 #pragma comment( lib, "hiredis_d.lib") 
-#pragma comment( lib, "Win32_Interop_d.lib") 
 #else
 
 #endif
@@ -12,8 +12,8 @@
 
 #if NF_PLATFORM == NF_PLATFORM_WIN
 #pragma comment( lib, "ws2_32" )
+#pragma comment( lib, "libevent.lib" )
 #pragma comment( lib, "hiredis.lib") 
-#pragma comment( lib, "Win32_Interop.lib") 
 
 #else
 
