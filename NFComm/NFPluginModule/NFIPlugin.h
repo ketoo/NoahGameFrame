534ed44a2855b75d6f34ce7870397f85b344c4b3
James <342006@qq.com>
2018-06-06 11:59:09 +1200

    new interfaces for test plugin

--- NFComm/NFPluginModule/NFIPlugin.h
+++ NFComm/NFPluginModule/NFIPlugin.h
@@ -20,12 +20,26 @@
 	assert((TIsDerived<className, classBaseName>::Result));	\
 	NFIModule* pRegisterModule##className= new className(pManager); \
     pRegisterModule##className->strName = (#className); \
-    pManager->AddModule( #classBaseName, pRegisterModule##className );AddElement( #classBaseName, pRegisterModule##className );
+    pManager->AddModule( #classBaseName, pRegisterModule##className );\
+    AddElement( #classBaseName, pRegisterModule##className );
 
-#define UNREGISTER_MODULE(pManager, classBaseName, className) NFIModule* pUnRegisterModule##className =  \
-	dynamic_cast<NFIModule*>( pManager->FindModule( #classBaseName )); \
-	pManager->RemoveModule( #classBaseName ); RemoveElement( #classBaseName ); delete pUnRegisterModule##className;
+#define REGISTER_TEST_MODULE(pManager, classBaseName, className)  \
+	assert((TIsDerived<classBaseName, NFIModule>::Result));	\
+	assert((TIsDerived<className, NFIModule>::Result));	\
+	NFIModule* pRegisterModule##className= new className(pManager); \
+    pRegisterModule##className->strName = (#className); \
+    pManager->AddTestModule( #classBaseName, pRegisterModule##className );
 
+#define UNREGISTER_MODULE(pManager, classBaseName, className) \
+    NFIModule* pUnRegisterModule##className = dynamic_cast<NFIModule*>( pManager->FindModule( #classBaseName )); \
+	pManager->RemoveModule( #classBaseName ); \
+    RemoveElement( #classBaseName ); \
+    delete pUnRegisterModule##className;
+
+#define UNREGISTER_TEST_MODULE(pManager, classBaseName, className) \
+    NFIModule* pUnRegisterModule##className = dynamic_cast<NFIModule*>( pManager->FindtESTModule( #classBaseName )); \
+	pManager->RemoveTestModule( #classBaseName ); \
+    delete pUnRegisterModule##className;
 
 #define CREATE_PLUGIN(pManager, className)  NFIPlugin* pCreatePlugin##className = new className(pManager); pManager->Registered( pCreatePlugin##className );
 
