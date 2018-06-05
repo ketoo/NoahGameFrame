1ac6afcfd2af390139ad3313cc5fc34c1289d5f7
James <342006@qq.com>
2018-06-06 11:59:09 +1200

    new interfaces for test plugin

--- NFComm/NFKernelPlugin/NFKernelPlugin.cpp
+++ NFComm/NFKernelPlugin/NFKernelPlugin.cpp
@@ -47,10 +47,23 @@ void NFKernelPlugin::Install()
 	REGISTER_MODULE(pPluginManager, NFIKernelModule, NFCKernelModule)
 	REGISTER_MODULE(pPluginManager, NFIEventModule, NFCEventModule)
 	REGISTER_MODULE(pPluginManager, NFIScheduleModule, NFCScheduleModule)
+
+	/*
+	REGISTER_TEST_MODULE(pPluginManager, NFISceneAOIModule, NFCSceneAOITestModule)
+	REGISTER_TEST_MODULE(pPluginManager, NFIKernelModule, NFCKernelTestModule)
+	REGISTER_TEST_MODULE(pPluginManager, NFIEventModule, NFCEventTestModule)
+	REGISTER_TEST_MODULE(pPluginManager, NFIScheduleModule, NFCScheduleTestModule)
+	*/
 }
 
 void NFKernelPlugin::Uninstall()
 {
+	/*
+	UNREGISTER_TEST_MODULE(pPluginManager, NFIEventModule, NFCEventTestModule)
+	UNREGISTER_TEST_MODULE(pPluginManager, NFIKernelModule, NFCKernelTestModule)
+	UNREGISTER_TEST_MODULE(pPluginManager, NFISceneAOIModule, NFCSceneAOITestModule)
+	UNREGISTER_TEST_MODULE(pPluginManager, NFIScheduleModule, NFCScheduleTestModule)
+*/
 	UNREGISTER_MODULE(pPluginManager, NFIEventModule, NFCEventModule)
 	UNREGISTER_MODULE(pPluginManager, NFIKernelModule, NFCKernelModule)
 	UNREGISTER_MODULE(pPluginManager, NFISceneAOIModule, NFCSceneAOIModule)
