1a32bc0a8bbbd90ecb5d49524752533a7fd8edc4
James <342006@qq.com>
2018-06-06 16:33:23 +1200

    added db server

---
+++ NFServer/NFDBNet_ServerPlugin/NFCDBNet_ServerModule.h
@@ -0,0 +1,71 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFCDBNet_ServerModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2013-01-02
+//    @Module           :    NFCDBNet_ServerModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+#ifndef NFC_WORLDNET_SERVER_MODULE_H
+#define NFC_WORLDNET_SERVER_MODULE_H
+
+#include "NFComm/NFCore/NFMap.hpp"
+#include "NFComm/NFMessageDefine/NFMsgDefine.h"
+#include "NFComm/NFPluginModule/NFIWorldToMasterModule.h"
+#include "NFComm/NFPluginModule/NFIWorldLogicModule.h"
+#include "NFComm/NFPluginModule/NFINetModule.h"
+#include "NFComm/NFPluginModule/NFIClassModule.h"
+#include "NFComm/NFPluginModule/NFIElementModule.h"
+#include "NFComm/NFPluginModule/NFILogModule.h"
+#include "NFComm/NFPluginModule/NFIWorldNet_ServerModule.h"
+#include "NFComm/NFPluginModule/NFIKernelModule.h"
+#include "NFComm/NFPluginModule/NFILoginNet_ServerModule.h"
+#include "NFComm/NFPluginModule/NFIWorldToMasterModule.h"
+
+class NFCDBNet_ServerModule
+    : public NFIWorldNet_ServerModule
+{
+public:
+    NFCDBNet_ServerModule(NFIPluginManager* p)
+    {
+        pPluginManager = p;
+        mnLastCheckTime = pPluginManager->GetNowTime();
+    }
+
+    virtual bool Init();
+    virtual bool Shut();
+    virtual bool Execute();
+
+    virtual bool AfterInit();
+
+protected:
+
+    void OnSocketEvent(const NFSOCK nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet);
+
+    void OnClientDisconnect(const NFSOCK nAddress);
+
+protected:
+
+    void OnGameServerRegisteredProcess(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
+    void OnGameServerUnRegisteredProcess(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
+
+    void OnRefreshWorldServerInfoProcess(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
+
+
+private:
+
+    NFINT64 mnLastCheckTime;
+
+    //serverid,data
+	NFConsistentHashMapEx<int, ServerData> mWorldMap;
+	NFConsistentHashMapEx<int, ServerData> mGameMap;
+
+    NFIElementModule* m_pElementModule;
+    NFIClassModule* m_pClassModule;
+    NFIKernelModule* m_pKernelModule;
+    NFILogModule* m_pLogModule;
+	NFINetModule* m_pNetModule;
+	NFINetClientModule* m_pNetClientModule;
+};
+
+#endif
---
+++ NFServer/NFDBNet_ServerPlugin/NFDBNet_ServerPlugin.cpp
@@ -0,0 +1,43 @@
+// NFWorldNet_ServerPlugin.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "NFWorldNet_ServerPlugin.h"
+#include "NFCDBNet_ServerModule.h"
+
+//
+//
+#ifdef NF_DYNAMIC_PLUGIN
+
+NF_EXPORT void DllStartPlugin(NFIPluginManager* pm)
+{
+    CREATE_PLUGIN(pm, NFWorldNet_ServerPlugin)
+
+};
+
+NF_EXPORT void DllStopPlugin(NFIPluginManager* pm)
+{
+    DESTROY_PLUGIN(pm, NFWorldNet_ServerPlugin)
+};
+
+#endif
+//////////////////////////////////////////////////////////////////////////
+
+const int NFWorldNet_ServerPlugin::GetPluginVersion()
+{
+    return 0;
+}
+
+const std::string NFWorldNet_ServerPlugin::GetPluginName()
+{
+	return GET_CLASS_NAME(NFWorldNet_ServerPlugin);
+}
+
+void NFWorldNet_ServerPlugin::Install()
+{
+    REGISTER_MODULE(pPluginManager, NFIWorldNet_ServerModule, NFCDBNet_ServerModule)
+}
+
+void NFWorldNet_ServerPlugin::Uninstall()
+{
+    UNREGISTER_MODULE(pPluginManager, NFIWorldNet_ServerModule, NFCDBNet_ServerModule)
+}
