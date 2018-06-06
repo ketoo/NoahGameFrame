39c6d66b349349c062aacd730eb904dae2c1b16c
James <342006@qq.com>
2018-06-06 16:33:23 +1200

    added db server

---
+++ NFServer/NFDBNet_ClientPlugin/NFCDBToWorldModule.h
@@ -0,0 +1,73 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFCDBToWorldModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2013-01-02
+//    @Module           :    NFCDBToWorldModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+#ifndef NFC_DB_NET_CLIENT_MODULE_H
+#define NFC_DB_NET_CLIENT_MODULE_H
+
+#include "NFComm/NFMessageDefine/NFMsgDefine.h"
+#include "NFComm/NFPluginModule/NFIWorldToMasterModule.h"
+#include "NFComm/NFPluginModule/NFIWorldLogicModule.h"
+#include "NFComm/NFPluginModule/NFINetModule.h"
+#include "NFComm/NFPluginModule/NFIClassModule.h"
+#include "NFComm/NFPluginModule/NFIElementModule.h"
+#include "NFComm/NFPluginModule/NFILogModule.h"
+#include "NFComm/NFPluginModule/NFIWorldNet_ServerModule.h"
+#include "NFComm/NFPluginModule/NFINetClientModule.h"
+#include "NFComm/NFPluginModule/NFISecurityModule.h"
+
+class NFIDBToMasterModule : public NFIModule
+{
+
+};
+
+class NFCDBToWorldModule
+	: public NFIDBToMasterModule
+{
+public:
+	NFCDBToWorldModule(NFIPluginManager* p)
+	{
+		pPluginManager = p;
+		mLastReportTime = 0;
+	}
+
+	virtual bool Init();
+	virtual bool BeforeShut();
+	virtual bool Shut();
+	virtual bool Execute();
+	virtual bool AfterInit();
+
+protected:
+
+	void OnSocketMSEvent(const NFSOCK nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet);
+	
+	void OnClientDisconnect(const NFSOCK nAddress);
+	
+	void OnClientConnected(const NFSOCK nAddress);
+
+	virtual void LogServerInfo(const std::string& strServerInfo);
+
+
+	void Register(NFINet* pNet);
+	void ServerReport();
+	void RefreshWorldInfo();
+	void OnServerInfoProcess(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
+
+	void InvalidMessage(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen);
+private:
+	NFINT64 mLastReportTime;
+
+	NFILogModule* m_pLogModule;
+	NFIElementModule* m_pElementModule;
+	NFIClassModule* m_pClassModule;
+	NFIWorldNet_ServerModule* m_pWorldNet_ServerModule;
+	NFINetClientModule* m_pNetClientModule;
+	NFINetModule* m_pNetModule;
+	NFISecurityModule* m_pSecurityModule;
+};
+
+#endif
---
+++ NFServer/NFDBNet_ServerPlugin/NFCDBNet_ServerModule.cpp
@@ -0,0 +1,215 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFCDBNet_ServerModule.cpp
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2013-01-02
+//    @Module           :    NFCDBNet_ServerModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+#include "NFCDBNet_ServerModule.h"
+#include "NFDBNet_ServerPlugin.h"
+#include "NFComm/NFMessageDefine/NFMsgDefine.h"
+#include "NFComm/NFMessageDefine/NFProtocolDefine.hpp"
+
+bool NFCDBNet_ServerModule::Init()
+{
+	m_pNetModule = pPluginManager->FindModule<NFINetModule>();
+	m_pKernelModule = pPluginManager->FindModule<NFIKernelModule>();
+	m_pLogModule = pPluginManager->FindModule<NFILogModule>();
+	m_pElementModule = pPluginManager->FindModule<NFIElementModule>();
+	m_pClassModule = pPluginManager->FindModule<NFIClassModule>();
+	m_pNetClientModule = pPluginManager->FindModule<NFINetClientModule>();
+
+    return true;
+}
+
+bool NFCDBNet_ServerModule::AfterInit()
+{
+	m_pNetModule->AddReceiveCallBack(NFMsg::EGMI_GTW_GAME_REGISTERED, this, &NFCDBNet_ServerModule::OnGameServerRegisteredProcess);
+	m_pNetModule->AddReceiveCallBack(NFMsg::EGMI_GTW_GAME_UNREGISTERED, this, &NFCDBNet_ServerModule::OnGameServerUnRegisteredProcess);
+	m_pNetModule->AddReceiveCallBack(NFMsg::EGMI_GTW_GAME_REFRESH, this, &NFCDBNet_ServerModule::OnRefreshGameServerInfoProcess);
+	m_pNetModule->AddReceiveCallBack(NFMsg::EGMI_STS_SERVER_REPORT, this, &NFCDBNet_ServerModule::OnTransmitServerReport);
+
+    m_pNetModule->AddEventCallBack(this, &NFCDBNet_ServerModule::OnSocketEvent);
+	m_pNetModule->ExpandBufferSize();
+
+    NF_SHARE_PTR<NFIClass> xLogicClass = m_pClassModule->GetElement(NFrame::Server::ThisName());
+    if (xLogicClass)
+    {
+		const std::vector<std::string>& strIdList = xLogicClass->GetIDList();
+		for (int i = 0; i < strIdList.size(); ++i)
+		{
+			const std::string& strId = strIdList[i];
+
+            const int nServerType = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::Type());
+            const int nServerID = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::ServerID());
+            if (nServerType == NF_SERVER_TYPES::NF_ST_WORLD && pPluginManager->GetAppID() == nServerID)
+            {
+                const int nPort = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::Port());
+                const int nMaxConnect = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::MaxOnline());
+                const int nCpus = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::CpuCount());
+                //const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::Name());
+                //const std::string& strIP = m_pElementModule->GetPropertyString(strId, NFrame::Server::IP());
+
+                int nRet = m_pNetModule->Initialization(nMaxConnect, nPort, nCpus);
+                if (nRet < 0)
+                {
+                    std::ostringstream strLog;
+                    strLog << "Cannot init server net, Port = " << nPort;
+                    m_pLogModule->LogNormal(NFILogModule::NLL_ERROR_NORMAL, NULL_OBJECT, strLog, __FUNCTION__, __LINE__);
+                    NFASSERT(nRet, "Cannot init server net", __FILE__, __FUNCTION__);
+                    exit(0);
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+void NFCDBNet_ServerModule::OnServerInfoProcess(const NFSOCK nSockIndex, const int nMsgID, const char * msg, const uint32_t nLen)
+{
+	NFGUID nPlayerID;
+	NFMsg::ServerInfoReportList xMsg;
+	if (!NFINetModule::ReceivePB(nMsgID, msg, nLen, xMsg, nPlayerID))
+	{
+		return;
+	}
+
+	for (int i = 0; i < xMsg.server_list_size(); ++i)
+	{
+		const NFMsg::ServerInfoReport& xData = xMsg.server_list(i);
+		if (xData.server_type() == NF_SERVER_TYPES::NF_ST_WORLD)
+		{
+			NF_SHARE_PTR<ServerData> pServerData = mWorldMap.GetElement(xData.server_id());
+			if (!pServerData)
+			{
+				pServerData = NF_SHARE_PTR<ServerData>(NF_NEW ServerData());
+				mWorldMap.AddElement(xData.server_id(), pServerData);
+			}
+
+			pServerData->nFD = nSockIndex;
+			*(pServerData->pData) = xData;
+
+			m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, xData.server_id()), xData.server_name(), "GameServerRegistered");
+		}
+	}
+
+	//sync to proxy
+
+	//sync to game
+
+}
+
+bool NFCDBNet_ServerModule::Shut()
+{
+
+    return true;
+}
+
+bool NFCDBNet_ServerModule::Execute()
+{
+    LogGameServer();
+
+	return true;
+}
+
+void NFCDBNet_ServerModule::OnGameServerRegisteredProcess(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen)
+{
+    NFGUID nPlayerID;
+    NFMsg::ServerInfoReportList xMsg;
+    if (!m_pNetModule->ReceivePB( nMsgID, msg, nLen, xMsg, nPlayerID))
+    {
+        return;
+    }
+
+    for (int i = 0; i < xMsg.server_list_size(); ++i)
+    {
+        const NFMsg::ServerInfoReport& xData = xMsg.server_list(i);
+        NF_SHARE_PTR<ServerData> pServerData =  mGameMap.GetElement(xData.server_id());
+        if (!pServerData)
+        {
+            pServerData = NF_SHARE_PTR<ServerData>(NF_NEW ServerData());
+            mGameMap.AddElement(xData.server_id(), pServerData);
+        }
+
+        pServerData->nFD = nSockIndex;
+        *(pServerData->pData) = xData;
+
+        m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, xData.server_id()), xData.server_name(), "GameServerRegistered");
+    }
+
+    SynGameToProxy();
+}
+
+void NFCDBNet_ServerModule::OnGameServerUnRegisteredProcess(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen)
+{
+    NFGUID nPlayerID;
+    NFMsg::ServerInfoReportList xMsg;
+    if (!m_pNetModule->ReceivePB( nMsgID, msg, nLen, xMsg, nPlayerID))
+    {
+        return;
+    }
+
+    for (int i = 0; i < xMsg.server_list_size(); ++i)
+    {
+        const NFMsg::ServerInfoReport& xData = xMsg.server_list(i);
+        mGameMap.RemoveElement(xData.server_id());
+
+        m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, xData.server_id()), xData.server_name(), "GameServerRegistered");
+    }
+}
+
+void NFCDBNet_ServerModule::OnRefreshWorldServerInfoProcess(const NFSOCK nSockIndex, const int nMsgID, const char* msg, const uint32_t nLen)
+{
+    NFGUID nPlayerID;
+    NFMsg::ServerInfoReportList xMsg;
+    if (!m_pNetModule->ReceivePB( nMsgID, msg, nLen, xMsg, nPlayerID))
+    {
+        return;
+    }
+
+    for (int i = 0; i < xMsg.server_list_size(); ++i)
+    {
+        const NFMsg::ServerInfoReport& xData = xMsg.server_list(i);
+
+        NF_SHARE_PTR<ServerData> pServerData =  mGameMap.GetElement(xData.server_id());
+        if (!pServerData)
+        {
+            pServerData = NF_SHARE_PTR<ServerData>(NF_NEW ServerData());
+            mGameMap.AddElement(xData.server_id(), pServerData);
+        }
+
+        pServerData->nFD = nSockIndex;
+        *(pServerData->pData) = xData;
+
+        m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, xData.server_id()), xData.server_name(), "GameServerRegistered");
+    }
+
+    SynGameToProxy();
+}
+
+void NFCDBNet_ServerModule::OnSocketEvent(const NFSOCK nSockIndex, const NF_NET_EVENT eEvent, NFINet* pNet)
+{
+    if (eEvent & NF_NET_EVENT_EOF)
+    {
+        m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, nSockIndex), "NF_NET_EVENT_EOF", "Connection closed", __FUNCTION__, __LINE__);
+        OnClientDisconnect(nSockIndex);
+    }
+    else if (eEvent & NF_NET_EVENT_ERROR)
+    {
+        m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, nSockIndex), "NF_NET_EVENT_ERROR", "Got an error on the connection", __FUNCTION__, __LINE__);
+        OnClientDisconnect(nSockIndex);
+    }
+    else if (eEvent & NF_NET_EVENT_TIMEOUT)
+    {
+        m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, nSockIndex), "NF_NET_EVENT_TIMEOUT", "read timeout", __FUNCTION__, __LINE__);
+        OnClientDisconnect(nSockIndex);
+    }
+    else  if (eEvent == NF_NET_EVENT_CONNECTED)
+    {
+        m_pLogModule->LogNormal(NFILogModule::NLL_INFO_NORMAL, NFGUID(0, nSockIndex), "NF_NET_EVENT_CONNECTED", "connectioned success", __FUNCTION__, __LINE__);
+        OnClientConnected(nSockIndex);
+    }
+}
+
