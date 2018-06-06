558bed2e14ef13d9bf37d5d6fc58ccb52c275401
ketoo <342006@qq.com>
2018-06-06 03:03:16 -0700

    connect to DBServer from GameServer

---
+++ NFComm/NFPluginModule/NFIGameServerToDBModule.h
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFCGameServerToDBModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2013-01-02
+//    @Module           :    NFCGameServerToDBModule
+//    @Desc             :
+// -------------------------------------------------------------------------
+
+#ifndef NFI_GAMESERVERTO_DB_MODULE_H
+#define NFI_GAMESERVERTO_DB_MODULE_H
+
+#include <iostream>
+#include "NFIModule.h"
+#include "NFINetClientModule.h"
+
+class NFIGameServerToDBModule
+	: public NFIModule
+{
+public:
+
+};
+
+#endif
