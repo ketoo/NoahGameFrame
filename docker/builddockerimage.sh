c6cfe5607f2cc320885b15b159e3d6d82af07302
ketoo <342006@qq.com>
2018-06-09 01:30:35 -0700

    1

--- docker/builddockerimage.sh
+++ docker/builddockerimage.sh
@@ -1,4 +1,6 @@
 #1 pull the Dockerfile from github
+
+rm -rf NoahGameFrame
 git clone https://github.com/ketoo/NoahGameFrame.git
 
 #2 building the Dockerfile to build a docker image with the app's binary file
@@ -9,6 +11,8 @@ docker build -t var .
 #3 push the image to docker repos
 
 docker push jameshuang4nz/var
+
+rm -rf NoahGameFrame
 
 #4 the other server pull the docker image and run it
 
