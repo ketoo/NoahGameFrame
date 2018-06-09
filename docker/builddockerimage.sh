a6430ed0062035742bd4ccff12419ae77c7305f0
ketoo <342006@qq.com>
2018-06-09 01:01:55 -0700

    fixed Dockerfile

--- docker/builddockerimage.sh
+++ docker/builddockerimage.sh
@@ -2,11 +2,12 @@
 
 #2 building the Dockerfile to build a docker image with the app's binary file
 
-docker build -t date +%Y%m%d_%H%M .
+var=`date "+%Y%m%d_%H%M"`
+docker build -t var .
 
 #3 push the image to docker repos
 
-docker push jameshuang4nz/sss
+docker push jameshuang4nz/var
 
 #4 the other server pull the docker image and run it
 
