9fb737872644c075c75af014469c344c0f5b0958
ketoo <342006@qq.com>
2018-06-09 00:29:15 -0700

    1

--- docker/install4docker.sh
+++ docker/install4docker.sh
@@ -1,10 +1,2 @@
-
 curl -fsSL get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-sudo systemctl enable docker
-sudo systemctl start docker
-
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
+sh get-docker.sh
