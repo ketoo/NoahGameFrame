90f17266194d4a624f2cbedfec1d9a3d23a25366
James <342006@qq.com>
2018-06-11 16:29:32 +1200

    docker images script

--- _Out/redis.sh
+++ _Out/redis.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd redis
 
 
--- docker/builddockerimage.sh
+++ docker/builddockerimage.sh
@@ -1,3 +1,7 @@
+export DOCKER_ID_USER="jameshuang4nz"
+export DOCKER_ID_PWD="*********"
+export REPOS_NAME="noahframe"
+
 #1 pull the Dockerfile from github
 
 rm -rf NoahGameFrame
@@ -6,13 +10,15 @@ git clone https://github.com/ketoo/NoahGameFrame.git
 #2 building the Dockerfile to build a docker image with the app's binary file
 
 var=`date "+%Y%m%d_%H%M"`
-docker build -t var .
+docker build -t $DOCKER_ID_USER/$REPOS_NAME:$var .
 
 #3 push the image to docker repos
 
-docker push jameshuang4nz/var
+docker login -u $DOCKER_ID_USER -p $DOCKER_ID_PWD
+docker push $DOCKER_ID_USER/$REPOS_NAME:$var
 
 rm -rf NoahGameFrame
 
 #4 the other server pull the docker image and run it
 
+#docker pull --all-tags jameshuang4nz/noahframe
