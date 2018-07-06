6c8c85ef99506ecd10b5b85c4a87bd3306c3e844
kytoo <342006@qq.com>
2018-07-06 15:22:52 +1200

    data tail module

---
+++ NFComm/NFKernelPlugin/NFCDataTailModule.h
@@ -0,0 +1,57 @@
+// -------------------------------------------------------------------------
+//    @FileName			:    NFCDataTailModule.h
+//    @Author           :    LvSheng.Huang
+//    @Date             :    2013-09-30
+//    @Module           :    NFCDataTailModule
+//
+// -------------------------------------------------------------------------
+
+#ifndef NFC_DATA_TRAIL_MODULE_H
+#define NFC_DATA_TRAIL_MODULE_H
+
+#include "NFComm/NFPluginModule/NFIKernelModule.h"
+#include "NFComm/NFPluginModule/NFIPropertyModule.h"
+#include "NFComm/NFPluginModule/NFIElementModule.h"
+#include "NFComm/NFPluginModule/NFIClassModule.h"
+#include "NFComm/NFPluginModule/NFIPropertyConfigModule.h"
+#include "NFComm/NFPluginModule/NFIPluginManager.h"
+#include "NFComm/NFPluginModule/NFIDataTailModule.h"
+#include "NFComm/NFPluginModule/NFILogModule.h"
+
+class NFCDataTailModule
+    : public NFIDataTailModule
+{
+public:
+    NFCDataTailModule(NFIPluginManager* p)
+    {
+        pPluginManager = p;
+    }
+    virtual ~NFCDataTailModule() {};
+
+    virtual bool Init();
+    virtual bool Shut();
+    virtual bool Execute();
+    virtual bool AfterInit();
+
+	virtual void LogObjectData(const NFGUID& self);
+    virtual void StartTrail(const NFGUID& self);
+
+protected:
+    void PrintStackTrace();
+
+    int TrailObjectData(const NFGUID& self);
+
+    int OnObjectPropertyEvent(const NFGUID& self, const std::string& strPropertyName, const NFData& oldVar, const NFData& newVar);
+
+    int OnObjectRecordEvent(const NFGUID& self, const RECORD_EVENT_DATA& xEventData, const NFData& oldVar, const NFData& newVar);
+
+private:
+
+    NFIKernelModule* m_pKernelModule;
+    NFIElementModule* m_pElementModule;
+    NFIClassModule* m_pClassModule;
+    NFILogModule* m_pLogModule;
+};
+
+
+#endif
