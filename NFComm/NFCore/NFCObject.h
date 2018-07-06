3cb33a1b6eba327f83ffbfb12cfd42593b49dbfd
James.Huang <james.huang@latipay.net>
2018-07-06 21:20:25 +1200

    /*             This file is part of:                 NoahFrame             http://noahframe.com
       Copyright 2009 - 2018 NoahFrame(NoahGameFrame)
    
       File creator: lvsheng.huang
    
       NoahFrame is opensorece software and you can redistribute it and/or modify
       it under the terms of the License.
    
       Licensed under the Apache License, Version 2.0 (the "License");
       you may not use this file except in compliance with the License.
       You may obtain a copy of the License at
    
           http://www.apache.org/licenses/LICENSE-2.0
    
       Unless required by applicable law or agreed to in writing, software
       distributed under the License is distributed on an "AS IS" BASIS,
       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
       See the License for the specific language governing permissions and
       limitations under the License.
    */

--- NFComm/NFCore/NFCObject.h
+++ NFComm/NFCore/NFCObject.h
@@ -1,10 +1,29 @@
-// -------------------------------------------------------------------------
-//    @FileName         :    NFCObject.h
-//    @Author           :    LvSheng.Huang
-//    @Date             :    2012-12-17
-//    @Module           :    NFCObject
-//
-// -------------------------------------------------------------------------
+/*
+            This file is part of: 
+                NoahFrame
+            http://noahframe.com
+
+   Copyright 2009 - 2018 NoahFrame(NoahGameFrame)
+
+   File creator: lvsheng.huang
+   
+   NoahFrame is opensorece software and you can redistribute it and/or modify
+   it under the terms of the License.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
 
 #ifndef NFC_OBJECT_H
 #define NFC_OBJECT_H
