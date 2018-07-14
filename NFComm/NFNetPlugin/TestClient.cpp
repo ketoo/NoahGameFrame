6259379c8ef3219cbbe8365f21da212f74175cbe
kytoo <342006@qq.com>
2018-07-14 20:11:35 +1200

    test code

--- NFComm/NFNetPlugin/TestClient.cpp
+++ NFComm/NFNetPlugin/TestClient.cpp
@@ -26,8 +26,12 @@
 #include "NFCNet.h"
 #include <thread>
 #include <string>
+#include "NFComm/NFLogPlugin/easylogging++.h"
+
 #pragma comment(lib,"ws2_32.lib")
 #pragma comment(lib,"NFNet_d.lib")
+
+INITIALIZE_EASYLOGGINGPP
 
 class TestClientClass
 {
@@ -50,10 +54,14 @@ public:
     void EventHandler(const NFSOCK nSockIndex, const NF_NET_EVENT e, NFINet* p)
     {
         std::cout << " fd: " << nSockIndex << " event_id: " << e << " thread_id: " << std::this_thread::get_id() << std::endl;
-		if(e == NF_NET_EVENT_CONNECTED)
+		if(events & BEV_EVENT_CONNECTED)
 		{
 			bConnected = true;
 		}
+        else
+        {
+            bConnected = false;
+        }
     }
 
     void Execute()
@@ -72,8 +80,89 @@ protected:
 private:
 };
 
+
+#if NF_PLATFORM == NF_PLATFORM_WIN
+
+#pragma comment( lib, "DbgHelp" )
+bool ApplicationCtrlHandler(DWORD fdwctrltype)
+{
+	switch (fdwctrltype)
+	{
+	case CTRL_C_EVENT:
+	case CTRL_CLOSE_EVENT:
+	case CTRL_BREAK_EVENT:
+	case CTRL_LOGOFF_EVENT:
+	case CTRL_SHUTDOWN_EVENT:
+	{
+		bExitApp = true;
+	}
+		return true;
+	default:
+		return false;
+	}
+}
+
+void CreateDumpFile(const std::string& strDumpFilePathName, EXCEPTION_POINTERS* pException)
+{
+    //Dump
+    HANDLE hDumpFile = CreateFile(strDumpFilePathName.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
+    dumpInfo.ExceptionPointers = pException;
+    dumpInfo.ThreadId = GetCurrentThreadId();
+    dumpInfo.ClientPointers = TRUE;
+
+    MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hDumpFile, MiniDumpNormal, &dumpInfo, NULL, NULL);
+
+    CloseHandle(hDumpFile);
+}
+
+long ApplicationCrashHandler(EXCEPTION_POINTERS* pException)
+{
+    time_t t = time(0);
+    char szDmupName[MAX_PATH];
+    tm* ptm = localtime(&t);
+
+    sprintf_s(szDmupName, "%04d_%02d_%02d_%02d_%02d_%02d.dmp",  ptm->tm_year + 1900, ptm->tm_mon + 1, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec);
+    CreateDumpFile(szDmupName, pException);
+
+    FatalAppExit(-1,  szDmupName);
+
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+#endif
+
+#if NF_PLATFORM != NF_PLATFORM_WIN
+void CrashHandler(int sig) {
+	// FOLLOWING LINE IS ABSOLUTELY NEEDED AT THE END IN ORDER TO ABORT APPLICATION
+	//el::base::debug::StackTrace();
+	//el::Helpers::logCrashReason(sig, true);
+
+
+	LOG(FATAL) << "crash sig:" << sig;
+
+	int size = 16;
+	void * array[16];
+	int stack_num = backtrace(array, size);
+	char ** stacktrace = backtrace_symbols(array, stack_num);
+	for (int i = 0; i < stack_num; ++i)
+	{
+		//printf("%s\n", stacktrace[i]);
+		LOG(FATAL) << stacktrace[i];
+	}
+
+	free(stacktrace);
+}
+#endif
+
 int main(int argc, char** argv)
 {
+#if NF_PLATFORM == NF_PLATFORM_WIN
+    SetUnhandledExceptionFilter((LPTOP_LEVEL_EXCEPTION_FILTER)ApplicationCrashHandler);
+#else
+	el::Helpers::setCrashHandler(CrashHandler);
+#endif
+
     std::list<TestClientClass*> list;
 
     for (int i = 0; i < 1000; ++i)
