0bb447570ae5384773c7ad2f8337c4b3bf5fd5f2
kytooooo <ketoo@users.noreply.github.com>
2018-08-23 14:25:45 +1200

    Update runtutoriald.sh

--- _Out/runtutoriald.sh
+++ _Out/runtutoriald.sh
@@ -6,6 +6,6 @@ cd Debug
 
 chmod -R 777  NFPluginLoader_d
 
-./NFPluginLoader_d  PluginX.xml Server=TutorialServer ID=0 Tutorial1.xml
+./NFPluginLoader_d Server=TutorialServer ID=0 Tutorial1.xml
 
 ps -A|grep NF
