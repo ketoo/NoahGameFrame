a929f06ddcc43fdd38a94ca354d1f6ff300c0db0
ketoo <342006@qq.com>
2018-08-30 02:41:08 -0700

    fixed problems for LUA

--- _Out/NFDataCfg/ScriptModule/game/test_game_module.lua
+++ _Out/NFDataCfg/ScriptModule/game/test_game_module.lua
@@ -47,7 +47,7 @@ end
 function test_game_module.after_init()
 	io.write("test_game_module after_init!----\n");
 	
-	script_module:import_proto_file("NFDataCfg\proto\NFMsgShare.proto");
+	--[[script_module:import_proto_file("NFDataCfg\proto\NFMsgShare.proto");
 	
 	local tbl = {
 	};
@@ -55,7 +55,7 @@ function test_game_module.after_init()
 	local data = script_module:encode("NFMsg.PropertyInt", tbl);
 	io.write(data);
 	local test_tbl =  script_module:decode("NFMsg.PropertyInt", data);
-	
+	--]]
 	local playerObject = script_module:create_object(NFGUID(), 1, 0, "Player", "", NFDataList());
 
 	io.write("create_object " .. playerObject:tostring() .. "\n");
