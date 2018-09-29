87e55b65b92c19eae726e1c088cc9f467ef45c49
ketoo <342006@qq.com>
2018-09-28 19:35:51 -0700

    modified shell script

--- _Out/rund.sh
+++ _Out/rund.sh
@@ -5,6 +5,7 @@ export LC_ALL="C"
 ulimit -c unlimited
 source /etc/profile
 
+
 sysOS=`uname -s`
 if [ $sysOS == "Darwin" ];then
     echo "I'm MacOS"
@@ -16,19 +17,21 @@ else
     echo "Other OS: $sysOS"
 fi
 
+
 cd Debug
 
 chmod -R 777  NFPluginLoader_d
 
-./NFPluginLoader_d -d Server=MasterServer ID=3
+./NFPluginLoader_d -d PluginX.xml Server=MasterServer ID=3
 
-./NFPluginLoader_d -d Server=WorldServer ID=7
+./NFPluginLoader_d -d PluginX.xml Server=WorldServer ID=7
 
+./NFPluginLoader_d -d PluginX.xml Server=DBServer ID=8
 
-./NFPluginLoader_d -d Server=LoginServer ID=4
+./NFPluginLoader_d -d PluginX.xml Server=LoginServer ID=4
 
-./NFPluginLoader_d -d Server=GameServer ID=6
+./NFPluginLoader_d -d PluginX.xml Server=GameServer ID=6
 
-./NFPluginLoader_d -d Server=ProxyServer ID=5
+./NFPluginLoader_d -d PluginX.xml Server=ProxyServer ID=5
 
 ps -A|grep NF
