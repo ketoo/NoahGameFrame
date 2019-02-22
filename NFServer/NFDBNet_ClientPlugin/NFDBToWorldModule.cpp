0d93f98b52b04069520a2e26ef02097a39d128a5
ketoo <342006@qq.com>
2019-02-21 21:46:34 -0800

    fixed bugs

--- NFServer/NFDBNet_ClientPlugin/NFDBToWorldModule.cpp
+++ NFServer/NFDBNet_ClientPlugin/NFDBToWorldModule.cpp
@@ -93,7 +93,7 @@ bool NFDBToWorldModule::AfterInit()
 				const int nPort = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::Port());
 				const int nMaxConnect = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::MaxOnline());
 				const int nCpus = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::CpuCount());
-				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::Name());
+				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::ID());
 				const std::string& strIP = m_pElementModule->GetPropertyString(strId, NFrame::Server::IP());
 
 				ConnectData xServerData;
@@ -102,7 +102,7 @@ bool NFDBToWorldModule::AfterInit()
 				xServerData.eServerType = (NF_SERVER_TYPES)nServerType;
 				xServerData.strIP = strIP;
 				xServerData.nPort = nPort;
-				xServerData.strName = strName;
+				xServerData.strName = strId;
 
 				m_pNetClientModule->AddServer(xServerData);
 			}
@@ -136,14 +136,14 @@ void NFDBToWorldModule::Register(NFINet* pNet)
 				const int nPort = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::Port());
 				const int nMaxConnect = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::MaxOnline());
 				const int nCpus = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::CpuCount());
-				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::Name());
+				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::ID());
 				const std::string& strIP = m_pElementModule->GetPropertyString(strId, NFrame::Server::IP());
 
 				NFMsg::ServerInfoReportList xMsg;
 				NFMsg::ServerInfoReport* pData = xMsg.add_server_list();
 
 				pData->set_server_id(nServerID);
-				pData->set_server_name(strName);
+				pData->set_server_name(strId);
 				pData->set_server_cur_count(0);
 				pData->set_server_ip(strIP);
 				pData->set_server_port(nPort);
@@ -185,13 +185,13 @@ void NFDBToWorldModule::ServerReport()
 			{
 				const int nPort = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::Port());
 				const int nMaxConnect = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::MaxOnline());
-				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::Name());
+				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::ID());
 				const std::string& strIP = m_pElementModule->GetPropertyString(strId, NFrame::Server::IP());
 
 				NFMsg::ServerInfoReport reqMsg;
 
 				reqMsg.set_server_id(nServerID);
-				reqMsg.set_server_name(strName);
+				reqMsg.set_server_name(strId);
 				reqMsg.set_server_cur_count(0);
 				reqMsg.set_server_ip(strIP);
 				reqMsg.set_server_port(nPort);
--- NFServer/NFMasterNet_ServerPlugin/NFMasterNet_ServerModule.cpp
+++ NFServer/NFMasterNet_ServerPlugin/NFMasterNet_ServerModule.cpp
@@ -277,7 +277,7 @@ bool NFMasterNet_ServerModule::AfterInit()
 				const int nPort = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::Port());
 				const int nMaxConnect = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::MaxOnline());
 				const int nCpus = m_pElementModule->GetPropertyInt32(strId, NFrame::Server::CpuCount());
-				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::Name());
+				const std::string& strName = m_pElementModule->GetPropertyString(strId, NFrame::Server::ID());
 				const std::string& strIP = m_pElementModule->GetPropertyString(strId, NFrame::Server::IP());
 
 				int nRet = m_pNetModule->Initialization(nMaxConnect, nPort, nCpus);
@@ -292,7 +292,7 @@ bool NFMasterNet_ServerModule::AfterInit()
 				NFMsg::ServerInfoReport reqMsg;
 
 				reqMsg.set_server_id(nServerID);
-				reqMsg.set_server_name(strName);
+				reqMsg.set_server_name(strId);
 				reqMsg.set_server_cur_count(0);
 				reqMsg.set_server_ip(strIP);
 				reqMsg.set_server_port(nPort);
