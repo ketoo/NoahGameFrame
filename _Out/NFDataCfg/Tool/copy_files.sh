67bc1837c730fde343b0d13ff5790c26c6ddea81
ketoo <>
2019-08-06 18:44:37 +1200

    config

--- _Out/NFDataCfg/Tool/copy_files.sh
+++ _Out/NFDataCfg/Tool/copy_files.sh
@@ -1,2 +1,15 @@
 ./NFFileProcess
 cp -a ../proto/NFProtocolDefine.hpp ../../../NFComm/NFMessageDefine
+
+rm -rf ../client
+mkdir ../client
+mkdir ../client/Ini
+mkdir ../client/proto
+mkdir ../client/Struct
+
+cp -a ../Ini ../client/
+cp -a ../proto ../client/
+cp -a ../Struct ../client/
+
+
+cp -a ../../../NFComm/NFMessageDefine/*.cs ../client/proto/
