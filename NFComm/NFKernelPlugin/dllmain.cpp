26a3c2907aa92105e759702d76b4616ae81bddd1
ketoo <342006@qq.com>
2019-10-16 21:13:54 +1300

    vcpkg

--- NFComm/NFKernelPlugin/dllmain.cpp
+++ NFComm/NFKernelPlugin/dllmain.cpp
@@ -37,13 +37,11 @@
 #pragma comment( lib, "NFCore_d.lib" )
 
 #else
-#pragma comment( lib, "NFCore_Static_d.lib" )
 #endif
 
 //
 //#pragma comment( lib, "libglog_d.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "NFCore_Static_d.a" )
 //#pragma comment( lib, "libglog_d.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
@@ -54,13 +52,11 @@
 #ifdef NF_DYNAMIC_PLUGIN
 #pragma comment( lib, "NFCore.lib" )
 #else
-#pragma comment( lib, "NFCore_Static.lib" )
 #endif
 
 //
 //#pragma comment( lib, "libglog.lib" )
 #elif NF_PLATFORM == NF_PLATFORM_LINUX || NF_PLATFORM == NF_PLATFORM_ANDROID
-#pragma comment( lib, "NFCore_Static.a" )
 //#pragma comment( lib, "libglog.a" )
 #elif NF_PLATFORM == NF_PLATFORM_APPLE || NF_PLATFORM == NF_PLATFORM_APPLE_IOS
 #endif
