0af464188b755903651d6148cd78566aba734911
ketoo <342006@qq.com>
2019-10-20 23:12:31 +1300

    build system

--- NFComm/NFMessageDefine/cpp.bat
+++ NFComm/NFMessageDefine/cpp.bat
@@ -1,11 +1,11 @@
 
-protoc -I=./ --cpp_out=./ ./NFDefine.proto
-protoc -I=./ --cpp_out=./ ./NFMsgBase.proto
-protoc -I=./ --cpp_out=./ ./NFMsgPreGame.proto
-protoc -I=./ --cpp_out=./ ./NFMsgShare.proto
-protoc -I=./ --cpp_out=./ ./NFMsgExtra.proto
-protoc -I=./ --cpp_out=./ ./NFMsgFriend.proto
-protoc -I=./ --cpp_out=./ ./NFMsgTeam.proto
+protoc.exe -I=./ --cpp_out=./ ./NFDefine.proto
+protoc.exe -I=./ --cpp_out=./ ./NFMsgBase.proto
+protoc.exe -I=./ --cpp_out=./ ./NFMsgPreGame.proto
+protoc.exe -I=./ --cpp_out=./ ./NFMsgShare.proto
+protoc.exe -I=./ --cpp_out=./ ./NFMsgExtra.proto
+protoc.exe -I=./ --cpp_out=./ ./NFMsgFriend.proto
+protoc.exe -I=./ --cpp_out=./ ./NFMsgTeam.proto
 
 
 pause
