a90a1ae5bd4d519377c6bbaa42ba1d2e79236d6e
ketoo <342006@qq.com>
2020-01-27 22:21:22 +1300

    copyright for 2020

--- NFComm/NFNoSqlPlugin/NFRedisClientSort.cpp
+++ NFComm/NFNoSqlPlugin/NFRedisClientSort.cpp
@@ -3,7 +3,7 @@
                 NoahFrame
             https://github.com/ketoo/NoahGameFrame
 
-   Copyright 2009 - 2019 NoahFrame(NoahGameFrame)
+   Copyright 2009 - 2020 NoahFrame(NoahGameFrame)
 
    File creator: lvsheng.huang
    
