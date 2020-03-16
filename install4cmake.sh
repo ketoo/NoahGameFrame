e937460bf6fc87eb0c111002e3c9a8b004be0c7e
sl-sandy <shaluoatnz@gmail.com>
2020-03-16 19:56:21 +1300

    change version checking step

--- install4cmake.sh
+++ install4cmake.sh
@@ -13,20 +13,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-g++ --version
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Please install g++ first."
-    echo "[ubuntu] sudo apt-get install g++ or [centos] yum install g++ or [mac] brew install g++"
-    exit 1
-fi
-
-g++-7 --version
-if [ $? -ne 0 ]; then
-    echo "[ERROR] Please install g++-7 first."
-    echo "[ubuntu] sudo apt-get -y install g++-7 or [centos] yum -y install g++-7 or [mac] brew install gcc@7"
-    exit 1
-fi
-
 automake --version
 if [ $? -ne 0 ]; then
     echo "[ERROR] Please install automake first."
@@ -34,6 +20,49 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+    DISTRO='CentOS'
+elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+    DISTRO='Ubuntu'
+else
+    DISTRO='unknow'
+fi
+
+sysOS=`uname -s`
+
+if [ $sysOS == "Darwin" ];then
+	gcc@7 --version
+	if [ $? -ne 0 ]; then
+		echo "[ERROR] Please install gcc@7 first."
+        echo "[mac] brew install gcc@7"
+        exit 1
+	fi
+elif [ $sysOS == "Linux" ];then
+    if [ $DISTRO == "Ubuntu" ]; then
+		g++-7 --version
+		if [ $? -ne 0 ]; then
+			echo "[ERROR] Please install g++-7 first."
+			echo "[Ubuntu] sudo apt-get -y install g++-7"
+			exit 1
+		fi
+	elif [ $DISTRO == "CentOS" ]; then
+	    g++ --version
+		if [ $? -ne 0 ]; then
+			echo "[ERROR] Please install g++-7 first."
+			echo "[Ubuntu] sudo apt-get -y install g++-7"
+			exit 1
+		fi
+		currentver="$(gcc -dumpfullversion -dumpversion)"
+        requiredver="7.0.0"
+        if [ ! "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then
+            echo "[ERROR] Upgrate g++ first, minimum version is 7.0.0"
+            echo "[CentOS Step 1] yum install centos-release-scl -y"
+            echo "[CentOS Step 2] yum install devtoolset-7 -y"
+            echo "[CentOS Step 3] scl enable devtoolset-7 bash"
+			exit 1
+        fi
+	fi
+fi
 #g++  sudo apt-get -y install g++
 #unzip sudo apt-get -y install unzip
 
