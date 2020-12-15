67c21281e4491d85a84264bb151761de684b083f
ketoo <>
2020-12-15 21:17:08 +0800

    improve code quality

--- NFComm/NFNetPlugin/NFWSModule.cpp
+++ NFComm/NFNetPlugin/NFWSModule.cpp
@@ -377,24 +377,24 @@ void NFWSModule::OnReceiveNetPack(const NFSOCK sockIndex, const int msgID, const
 #if NF_PLATFORM != NF_PLATFORM_WIN
 		NF_CRASH_TRY
 #endif
-        std::map<int, std::list<NET_RECEIVE_FUNCTOR_PTR>>::iterator it = mxReceiveCallBack.find(msgID);
+		auto it = mxReceiveCallBack.find(msgID);
         if (mxReceiveCallBack.end() != it)
         {
-            std::list<NET_RECEIVE_FUNCTOR_PTR>& xFunList = it->second;
-            for (std::list<NET_RECEIVE_FUNCTOR_PTR>::iterator itList = xFunList.begin(); itList != xFunList.end(); ++itList)
+			auto& xFunList = it->second;
+            for (auto itList = xFunList.begin(); itList != xFunList.end(); ++itList)
             {
-                NET_RECEIVE_FUNCTOR_PTR& pFunPtr = *itList;
-                NET_RECEIVE_FUNCTOR* pFunc = pFunPtr.get();
+				auto& pFunPtr = *itList;
+				auto pFunc = pFunPtr.get();
 
                 pFunc->operator()(sockIndex, msgID, msg, len);
             }
         } 
         else
         {
-            for (std::list<NET_RECEIVE_FUNCTOR_PTR>::iterator itList = mxCallBackList.begin(); itList != mxCallBackList.end(); ++itList)
+            for (auto itList = mxCallBackList.begin(); itList != mxCallBackList.end(); ++itList)
             {
-                NET_RECEIVE_FUNCTOR_PTR& pFunPtr = *itList;
-                NET_RECEIVE_FUNCTOR* pFunc = pFunPtr.get();
+				auto& pFunPtr = *itList;
+				auto pFunc = pFunPtr.get();
 
                 pFunc->operator()(sockIndex, msgID, msg, len);
             }
@@ -403,26 +403,15 @@ void NFWSModule::OnReceiveNetPack(const NFSOCK sockIndex, const int msgID, const
         NF_CRASH_END
 #endif
     }
-
-    NFPerformance performance;
-    if (performance.CheckTimePoint(1))
-    {
-        std::ostringstream os;
-        os << "---------------net module performance problem------------------- ";
-        os << performance.TimeScope();
-        os << "---------- ";
-        m_pLogModule->LogWarning(NFGUID(0, msgID), os, __FUNCTION__, __LINE__);
-    }
-
 }
 
 void NFWSModule::OnSocketNetEvent(const NFSOCK sockIndex, const NF_NET_EVENT eEvent, NFINet* pNet)
 {
-    for (std::list<NET_EVENT_FUNCTOR_PTR>::iterator it = mxEventCallBackList.begin();
+    for (auto it = mxEventCallBackList.begin();
          it != mxEventCallBackList.end(); ++it)
     {
-        NET_EVENT_FUNCTOR_PTR& pFunPtr = *it;
-        NET_EVENT_FUNCTOR* pFunc = pFunPtr.get();
+		auto& pFunPtr = *it;
+		auto pFunc = pFunPtr.get();
         pFunc->operator()(sockIndex, eEvent, pNet);
     }
 }
