a73f19124a507e4bd401d4967fe3b1c8ea32fe81
ketoo <>
2021-01-21 12:25:41 +1300

    fix bugs for new property event system

--- Tutorial/Tutorial3/HelloWorld3Module.cpp
+++ Tutorial/Tutorial3/HelloWorld3Module.cpp
@@ -80,7 +80,7 @@ int NFHelloWorld3Module::OnClassCallBackEvent(const NFGUID& self, const std::str
 	return 0;
 }
 
-int NFHelloWorld3Module::OnPropertyCallBackEvent( const NFGUID& self, const std::string& propertyName, const NFData& oldVar, const NFData& newVar)
+int NFHelloWorld3Module::OnPropertyCallBackEvent( const NFGUID& self, const std::string& propertyName, const NFData& oldVar, const NFData& newVar, const int64_t reason)
 {
 	
 	std::cout << "OnPropertyCallBackEvent Property: " << propertyName << " OldValue: " << oldVar.GetInt() << " NewValue: " << newVar.GetInt() << std::endl;
@@ -88,7 +88,7 @@ int NFHelloWorld3Module::OnPropertyCallBackEvent( const NFGUID& self, const std:
 	return 0;
 }
 
-int NFHelloWorld3Module::OnPropertyStrCallBackEvent( const NFGUID& self, const std::string& propertyName, const NFData& oldVar, const NFData& newVar)
+int NFHelloWorld3Module::OnPropertyStrCallBackEvent( const NFGUID& self, const std::string& propertyName, const NFData& oldVar, const NFData& newVar, const int64_t reason)
 {
 	
 	std::cout << "OnPropertyCallBackEvent Property: " << propertyName << " OldValue: " << oldVar.GetString() << " NewValue: " << newVar.GetString() << std::endl;
