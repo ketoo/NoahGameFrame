eb3358a53fd14503edc7050e3e2e6a6b0fd1cb50
908760230 <908760230@qq.com>
2021-11-14 08:26:50 +0800

    removed the comipled code about midware in shell script

--- BuildScript/linux/BuildNF.CMake.Release.sh
+++ BuildScript/linux/BuildNF.CMake.Release.sh
@@ -2,11 +2,7 @@ mkdir Build.Linux.Release
 
 cd Build.Linux.Release
 
-if [ "$1" == 'BUILD_MID_WARE' ] ; then
-cmake ../../../ -G "CodeBlocks - Unix Makefiles"  -DBUILD_MID_WARE_SDK=ON -DCMAKE_BUILD_TYPE=Release
-else
 cmake ../../../ -G "CodeBlocks - Unix Makefiles" -DBUILD_MID_WARE_SDK=OFF -DCMAKE_BUILD_TYPE=Release
-fi
 
 make
 cd ../
--- buildServer.sh
+++ buildServer.sh
@@ -7,38 +7,35 @@
 #compile dep libraries
 
 #example 1: ./buildServer
-#example 2: ./buildServer BUILD_MID_WARE DEBUG
-#example 3: ./buildServer BUILD_MID_WARE
+#example 2: ./buildServer DEBUG
 
-if [ "$1" == "BUILD_MID_WARE" ]; then
-   echo "we dont need to compile message and tools again"
-else
-   echo "we only build NF SDK here"
-    cd NFComm/NFMessageDefine
-    ./cpp.sh
-    cd ../../
 
-    cd BuildScript/linux/
-    chmod -R 755 ./BuildNF.CMake.Tools.sh
-    ./BuildNF.CMake.Tools.sh
-    cd ../../
+echo "we only build NF SDK here"
+cd NFComm/NFMessageDefine
+./cpp.sh
+cd ../../
 
-    cd _Out/NFDataCfg/Tool/
-    chmod 755 ./NFFileProcess
-    chmod 755 ./copy_files.sh
-    ./copy_files.sh
-    cd ..
-    cd ..
-    cd ..
-fi
+cd BuildScript/linux/
+chmod -R 755 ./BuildNF.CMake.Tools.sh
+./BuildNF.CMake.Tools.sh
+cd ../../
 
-if [ "$2" == "DEBUG" ]; then
+cd _Out/NFDataCfg/Tool/
+chmod 755 ./NFFileProcess
+chmod 755 ./copy_files.sh
+./copy_files.sh
+cd ..
+cd ..
+cd ..
+
+
+if [ "$1" == "DEBUG" ]; then
 rm -rf ./_Out/Debug/NFServer
 rm -rf ./_Out/Debug/*.a
 
 cd BuildScript/linux/
 chmod -R 755 ./BuildNF.CMake.Debug.sh
-time ./BuildNF.CMake.Debug.sh  $1
+time ./BuildNF.CMake.Debug.sh  
 
 else
 
@@ -47,7 +44,7 @@ rm -rf ./_Out/Release/*.a
 
 cd BuildScript/linux/
 chmod -R 755 ./BuildNF.CMake.Release.sh
-time ./BuildNF.CMake.Release.sh  $1
+time ./BuildNF.CMake.Release.sh 
 fi
 
 cd ../../
